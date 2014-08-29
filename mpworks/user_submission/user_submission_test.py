0e36c5845e91783d9fab6851db9048d937d4ac9e
Patrick Huck <patrick@the-huck.com>
2014-08-28 18:30:18 -0700

    new import scheme works, orient by user

--- mpworks/user_submission/user_submission_test.py
+++ mpworks/user_submission/user_submission_test.py
@@ -4,23 +4,8 @@ from StringIO import StringIO
 import json, sys, re, string
 import matplotlib.pyplot as plt
 pd.options.display.mpl_style = 'default'
-
-# (sub-)section separator regex'es
 section_separator_regex = r'\n*>{4,}(.+)\n*'
 subsection_separator_regex = r'\n*>{2,3}(.+)\n*'
-
-options = [
-    { 'header': None },
-    { 'index_col': 0, 'header': None },
-    {}, {'index_col': 0}, {}
-]
-orients = [ 'values', 'index', 'records', 'columns', 'values' ]
-plotopts = [
-    {}, {},
-    { 'x': 'name', 'kind': 'bar' },
-    { 'table': True },
-    { 'x': 'freq' }
-]
 
 def clean_title(title):
     """strip in-line comments & spaces, make lower-case"""
@@ -31,9 +16,6 @@ def read_csv(s, **options):
     return pd.read_csv(
         StringIO(s), comment='#', skipinitialspace=True, squeeze=True, **options
     )
-
-def json_rep():
-    pass
 
 # read input file,
 # split into section title line (even) and section body (odd entries)
@@ -46,34 +28,52 @@ doc = {}
 for section_index,section_body in enumerate(sections[1::2]):
     section_title = clean_title(sections[2*section_index])
     subsections = re.split(subsection_separator_regex, section_body)
-    number_subsections = len(subsections)
-    # all sections can have optional subsections named 'general', 'plot' & 'data'
-    if number_subsections == 1: # no subsections (only data) or 'general'-section
+    # 'general'-section or section with no subsections (only data)
+    if len(subsections) == 1:
         read_csv_options = {
             'sep': ':' if section_title == 'general' else ',',
             'header': None if section_title == 'general' else 0,
             'index_col': 0,
         }
         df = read_csv(subsections[0], **read_csv_options)
-        print '%s:\n%r' % (section_title, df)
+        to_json_options = {
+            'orient': 'index' if section_title == 'general' else 'columns',
+        }
+        df_json = df.to_json(**to_json_options)
+        doc_json = '{"%s":%s}' % (section_title, df_json) 
+        doc.update(json.loads(doc_json))
         continue
-    continue
+    # section with subsections 'general' or 'plot' or 'data'
     subsections = subsections[1:] # dirty fix to omit leading empty string
+    user_orient = None
     for subsection_index,subsection_body in enumerate(subsections[1::2]):
         subsection_title = clean_title(subsections[2*subsection_index])
-        df = read_csv(subsection_body, sep = ',' if subsection_title == 'data' else ':')
-        print '%s > %s: %s' % (section_title, subsection_title, df.to_json())
-    continue
-    # import data table
-    df_json = df.to_json(orient=orients[i])
-    sec_json = '{"'+section_titles[i-1]+'":'+df_json+'}'
-    doc.update(json.loads(sec_json))
-    # plot (possibly replace with plot.ly)
-    fig, ax = plt.subplots(1, 1)
-    if i == 2: ax.get_xaxis().set_visible(False)
-    df.plot(ax=ax, **plotopts[i])
-    plt.savefig('png/fig%d' % i, dpi=300, bbox_inches='tight')
+        read_csv_options = { 'sep': ',' }
+        if subsection_title != 'data':
+            read_csv_options = { 'sep': ':', 'index_col': 0, 'header': None }
+        df = read_csv(subsection_body, **read_csv_options)
+        if subsection_title == 'general' and 'orient' in df.index:
+            user_orient = df.orient
+        df_json = df.to_json(
+            orient = 'index' if (
+                subsection_title != 'data' or user_orient is None
+            ) else user_orient
+        )
+        doc_json = '{"%s":{"%s":%s}}' % (section_title, subsection_title, df_json)
+        doc.update(json.loads(doc_json))
 
-json.dump(
-    doc, open('output.json','wb'), indent=2, sort_keys=True
-)
+json.dump(doc, open('output.json','wb'), indent=2, sort_keys=True)
+
+## TODO plotting based on output.json
+## plot (possibly replace with plot.ly)
+#fig, ax = plt.subplots(1, 1)
+#if i == 2: ax.get_xaxis().set_visible(False)
+#df.plot(ax=ax, **plotopts[i])
+#plt.savefig('png/fig%d' % i, dpi=300, bbox_inches='tight')
+#plotopts = [
+#    {}, {},
+#    { 'x': 'name', 'kind': 'bar' },
+#    { 'table': True },
+#    { 'x': 'freq' }
+#]
+
