6d314810e085f0ed0caccdc41f9cfb3c2462fd15
Patrick Huck <patrick@the-huck.com>
2014-10-09 16:34:54 -0700

    submit_cif_from_snl: test sma.submit_snl

--- mpworks/user_submission/user_submission_test.py
+++ mpworks/user_submission/user_submission_test.py
@@ -146,9 +146,9 @@ def plot(filename):
 def submit_snl_from_cif(submitter_email, cif_file, metadata_file):
         """
         method to submit StructureNL object generated from CIF file via separate
-        file containing MetaData in YAML format. Developed to be used for the
-        submission of new structures during RSC publishing process (pilot
-        project).
+        file containing MetaData in YAML format as required by the MPStructureNL
+        constructor. Developed to be used for the submission of new structures
+        during RSC publishing process (pilot project).
 
         Args:
             metadata_file: name of file parsed via monty's loadfn
@@ -156,14 +156,17 @@ def submit_snl_from_cif(submitter_email, cif_file, metadata_file):
         from monty.serialization import loadfn
         from pymatgen.core import Structure
         from pymatgen.matproj.snl import StructureNL
-        #from mpworks.submission.submission_mongo import SubmissionMongoAdapter
-        structure = Structure.from_file(cif_file)
-        config = loadfn(metadata_file)
+        from mpworks.submission.submission_mongo import SubmissionMongoAdapter
+        sma = SubmissionMongoAdapter.auto_load()
+        pth = os.path.dirname(os.path.realpath(__file__))
+        structure = Structure.from_file(os.path.join(pth, cif_file))
+        config = loadfn(os.path.join(pth, metadata_file))
         if not config['references'].startswith('@'):
-            config['references'] = open(config['references'],'r').read()
+            config['references'] = open(
+                os.path.join(pth, config['references']),'r'
+            ).read()
         snl = StructureNL(structure, **config)
-        #sma = SubmissionMongoAdapter.auto_load()
-        #sma.submit_snl(snl, submitter_email)
+        sma.submit_snl(snl, submitter_email)
 
 if __name__ == '__main__':
     import argparse, os
