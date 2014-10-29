90199604ca79cdbe3c236a86f78268f2102c6562
Patrick Huck <patrick@the-huck.com>
2014-10-28 21:54:26 -0700

    start submit_contribution()

--- mpworks/user_contributions/rest.py
+++ mpworks/user_contributions/rest.py
@@ -3,9 +3,11 @@ from monty.serialization import loadfn
 from pymatgen.core import Structure
 from pymatgen.matproj.snl import StructureNL
 from mpworks.submission.submission_mongo import SubmissionMongoAdapter
+from parsers import RecursiveParser
 
 def submit_snl_from_cif(submitter_email, cif_file, metadata_file):
-    """
+    """submit StructureNL via CIF and YAML MetaData files
+
     method to submit StructureNL object generated from CIF file via separate
     file containing MetaData in YAML format as required by the MPStructureNL
     constructor. Developed to be used for the submission of new structures
@@ -24,3 +26,22 @@ def submit_snl_from_cif(submitter_email, cif_file, metadata_file):
         ).read()
     snl = StructureNL(structure, **config)
     sma.submit_snl(snl, submitter_email)
+
+
+def submit_contribution(
+    input_handle, contribution_id=None, parser=RecursiveParser()
+):
+    """submit user data to `user_contributions` database
+
+    Args:
+    input_handle: object to "connect" to input, i.e. file handle
+    contribution_id: None if new contribution else update/replace
+    parser: parser class to use on input handle
+    """
+    if not isinstance(input_handle, file):
+        raise TypeError(
+            'type %r not supported as input handle!' % type(input_handle)
+        )
+    fileExt = os.path.splitext(input_handle.name)[1][1:]
+    parser.parse(input_handle.read(), fileExt=fileExt)
+    return parser
