19ef681e7c94c8df42fc621c16f7b28adfc23c90
Patrick Huck <patrick@the-huck.com>
2014-10-29 15:54:48 -0700

    fakers: _get_level_n_section_line

--- mpworks/user_contributions/test_faker.py
+++ mpworks/user_contributions/test_faker.py
@@ -1,4 +1,9 @@
 from mpworks.user_contributions.fakers import CsvInputFile
 f = CsvInputFile()
+print 'test 1 ...'
 for i in range(5):
     print f._get_level_n_section_line(i)
+print 'test 2 ...'
+for i in range(10):
+    for j in range(3):
+        print f._get_level_n_section_line(j)
