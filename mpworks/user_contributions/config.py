23f6d3b2e789ce2eddd4f35a78be9b382e44f392
Patrick Huck <patrick@the-huck.com>
2014-10-29 23:54:46 -0700

    fakers: make_file, mp_cat_keys

--- mpworks/user_contributions/config.py
+++ mpworks/user_contributions/config.py
@@ -1,5 +1,15 @@
 indent_symbol = '>'
 min_indent_level = 3 # minimum level to avoid collision w/ '>>'
-mp_category_keys = [ 'mp_id', 'composition', 'chemical_system' ]
+mp_categories = {
+    'mp_id': [
+        'numerify', 'mp-####'
+    ], # mp-id, e.g. mp-1234
+    'composition': [
+        'bothify', '??#?#'
+    ], # composition, e.g. Fe2O3
+    'chemical_system': [
+        'bothify', '??[#-#,#]?#'
+    ], # chem. system, e.g. Fe[2-3,5]O4
+}
 mp_level01_titles = [ 'general', 'data', 'plot' ]
 csv_comment_char = '#'
--- mpworks/user_contributions/test_faker.py
+++ mpworks/user_contributions/test_faker.py
@@ -1,3 +1,3 @@
 from mpworks.user_contributions.fakers import CsvInputFile
 f = CsvInputFile()
-f._make_level_n_section(0, 3, 2) # start_level, max_level, max_num_subsec
+f.make_file()
