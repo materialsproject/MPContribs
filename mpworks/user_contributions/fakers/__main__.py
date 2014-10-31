16a481595a5b1bd5a29e26725f927eaba76cc19a
Patrick Huck <patrick@the-huck.com>
2014-10-31 16:26:22 -0700

    fakers: argparse in main

--- mpworks/user_contributions/fakers/__main__.py
+++ mpworks/user_contributions/fakers/__main__.py
@@ -1,4 +1,9 @@
+import argparse
 from mp_input_file_v1 import MPInputFile
 
-f = MPInputFile(main_general=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("--main", help="contribution mode (main general or not)",
+                    action="store_true")
+args = parser.parse_args()
+f = MPInputFile(main_general=args.main)
 f.make_file(3)
