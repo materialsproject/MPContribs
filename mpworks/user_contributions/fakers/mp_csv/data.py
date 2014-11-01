ce2eebe3a21a728d3accc26f8748c66c1b04b8fe
Patrick Huck <patrick@the-huck.com>
2014-10-31 18:24:53 -0700

    fakers.mp_csv.data: organize_player_info()

--- mpworks/user_contributions/fakers/mp_csv/data.py
+++ mpworks/user_contributions/fakers/mp_csv/data.py
@@ -1,7 +1,9 @@
-import os
+import os, re
 from faker import Faker
-from ...config import csv_database
 from pandas.io.parsers import read_csv
+from collections import Counter
+from ...config import csv_database
+from ...parsers import RecursiveDict
 
 class DataGenerator(object):
     """generate MP-like data from baseball database
@@ -17,3 +19,21 @@ class DataGenerator(object):
         df = read_csv(self.master, index_col=0)
         player_id = self.fake.random_element(elements=df.index)
         return df.xs(player_id).dropna()
+
+    def _split_string_at_caps(self, string):
+        return re.split(r'([A-Z][a-z]*)', string)[:-1]
+
+    def organize_player_info(self):
+        """organize player info into nested dict"""
+        player = self.get_player()
+        splits = map(self._split_string_at_caps, player.index)
+        counter = Counter([ el[0] for el in splits if el ])
+        subsecs = [key for key,cnt in counter.iteritems() if cnt > 1]
+        info = RecursiveDict({})
+        for k,v in player.iteritems():
+            keys = self._split_string_at_caps(k)
+            nested = {keys[0]: {keys[1]: v}} if (
+                keys and keys[0] in subsecs
+            ) else {k: v}
+            info.rec_update(nested)
+        return info
