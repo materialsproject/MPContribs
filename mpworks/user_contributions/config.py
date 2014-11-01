aa1eb626fcc7e5589764e3d26a6cbebe41263100
Patrick Huck <patrick@the-huck.com>
2014-10-31 17:34:32 -0700

    fakers: start DataGenerator for mp_csv

--- mpworks/user_contributions/config.py
+++ mpworks/user_contributions/config.py
@@ -1,3 +1,5 @@
+import os
+
 indent_symbol = '>'
 min_indent_level = 3 # minimum level to avoid collision w/ '>>'
 mp_categories = {
@@ -13,3 +15,7 @@ mp_categories = {
 }
 mp_level01_titles = [ 'general', 'data', 'plots' ]
 csv_comment_char = '#'
+csv_database = os.path.join(
+  os.path.dirname(os.path.realpath(__file__)),
+  'test_files/lahman-csv_2014-02-14'
+)
