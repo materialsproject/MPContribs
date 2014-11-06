f147c1cf844bacc1517233c3f47c66316353d47d
Patrick Huck <patrick@the-huck.com>
2014-11-06 14:08:50 -0800

    builders: simple propagation from contributions to materials works

--- mpworks/user_contributions/builders.py
+++ mpworks/user_contributions/builders.py
@@ -1,9 +1,19 @@
+from pymatgen.matproj.snl import Author
 from pymongo import MongoClient
-host, port, db_name = 'localhost', 27017, 'user_contributions'
+host, port, db_name = 'localhost', 27019, 'user_contributions'
 username, password = 'test', 'test'
 client = MongoClient(host, port, j=False)
 client[db_name].authenticate(username, password)
-db = client[db_name].contributions
+contrib_coll = client[db_name].contributions
+mat_coll = client[db_name].materials
+
+# http://stackoverflow.com/a/19647596
+def flatten_dict(dd, separator='_', prefix=''):
+    return { prefix + separator + k if prefix else k : v
+            for kk, vv in dd.items()
+            for k, v in flatten_dict(vv, separator, kk).items()
+           } if isinstance(dd, dict) else { prefix : dd }
+
 # NOTE: distinguish mp cat's by format of mp_cat_id
 pipeline = [
     { '$group': {
@@ -12,5 +22,21 @@ pipeline = [
         'contrib_ids': { '$addToSet': '$contribution_id' }
     }}
 ]
-for doc in db.aggregate(pipeline, cursor={}):
-    print doc
+for doc in contrib_coll.aggregate(pipeline, cursor={}):
+    for cid in doc['contrib_ids']:
+        contrib = contrib_coll.find_one(
+            {'contribution_id': cid}, {
+                'content.data': 0, 'content.plots': 0, '_id': 0
+            }
+        )
+        author = Author.parse_author(contrib['contributor_email'])
+        tabular_data = flatten_dict(contrib)
+        mat_coll.update( # TODO: overwriting previous nested keys?
+            {'task_id': doc['_id']},
+            { '$set': {
+                'external_data': {
+                    author.name: { 'tabular_data': tabular_data }
+                }
+            }}
+        )
+        print doc['_id']
