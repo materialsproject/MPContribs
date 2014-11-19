80d87fc1ef06ef18db6940ff2079d9e73e6bbda3
Patrick Huck <patrick@the-huck.com>
2014-11-19 15:50:48 -0800

    __main__: --infile, --reset, --build

--- mpworks/user_contributions/__main__.py
+++ mpworks/user_contributions/__main__.py
@@ -6,7 +6,9 @@ from builders import MPContributionsBuilder
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-contributions", default=5, type=int,
                     help="number of contributions to fake")
-parser.add_argument("--infile", help="submit specific input file")
+parser.add_argument("--infile", help="submit specific input file (ignored if --build")
+parser.add_argument("--reset", help="reset contributions collection (ignored if --build)", action="store_true")
+parser.add_argument("--build", help="build ", action="store_true")
 parser.add_argument("--log", help="show log output", action="store_true")
 args = parser.parse_args()
 loglevel = 'DEBUG' if args.log else 'WARNING'
@@ -15,19 +17,19 @@ logging.basicConfig(
 )
 contributor = 'Patrick Huck <phuck@lbl.gov>'
 
-cma = ContributionMongoAdapter()
-if args.infile is None:
-    cma._reset()
-    cma.fake_multiple_contributions(num_contributions=args.num_contributions)
-else:
-    logging.info(cma.submit_contribution(
-        open(args.infile, 'rb'), contributor
-    ))
-
-# TODO: make incremental
-mcp = MPContributionsBuilder()
-mcp._reset()
-mcp.build()
+if not args.build:
+    cma = ContributionMongoAdapter()
+    if args.reset: cma._reset()
+    if args.infile is None:
+        cma.fake_multiple_contributions(num_contributions=args.num_contributions)
+    else:
+        logging.info(cma.submit_contribution(
+            open(args.infile, 'rb'), contributor
+        ))
+else: # TODO: make incremental
+    mcb = MPContributionsBuilder()
+    mcb._reset()
+    mcb.build()
 
 #submit_snl_from_cif(
 #    contributor, 'test_filesFe3O4.cif',
