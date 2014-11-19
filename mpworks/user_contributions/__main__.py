20a61e092d97a193f9fa16bb8bd9bfd5c7da98ff
Patrick Huck <patrick@the-huck.com>
2014-11-19 14:18:42 -0800

    add infile arg to __main__

--- mpworks/user_contributions/__main__.py
+++ mpworks/user_contributions/__main__.py
@@ -6,22 +6,30 @@ from builders import MPContributionsBuilder
 parser = argparse.ArgumentParser()
 parser.add_argument("--num-contributions", default=5, type=int,
                     help="number of contributions to fake")
+parser.add_argument("--infile", help="submit specific input file")
 parser.add_argument("--log", help="show log output", action="store_true")
 args = parser.parse_args()
 loglevel = 'DEBUG' if args.log else 'WARNING'
 logging.basicConfig(
     format='%(message)s', level=getattr(logging, loglevel)
 )
+contributor = 'Patrick Huck <phuck@lbl.gov>'
 
 cma = ContributionMongoAdapter()
-cma._reset()
-cma.fake_multiple_contributions(num_contributions=args.num_contributions)
+if args.infile is None:
+    cma._reset()
+    cma.fake_multiple_contributions(num_contributions=args.num_contributions)
+else:
+    logging.info(cma.submit_contribution(
+        open(args.infile, 'rb'), contributor
+    ))
 
+# TODO: make incremental
 mcp = MPContributionsBuilder()
 mcp._reset()
 mcp.build()
 
 #submit_snl_from_cif(
-#    'Patrick Huck <phuck@lbl.gov>', 'test_filesFe3O4.cif',
+#    contributor, 'test_filesFe3O4.cif',
 #    'test_files/input_rsc.yaml'
 #)
