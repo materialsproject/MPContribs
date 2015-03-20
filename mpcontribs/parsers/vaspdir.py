7692f2e7357581c892f0001c8e8945c4164f56ed
Patrick Huck <patrick@the-huck.com>
2015-03-19 22:34:28 -0700

    vaspdir: starting diffcoeff for SI2, using drone

--- mpcontribs/parsers/vaspdir.py
+++ mpcontribs/parsers/vaspdir.py
@@ -1,85 +1,44 @@
-import os, json
-from pymatgen.io.vaspio import Vasprun
-from base import BaseParser
-from pymongo import MongoClient
-from monty.serialization import loadfn
-from collections import Counter
+import os, json, math
+from pymatgen.apps.borg.hive import SimpleVaspToComputedEntryDrone
+from pymatgen.apps.borg.queen import BorgQueen
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from ..config import mp_level01_titles
 from utils import nest_dict
 
-class VaspDirParser(BaseParser):
-    # case identical to RecursiveParse as list of (independent) vasp runs
-    # for arbitrary structure => no main general section
-    # -> fill self.document with all vasp runs
-    def __init__(self, rootdir, db_yaml='materials_db_prod.yaml'):
-        BaseParser.__init__(self)
-        # counter for uniquification of mp_id
-        self.mp_id_counter = Counter()
-        # find all vasprun files in rootdir (recursive)
-        self.vasprun_files = [
-            os.path.join(os.path.abspath(root), curfile)
-            for root, dirs, files in os.walk(rootdir)
-            for curfile in files
-            if curfile == 'vasprun.xml'
-        ]
-        # connect to materials database
-        config = loadfn(os.path.join(os.environ['DB_LOC'], db_yaml))
-        client = MongoClient(config['host'], config['port'], j=False)
-        client[config['db']].authenticate( config['username'], config['password'])
-        self.mat_coll = client[config['db']].materials
+class VaspDirParser():
+    def __init__(self, rootdir):
+        """read vasp output via drone (main general section?)"""
+        self.drone = SimpleVaspToComputedEntryDrone(inc_structure=True)
+        self.queen = BorgQueen(self.drone, rootdir, 2)
+        self.data = self.queen.get_data()
 
-    def parse(self):
-        for i, vasprun_file in enumerate(self.vasprun_files):
-            # parse via Vasprun for each vasprun file
-            vasprun = Vasprun(vasprun_file)
-            # The mp-id should be determined from the final structure (last
-            # ionic step vasprun.structures[-1]) in the vasprun.xml. This
-            # requires getting the spacegroup and composition of the structure
-            # and matching it with the according set of structures available in
-            # the materials database (canonical snls). It's not clear, yet, how
-            # to accomplish this efficiently and what to do if the structure to
-            # be submitted does not exist in MP, yet. Hence, for now, since
-            # we're testing diffusion vasp calculations, we attribute the
-            # vasprun to the MP material for the most abundant element in the
-            # submitted structure (Cu).
-            comp = vasprun.structures[-1].composition
-            elem = max(comp, key=comp.get).symbol
-            mp_id = self.mat_coll.find_one({'pretty_formula': elem}).get('task_id')
-            # uniquify mp-id via --<#>
-            self.mp_id_counter.update([mp_id])
-            mp_id = '--'.join([mp_id, str(self.mp_id_counter[mp_id]-1)])
-            # add extra key for vasprun.xml file location to document
-            vasprun_dirname = os.path.dirname(vasprun_file)
-            self.document.rec_update(nest_dict(
-                {'vasprun_dirname': vasprun_dirname}, [mp_id, mp_level01_titles[0]]
-            ))
-            # extract list of e_wo_entrp for each ionic step
-            ycols = [ [
-                v for es_dict in ionic_step['electronic_steps'][1:]
-                for k,v in es_dict.iteritems() if k == 'e_wo_entrp'
-            ] for ionic_step in vasprun.ionic_steps ]
-            # afterburn: make all ycols of same length (fill with None)
-            max_el_steps = len(max(ycols, key=len))
-            ycols = [
-                col + [None] * (max_el_steps-len(col))
-                for col in ycols
-            ]
-            # prepare ycols dict for document
-            # x: electronic step number (esN), y: e_wo_entrp (ewe) for each ionic step (is)
-            # format: esN ewe_is0 ewe_is1 ... ewe_isN
-            ycols_dict = dict(('ewe_is%d' % n, col) for n,col in enumerate(ycols))
-            ycols_dict.update({'esN': range(max_el_steps)})
-            # add special data section for default graph to document
-            self.document.rec_update(nest_dict(
-                ycols_dict, [mp_id, mp_level01_titles[1]]
-            ))
-            # add plots section for default plot (x: index column)
-            self.document.rec_update(nest_dict(
-                {'x': 'esN', 'marker': 'o'},
-                [mp_id, mp_level01_titles[2], 'default']
-            ))
-            # add full vasprun to document
-            self.document.rec_update(nest_dict(
-                {'vasprun': vasprun.as_dict()}, [mp_id]
-            ))
-            break # TODO: remove to extend to all files
+    def reduce(self):
+        """extraction/reduce phase"""
+        struct, values = None, {}
+        for entry in self.data:
+            if 'perfect_stat' in entry.data['filename']:
+                struct = entry.structure
+        numatom = len(struct)
+        reduced = SpacegroupAnalyzer(struct, symprec=1e-2).get_primitive_standard_structure()
+        values['a'] = reduced.lattice.abc[0] * math.sqrt(2) * 10**(-8)
+        print values['a']
+        #values['enebarr'] = self.get_barrier(Edir,Edir_saddle,Edir_min)
+        #values['v'] = self.get_v(vdir,vdir_num,vdir_denom)
+        #values['HVf'] = self.get_HB_and_HVf(Hdir,numatom,'HVf')
+
+    def compile(self):
+        """compile phase"""
+        ## prepare ycols dict for document
+        ## x: electronic step number (esN), y: e_wo_entrp (ewe) for each ionic step (is)
+        ## format: esN ewe_is0 ewe_is1 ... ewe_isN
+        #ycols_dict = dict(('ewe_is%d' % n, col) for n,col in enumerate(ycols))
+        #ycols_dict.update({'esN': range(max_el_steps)})
+        ## add special data section for default graph to document
+        #self.document.rec_update(nest_dict(
+        #    ycols_dict, [mp_id, mp_level01_titles[1]]
+        #))
+        ## add plots section for default plot (x: index column)
+        #self.document.rec_update(nest_dict(
+        #    {'x': 'esN', 'marker': 'o'},
+        #    [mp_id, mp_level01_titles[2], 'default']
+        #))
