da4492b737f5c5c82ddd0cb051d5ade4b007b6a9
Patrick Huck <patrick@the-huck.com>
2015-03-22 00:47:11 -0700

    parsers -> io rename

--- mpcontribs/__main__.py
+++ mpcontribs/__main__.py
@@ -1,6 +1,6 @@
 import argparse, logging, os
 from rest import ContributionMongoAdapter#,submit_snl_from_cif
-from parsers.mpfile import RecursiveParser
+from io.mpfile import RecursiveParser
 from builders import MPContributionsBuilder
 
 if __name__ == '__main__':
