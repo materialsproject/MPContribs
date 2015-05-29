b9b58453b4f055cda9eec7adef811e42647452e1
Patrick Huck <patrick@the-huck.com>
2015-05-29 15:03:52 -0700

    io: omit csv/tsv check, improve MPFile json print

--- mpcontribs/io/mpfile.py
+++ mpcontribs/io/mpfile.py
@@ -47,7 +47,7 @@ class MPFile(PMGSONable):
         Returns:
             String representation of MPFile.
         """
-        return json.dumps(self.document)
+        return json.dumps(self.document, indent=4)
 
     def __repr__(self):
         return self.get_string()
