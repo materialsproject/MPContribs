244f4468a564ad6d620ceaa7f9021ba8854911b7
Patrick Huck <patrick@the-huck.com>
2015-06-19 16:36:22 -0700

    mpfile: start get_string method

--- mpcontribs/io/utils.py
+++ mpcontribs/io/utils.py
@@ -1,8 +1,11 @@
-from collections import OrderedDict
+from collections import OrderedDict, Mapping
+from ..config import indent_symbol, min_indent_level
 
 class RecursiveDict(OrderedDict):
-    """https://gist.github.com/Xjs/114831"""
+    """extension of dict for internal representation of MPFile"""
+
     def rec_update(self, other):
+        """https://gist.github.com/Xjs/114831"""
         for key,value in other.iteritems():
             if key in self and \
                isinstance(self[key], dict) and \
@@ -11,6 +14,20 @@ class RecursiveDict(OrderedDict):
                 self[key].rec_update(value)
             elif key not in self: # don't overwrite existing unnested key
                 self[key] = value
+
+    def iterate(self, nested_dict=None):
+        """http://stackoverflow.com/questions/10756427/loop-through-all-nested-dictionary-values"""
+        d = self if nested_dict is None else nested_dict
+        if nested_dict is None: self.level = 0
+        for key,value in d.iteritems():
+            if isinstance(value, Mapping):
+                yield get_indentor(n=self.level), key
+                self.level += 1
+                for inner_key,inner_value in self.iterate(nested_dict=value):
+                    yield inner_key, inner_value
+                self.level -= 1
+            else:
+                yield key, value
 
 def nest_dict(dct, keys):
     """nest dict under list of keys"""
