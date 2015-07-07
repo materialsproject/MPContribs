978bacd7203526ef954fc9d304a9d34a8a1bcce8
Patrick Huck <patrick@the-huck.com>
2015-07-07 15:04:34 -0700

    start simple webui for MPFile viewer

---
+++ mpcontribs/webui/webui.py
@@ -0,0 +1,17 @@
+import json
+from flask import Flask
+from mpcontribs.io.mpfile import MPFile
+from mpcontribs.rest import ContributionMongoAdapter
+app = Flask('webui')
+
+@app.route('/')
+def index():
+    contributor = 'Patrick Huck <phuck@lbl.gov>'
+    mpfile = MPFile.from_file('test_files/atn_test_input.txt')
+    cma = ContributionMongoAdapter()
+    docs = cma.submit_contribution(mpfile, contributor)
+    return '<html><body><pre><code>'+json.dumps(docs, indent=4)+'</code></pre></body></html>'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
