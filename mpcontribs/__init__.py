f01361bc5c9210791b125a3a3443179416e3ba3f
Patrick Huck <patrick@the-huck.com>
2015-07-09 16:20:44 -0700

    add setup.py

--- mpcontribs/__init__.py
+++ mpcontribs/__init__.py
@@ -1,6 +1,8 @@
+"""The Materials Project's Community Contribution Framework"""
+
+__version__ = '0.0'
+__url__ = 'https://github.com/materialsproject/MPContribs'
 __author__ = 'Patrick Huck'
-__copyright__ = 'Copyright 2014, The Materials Project'
-__version__ = '0.1'
-__maintainer__ = 'Patrick Huck'
 __email__ = 'phuck@lbl.gov'
-__date__ = 'Aug 22, 2014'
+__copyright__ = 'Copyright 2014-2015, The Materials Project'
+__maintainer__ = 'Patrick Huck'
---
+++ setup.py
@@ -0,0 +1,28 @@
+import io, re
+from setuptools import setup
+
+package_name = 'mpcontribs'
+init_py = io.open('{}/__init__.py'.format(package_name)).read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
+
+setup(
+    name = package_name,
+    version = metadata['version'],
+    description = metadata['doc'],
+    author = metadata['author'],
+    author_email = metadata['email'],
+    url = metadata['url'],
+    packages = [
+        package_name, '{}.fake'.format(package_name),
+        '{}.io'.format(package_name),
+        '{}.webui'.format(package_name)
+    ],
+    include_package_data=True,
+    install_requires = [
+        'numpy==1.9.1', 'Flask==0.10.1', 'pandas==0.15.2', 'plotly==1.6.15',
+        'six==1.9.0', 'monty==0.6.4'
+    ],
+    license = 'MIT',
+    keywords = ['materials', 'contribution', 'framework', 'data', 'interactive'],
+)
