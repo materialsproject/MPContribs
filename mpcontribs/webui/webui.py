1f57c97071472ac0831f73cde47d0cedf4c9499e
Patrick Huck <patrick@the-huck.com>
2015-07-09 12:45:25 -0700

    webui: buttons to choose and view file

--- mpcontribs/webui/webui.py
+++ mpcontribs/webui/webui.py
@@ -1,5 +1,5 @@
 import json, os, pwd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mpcontribs.io.mpfile import MPFile
 from mpcontribs.rest import ContributionMongoAdapter
 from mpcontribs.builders import MPContributionsBuilder
@@ -7,13 +7,21 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 stat_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 app = Flask('webui', template_folder=tmpl_dir, static_folder=stat_dir)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+@app.route('/<path:path>', methods=['GET', 'POST'])
 def index(path):
-    if not path: return 'Append path to URL in order to view MPFile'
+    print path, request.method
+    if not path and request.method == 'GET': return '''
+    <!doctype html>
+    <title>MPFile Viewer</title>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+    <input type=submit value=View>
+    </form>
+    '''
     full_name = pwd.getpwuid(os.getuid())[4]
     contributor = '{} <phuck@lbl.gov>'.format(full_name)
-    mpfile = MPFile.from_file(path)
+    mpfile = MPFile.from_file(path if path else request.files['file'])
     cma = ContributionMongoAdapter()
     docs = cma.submit_contribution(mpfile, contributor)
     mcb = MPContributionsBuilder(docs)
