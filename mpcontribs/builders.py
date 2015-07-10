183317e3bf994fc7a5c25085c08a943fec75cd85
Patrick Huck <patrick@the-huck.com>
2015-07-09 17:09:23 -0700

    builders/rest: remove pymatgen.matproj.snl.Author and pymongo deps

--- mpcontribs/builders.py
+++ mpcontribs/builders.py
@@ -1,7 +1,5 @@
-from pymatgen.matproj.snl import Author
-from pymongo import MongoClient
 from monty.serialization import loadfn
-import json, os, logging
+import json, os, logging, re
 import pandas as pd
 import matplotlib.pyplot as plt
 pd.options.display.mpl_style = 'default'
@@ -10,6 +8,60 @@ from plotly.graph_objs import *
 from itertools import groupby
 from io.utils import nest_dict, RecursiveDict
 from config import mp_level01_titles
+from collections import namedtuple
+
+# from pymatgen.matproj.snl
+class Author(namedtuple('Author', ['name', 'email'])):
+    """
+    An Author contains two fields:
+
+    .. attribute:: name
+
+        Name of author (String)
+
+    .. attribute:: email
+
+        Email of author (String)
+    """
+
+    def __str__(self):
+        """String representation of an Author"""
+        return '{} <{}>'.format(self.name, self.email)
+
+    def as_dict(self):
+        return {"name": self.name, "email": self.email}
+
+    @staticmethod
+    def from_dict(d):
+        return Author(d['name'], d['email'])
+
+    @staticmethod
+    def parse_author(author):
+        """
+        Parses an Author object from either a String, dict, or tuple
+
+        Args:
+            author: A String formatted as "NAME <email@domain.com>",
+                (name, email) tuple, or a dict with name and email keys.
+
+        Returns:
+            An Author object.
+        """
+        if isinstance(author, string_types):
+            # Regex looks for whitespace, (any name), whitespace, <, (email),
+            # >, whitespace
+            m = re.match('\s*(.*?)\s*<(.*?@.*?)>\s*', author)
+            if not m or m.start() != 0 or m.end() != len(author):
+                raise ValueError("Invalid author format! {}".format(author))
+            return Author(m.groups()[0], m.groups()[1])
+        elif isinstance(author, dict):
+            return Author.from_dict(author)
+        else:
+            if len(author) != 2:
+                raise ValueError("Invalid author, should be String or (name, "
+                                 "email) tuple: {}".format(author))
+            return Author(author[0], author[1])
+
 
 class MPContributionsBuilder():
     """build user contributions from `mg_core_*.contributions`"""
@@ -37,6 +89,7 @@ class MPContributionsBuilder():
 
     @classmethod
     def from_config(cls, db_yaml='materials_db_dev.yaml'):
+        from pymongo import MongoClient
         config = loadfn(os.path.join(os.environ['DB_LOC'], db_yaml))
         client = MongoClient(config['host'], config['port'], j=False)
         db = client[config['db']]
