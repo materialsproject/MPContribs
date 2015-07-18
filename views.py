f0a8c3473614cf84a24eb5d73678b735f50b7d53
Donny Winston <dwinston@alum.mit.edu>
2015-07-18 02:30:09 -0700

    Ensure proper encoding for composition detail
    For #426

--- views.py
+++ views.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.template import RequestContext, loader
 
-# FIXME UnicodeEncodeError (play nice with (utf-8?) encoding!)
-from monty.json import jsanitize as clean_json
-
 from utils import connector
 
 @connector.mdb
@@ -18,5 +15,22 @@ def composition_contributions(request, composition, mdb=None):
         material = {k: composition for k in ['task_id', 'pretty_formula']}
 
     t = loader.get_template('materials/templates/material_contributions.html')
-    c = RequestContext(request, {'material': clean_json(material)})
+    c = RequestContext(request, {'material': jsanitize(material)})
     return HttpResponse(t.render(c))
+
+# Instead of
+# from monty.json import jsanitize
+# use the following to fix UnicodeEncodeError
+# and play nice with utf-8 encoding
+def jsanitize(obj):
+    if isinstance(obj, (list, tuple)):
+        return [jsanitize(i) for i in obj]
+    elif isinstance(obj, dict):
+        return {unicode(k).encode('utf-8'): jsanitize(v)
+                for k, v in obj.items()}
+    elif isinstance(obj, (int, float)):
+        return obj
+    elif obj is None:
+        return None
+    else:
+        return unicode(obj).encode('utf-8')
