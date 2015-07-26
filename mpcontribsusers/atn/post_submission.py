3e8b9057143515434d369049bf89f7a0e3e5f5a1
Patrick Huck <patrick@the-huck.com>
2015-07-25 17:42:53 -0700

    atn.post_submission #3: pulling and merging graphs into PDF

--- mpcontribsusers/atn/post_submission.py
+++ mpcontribsusers/atn/post_submission.py
@@ -1,4 +1,7 @@
-import argparse
+import argparse, os, requests
+from pymatgen.matproj.rest import MPRester
+from StringIO import StringIO
+from PyPDF2 import PdfFileMerger
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""post submission analyses""")
@@ -22,4 +25,26 @@ if __name__ == '__main__':
         mpfile = MPFile.from_file(args.cids_file) # TODO cid-only read mode?
         cids = [ObjectId(x[1]) for x in mpfile.get_identifiers()]
 
-    print cids
+    #SITE = 'https://www.materialsproject.org'
+    #ENDPOINT, API_KEY = "{}/rest".format(SITE), os.environ.get('MAPI_KEY')
+    SITE = 'http://localhost:8000'
+    ENDPOINT, API_KEY = "{}/rest".format(SITE), os.environ.get('MAPI_KEY_LOC')
+    mpr = MPRester(API_KEY, endpoint=ENDPOINT)
+
+    # TODO: in Plotly, set figure titles to composition
+    merger = PdfFileMerger()
+    for cid in cids:
+        prefix = 'LBNL.{}'.format(cid)
+        criteria = {prefix: {'$exists': 1}}
+        projection = {'{}.plotly_urls'.format(prefix): 1}
+        doc = mpr.query_contributions(
+            criteria=criteria, projection=projection,
+            collection='compositions', contributor_only=False
+        )[0]
+        plotly_url = doc['LBNL'][str(cid)]['plotly_urls'][0] # TODO Ni or Fe
+        print cid, plotly_url
+        image_bytes = requests.get('{}.pdf'.format(plotly_url)).content
+        merger.append(StringIO(image_bytes))
+        break
+    merger.write('test.pdf')
+
