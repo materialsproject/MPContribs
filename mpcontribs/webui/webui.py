e9cd4333226fff613b3e6e23fcb739b79c7aba2e
Patrick Huck <patrick@the-huck.com>
2015-07-29 15:08:35 -0700

    webui: use default/graphs endpoints and parameters

--- mpcontribs/webui/webui.py
+++ mpcontribs/webui/webui.py
@@ -1,19 +1,21 @@
 from __future__ import unicode_literals, print_function
 import json, os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from mpcontribs.utils import submit_mpfile, get_short_object_id
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 stat_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 app = Flask('webui', template_folder=tmpl_dir, static_folder=stat_dir)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
-@app.route('/<path:path>', methods=['GET', 'POST'])
-@app.route('/graphs/<path:path>', methods=['GET', 'POST'])
-def index(path):
-    if not path and request.method == 'GET':
-        return render_template('choose.html')
-    mpfile = path if path else request.files['file']
+@app.route('/default', methods=['GET', 'POST'])
+@app.route('/graphs', methods=['GET', 'POST'])
+def index():
+    mpfile = request.args.get('mpfile', None)
+    if mpfile is None:
+        if 'file' in request.files:
+            mpfile = request.files['file']
+        else:
+            return render_template('choose.html')
     content = submit_mpfile(mpfile, test=True)
     for value in content.itervalues():
         for project_data in value.itervalues():
@@ -23,3 +25,7 @@ def index(path):
                 project_data[cid_short] = d
     template = 'graphs.html' if 'graphs' in request.path else 'index.html'
     return render_template(template, content=content)
+
+@app.route('/')
+def choose():
+  return render_template('choose.html')
