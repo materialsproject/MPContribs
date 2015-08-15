93d84ee2664b8f9731d7ffdf1da4bddb7fcf30db
Patrick Huck <patrick@the-huck.com>
2015-08-14 20:51:25 -0700

    webui: single form, multiple submit buttons

--- mpcontribs/webui/webui.py
+++ mpcontribs/webui/webui.py
@@ -1,19 +1,23 @@
 from __future__ import unicode_literals, print_function
 import json, os
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, session
 from mpcontribs.utils import submit_mpfile, get_short_object_id
 from six import string_types
+from StringIO import StringIO
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 stat_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 app = Flask('webui', template_folder=tmpl_dir, static_folder=stat_dir)
 app.config['JSON_SORT_KEYS'] = False
+app.secret_key = 'xxxrrr'
 
 @app.route('/default', methods=['GET', 'POST'])
 @app.route('/graphs', methods=['GET', 'POST'])
 def index():
-    mpfile = request.args.get('mpfile', request.files.get('file', None))
+    mpfile = request.args.get('mpfile')
     if mpfile is None:
-        return render_template('home.html')
+        mpfile = StringIO(session.get('mpfile'))
+        if mpfile is None:
+            return render_template('home.html')
     content = submit_mpfile(mpfile, test=True)
     for value in content.itervalues():
         for project_data in value.itervalues():
@@ -27,14 +31,26 @@ def index():
 @app.route('/')
 @app.route('/load', methods=['GET', 'POST'])
 def home():
-    mpfile = request.args.get('mpfile', request.files.get('file', None))
+    mpfile = request.args.get('mpfile')
     if mpfile is None:
-        return render_template('home.html')
-    if isinstance(mpfile, string_types):
-        mpfile = open(mpfile, 'r').read()
+        mpfile = session.get('mpfile') if request.path != '/' else None
+        if mpfile is None:
+            return render_template('home.html')
     else:
-        mpfile = mpfile.read()
+        mpfile = open(mpfile, 'r').read()
     return render_template('home.html', content={'aml': mpfile})
+
+@app.route('/action', methods=['POST'])
+def action():
+    session['mpfile'] = request.files.get('file').read()
+    if request.form['submit'] == 'Load File':
+        return redirect('/load')
+    elif request.form['submit'] == 'View MPFile':
+        return redirect('/default')
+    elif request.form['submit'] == 'View Graphs':
+        return redirect('/graphs')
+    else:
+        return redirect('/')
 
 @app.route('/shutdown', methods=['GET', 'POST'])
 def shutdown():
