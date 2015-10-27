36daa8f6374c8b940e3129456bb556305d999c15
Patrick Huck <phuck@lbl.gov>
2015-10-27 15:55:43 -0700

    update requirements, setup

--- setup.py
+++ setup.py
@@ -16,18 +16,15 @@ setup(
     url = metadata['url'],
     packages = [
         package_name, '{}.fake'.format(package_name),
-        '{}.io'.format(package_name),
-        '{}.webui'.format(package_name)
+        '{}.io'.format(package_name), '{}.webui'.format(package_name),
+        '{}.explorer'.format(package_name), '{}.pmg_utils'.format(package_name),
+        '{}.rest'.format(package_name),
     ],
     install_requires = [
         'numpy==1.9.2', 'Flask==0.10.1', 'pandas==0.16.2', 'plotly==1.7.7',
         'six==1.9.0', 'monty==0.6.5', 'matplotlib==1.4.3', 'pymongo==3.0.3',
-        'pyyaml==3.11', 'ipython==3.2.1', 'cufflinks==0.4', #'pymatgen-0.0'
+        'pyyaml==3.11', 'ipython==3.2.1', 'cufflinks==0.4', 'django-1.8.5'
     ],
-    #setup_requires = [ 'numpy==1.9.1', 'pymatgen-0.0' ],
-    #dependency_links = [
-    #    'http://github.com/tschaume/pymatgen/tarball/submit_mpfile#egg=pymatgen-0.0'
-    #],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive'],
     scripts = glob.glob(os.path.join(SETUP_PTH, "scripts", "*")),
