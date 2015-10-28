1af34abc82fa48f8f65b9ffb5952cd0c47e765dc
Patrick Huck <phuck@lbl.gov>
2015-10-28 16:26:03 -0700

    fix more dependencies and imports for nosetests

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
     ],
     install_requires = [
         'numpy', 'Flask', 'pandas', 'plotly', 'six', 'monty', 'matplotlib',
-        'pymongo', 'pyyaml', 'ipython', 'cufflinks', 'Django==1.8.5', 'archieml'
+        'pymongo', 'pyyaml', 'ipython', 'cufflinks', 'Django==1.8.5',
+        'archieml', 'django-browserid'
     ],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive'],
