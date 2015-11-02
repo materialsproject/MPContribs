50661533cdc4385df72f692cfe3cc9e4067132de
Patrick Huck <phuck@lbl.gov>
2015-11-02 13:17:02 -0800

    mpweb_core.configure_settings: include DJANGO_SETTINGS_MODULE

--- mpweb_core/configure_settings.py
+++ mpweb_core/configure_settings.py
@@ -1,3 +1,5 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_site.settings')
 from django.conf import settings
 if not settings.configured:
     from test_site import settings as test_site_settings
