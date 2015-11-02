f496708e26d3aa03891c859a6d97ca86ba803ae6
Patrick Huck <phuck@lbl.gov>
2015-11-02 13:16:30 -0800

    webui: run ipython notebook via multiprocessing

--- mpcontribs/webui/webui.py
+++ mpcontribs/webui/webui.py
@@ -1,13 +1,15 @@
-from __future__ import unicode_literals, print_function
+from __future__ import unicode_literals, print_function, absolute_import
+
 import json, os, socket, SocketServer
+import sys, warnings, multiprocessing
+from IPython.terminal.ipapp import launch_new_instance
 from flask import Flask, render_template, request, Response
 from flask import url_for, redirect, make_response, stream_with_context
 from mpcontribs.utils import process_mpfile, submit_mpfile
-from six import string_types
 from StringIO import StringIO
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 stat_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-app = Flask('webui', template_folder=tmpl_dir, static_folder=stat_dir)
+app = Flask('mpcontribs.webui', template_folder=tmpl_dir, static_folder=stat_dir)
 app.config['JSON_SORT_KEYS'] = False
 app.secret_key = 'xxxrrr'
 
@@ -24,6 +26,31 @@ def patched_finish(self):
 
 SocketServer.StreamRequestHandler.finish = patched_finish
 
+class NotebookProcess(multiprocessing.Process):
+    def __init__(self):
+        super(NotebookProcess, self).__init__(name='notebook_process')
+
+    def run(self):
+        warnings.filterwarnings("ignore", module = "zmq.*")
+        sys.argv.append("notebook")
+        sys.argv.append("--IPKernelApp.pylab='inline'")
+        sys.argv.append("--NotebookApp.ip=localhost")
+        sys.argv.append("--NotebookApp.open_browser=False")
+        sys.argv.append('--NotebookApp.allow_origin="*"')
+        launch_new_instance()
+
+def start_notebook():
+    global notebook_process
+    if not notebook_process:
+        notebook_process = NotebookProcess()
+        notebook_process.start()
+
+def stop_notebook():
+    global notebook_process
+    if notebook_process:
+        notebook_process.terminate()
+        notebook_process = None
+
 def stream_template(template_name, **context):
     # http://stackoverflow.com/questions/13386681/streaming-data-with-python-and-flask
     # http://flask.pocoo.org/docs/patterns/streaming/#streaming-from-templates
@@ -31,6 +58,8 @@ def stream_template(template_name, **context):
     t = app.jinja_env.get_template(template_name)
     rv = t.stream(context)
     return rv
+
+notebook_process = None
 
 @app.route('/view/<template>')
 def view(template):
@@ -50,6 +79,8 @@ def view(template):
 @app.route('/')
 def home():
     session.clear()
+    stop_notebook()
+    start_notebook()
     return render_template('home.html', fmt='archieml')
 
 @app.route('/load')
@@ -92,6 +123,7 @@ def action():
 
 @app.route('/shutdown', methods=['GET', 'POST'])
 def shutdown():
+    stop_notebook()
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
