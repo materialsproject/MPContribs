f2664cad58b35a9f533ec4a42b9b956320a51994
Patrick Huck <phuck@lbl.gov>
2015-11-02 22:36:30 -0800

    webui: also catch exceptions during local process_mpfile

--- mpcontribs/utils.py
+++ mpcontribs/utils.py
@@ -42,63 +42,69 @@ def submit_mpfile(path_or_mpfile, fmt='archieml'):
             return
 
 def process_mpfile(path_or_mpfile, target=None, fmt='archieml'):
-    if isinstance(path_or_mpfile, six.string_types) and \
-       not os.path.isfile(path_or_mpfile):
-        yield '{} not found'.format(path_or_mpfile)
-        return
-    mod = import_module('mpcontribs.io.{}.mpfile'.format(fmt))
-    MPFile = getattr(mod, 'MPFile')
-    full_name = pwd.getpwuid(os.getuid())[4]
-    contributor = '{} <phuck@lbl.gov>'.format(full_name) # fake
-    cma = ContributionMongoAdapter()
-    # split input MPFile into contributions: treat every mp_cat_id as separate DB insert
-    mpfile, cid_shorts = MPFile.from_dict(), [] # output
-    for idx, mpfile_single in enumerate(MPFile.from_file(path_or_mpfile).split()):
-        mp_cat_id = mpfile_single.document.keys()[0]
-        # TODO test update mode
-        cid = mpfile_single.document[mp_cat_id].get('cid', None)
-        update = bool(cid is not None)
-        if update:
+    try:
+        yield 'start processing MPFile ...<br>'
+        if isinstance(path_or_mpfile, six.string_types) and \
+           not os.path.isfile(path_or_mpfile):
+            raise Exception('{} not found'.format(path_or_mpfile))
+        mod = import_module('mpcontribs.io.{}.mpfile'.format(fmt))
+        MPFile = getattr(mod, 'MPFile')
+        full_name = pwd.getpwuid(os.getuid())[4]
+        contributor = '{} <phuck@lbl.gov>'.format(full_name) # fake
+        cma = ContributionMongoAdapter()
+        # split input MPFile into contributions: treat every mp_cat_id as separate DB insert
+        mpfile, cid_shorts = MPFile.from_dict(), [] # output
+        for idx, mpfile_single in enumerate(MPFile.from_file(path_or_mpfile).split()):
+            mp_cat_id = mpfile_single.document.keys()[0]
+            # TODO test update mode
+            cid = mpfile_single.document[mp_cat_id].get('cid', None)
+            update = bool(cid is not None)
+            if update:
+                cid_short = get_short_object_id(cid)
+                yield 'use contribution #{} to update ID #{} ... '.format(idx, cid_short)
+            # always run local "submission" to catch failure before interacting with DB
+            yield 'locally process contribution #{} ... '.format(idx)
+            doc = cma.submit_contribution(mpfile_single, contributor) # does not use get_string
+            cid = doc['_id']
+            yield 'check consistency ... '
+            mpfile_single_cmp = MPFile.from_string(mpfile_single.get_string())
+            if mpfile_single.document != mpfile_single_cmp.document:
+                # compare json strings to find first inconsistency
+                for a, b in zip(
+                    json.dumps(mpfile_single.document, indent=4).split('\n'),
+                    json.dumps(mpfile_single_cmp.document, indent=4).split('\n')
+                ):
+                    if a != b:
+                        raise Exception('{} <====> {}'.format(a.strip(), b.strip()))
+            if target is not None:
+                yield 'submit to MP ... '
+                cid = target.submit_contribution(mpfile_single, fmt) # uses get_string
             cid_short = get_short_object_id(cid)
-            yield 'use contribution #{} to update ID #{} ... '.format(idx, cid_short)
-        # always run local "submission" to catch failure before interacting with DB
-        yield 'locally process contribution #{} ... '.format(idx)
-        doc = cma.submit_contribution(mpfile_single, contributor) # does not use get_string
-        cid = doc['_id']
-        yield 'check consistency ... '
-        mpfile_single_cmp = MPFile.from_string(mpfile_single.get_string())
-        if mpfile_single.document != mpfile_single_cmp.document:
-            # compare json strings to find first inconsistency
-            for a, b in zip(
-                json.dumps(mpfile_single.document, indent=4).split('\n'),
-                json.dumps(mpfile_single_cmp.document, indent=4).split('\n')
-            ):
-                if a != b:
-                    raise Exception('{} <====> {}'.format(a.strip(), b.strip()))
+            mpfile_single.insert_id(mp_cat_id, cid)
+            cid_shorts.append(cid_short)
+            yield 'build into {} ... '.format(mp_cat_id)
+            if target is not None:
+                url = target.build_contribution(cid)
+                yield ("OK. <a href='{}/{}' class='btn btn-default btn-xs' " +
+                       "role='button' target='_blank'>View</a></br>").format(SITE, url)
+            else:
+                mcb = MPContributionsBuilder(doc)
+                yield mcb.build(contributor, cid)
+                yield 'OK.</br>'.format(idx, cid_short)
+            mpfile.concat(mpfile_single)
+            time.sleep(.1)
+        ncontribs = len(cid_shorts)
+        #if target is not None and \
+        #   isinstance(path_or_mpfile, six.string_types) and \
+        #   os.path.isfile(path_or_mpfile):
+        #    yield 'embed #{} in MPFile ...'.format('/'.join(cid_shorts))
+        #    mpfile.write_file(path_or_mpfile, with_comments=True)
         if target is not None:
-            yield 'submit to MP ... '
-            cid = target.submit_contribution(mpfile_single, fmt) # uses get_string
-        cid_short = get_short_object_id(cid)
-        mpfile_single.insert_id(mp_cat_id, cid)
-        cid_shorts.append(cid_short)
-        yield 'build into {} ... '.format(mp_cat_id)
-        if target is not None:
-            url = target.build_contribution(cid)
-            yield ("OK. <a href='{}/{}' class='btn btn-default btn-xs' " +
-                   "role='button' target='_blank'>View</a></br>").format(SITE, url)
+            yield '<strong>{} contributions successfully submitted.</strong>'.format(ncontribs)
         else:
-            mcb = MPContributionsBuilder(doc)
-            yield mcb.build(contributor, cid)
-            yield 'OK.</br>'.format(idx, cid_short)
-        mpfile.concat(mpfile_single)
-        time.sleep(.1)
-    ncontribs = len(cid_shorts)
-    #if target is not None and \
-    #   isinstance(path_or_mpfile, six.string_types) and \
-    #   os.path.isfile(path_or_mpfile):
-    #    yield 'embed #{} in MPFile ...'.format('/'.join(cid_shorts))
-    #    mpfile.write_file(path_or_mpfile, with_comments=True)
-    if target is not None:
-        yield '<strong>{} contributions successfully submitted.</strong>'.format(ncontribs)
-    else:
-        yield '<strong>{} contributions successfully processed.</strong>'.format(ncontribs)
+            yield '<strong>{} contributions successfully processed.</strong>'.format(ncontribs)
+    except:
+        ex = sys.exc_info()[1]
+        yield 'FAILED.</br>'
+        yield str(ex).replace('"',"'")
+        return
