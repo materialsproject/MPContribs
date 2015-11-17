ed2cb8cdf9beb8c725fc3d391ee13d71416b189d
Patrick Huck <phuck@lbl.gov>
2015-11-16 17:32:53 -0800

    webui: fix STATIC_ROOT dir OS-independent

--- mpcontribs/webui/__main__.py
+++ mpcontribs/webui/__main__.py
@@ -3,13 +3,14 @@ from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware, SharedDataMiddleware
 from mpcontribs.webui.webui import app as flask_app
 from test_site.wsgi import application as django_app
+from test_site.settings import STATIC_ROOT
 from django.core.management import call_command
 
 flask_app.debug = True
 call_command('collectstatic', '--clear', '--noinput', '-v 0')
 
 application = DispatcherMiddleware(flask_app, { '/test_site': django_app })
-application = SharedDataMiddleware(application, { '/static': '/tmp/static' })
+application = SharedDataMiddleware(application, { '/static': STATIC_ROOT })
 
 if __name__ == '__main__':
     run_simple('localhost', 5000, application, use_reloader=True,
