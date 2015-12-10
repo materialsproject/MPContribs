27fb4a8fd4f4390453de3a189bf03355a951facb
Patrick Huck <phuck@lbl.gov>
2015-12-10 15:40:16 -0800

    explorer: save options and selections on POST

--- mpcontribs/explorer/views.py
+++ mpcontribs/explorer/views.py
@@ -1,5 +1,6 @@
 """This module provides the views for the explorer interface."""
 
+import json
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mpcontribs.rest.rester import MPContribsRester
@@ -10,26 +11,41 @@ def get_endpoint():
 
 def index(request, collection=None):
     ctx = RequestContext(request)
+    fields = ['identifiers', 'projects', 'cids']
     if request.user.is_authenticated():
-        if collection is not None:
+        if collection in ['materials', 'compositions']:
             API_KEY = request.user.api_key
             ENDPOINT = request.build_absolute_uri(get_endpoint())
             with MPContribsRester(API_KEY, endpoint=ENDPOINT) as mpr:
-                identifiers, projects, cids = set(), set(), set()
-                docs = mpr.query_contributions(collection=collection)
-                if docs:
-                    for doc in docs:
-                        for project, contribs in doc.iteritems():
-                            if project == '_id':
-                                # contribs = mp-id or composition
-                                identifiers.add(contribs)
-                            else:
-                                projects.add(project)
-                                # contribs = contributions for specific project
-                                for cid in contribs:
-                                    cids.add(cid)
-                else:
-                    ctx.update({'alert': 'No {} available!'.format(collection)})
+                if request.method == 'GET':
+                    options = dict((field, set()) for field in fields)
+                    docs = mpr.query_contributions(collection=collection)
+                    if docs:
+                        for doc in docs:
+                            for project, contribs in doc.iteritems():
+                                if project == '_id':
+                                    # contribs = mp-id or composition
+                                    options[fields[0]].add(str(contribs))
+                                else:
+                                    options[fields[1]].add(str(project))
+                                    # contribs = contributions for specific project
+                                    for cid in contribs:
+                                        options[fields[2]].add(str(cid))
+                    else:
+                        ctx.update({'alert': 'No {} available!'.format(collection)})
+                    options = dict((k, list(v)) for k,v in options.iteritems())
+                    selection = dict((field, []) for field in fields)
+                elif request.method == 'POST':
+                    options, selection = (
+                        dict(
+                            (field, [str(el) for el in json.loads(
+                                request.POST['_'.join([prefix, field])]
+                            )]) for field in fields
+                        ) for prefix in ['options', 'selection']
+                    )
+                    # TODO mpr request
+        else:
+            ctx.update({'alert': 'Collection {} does not exist!'.format(collection)})
     else:
         ctx.update({'alert': 'Please log in!'})
     return render_to_response("mpcontribs_explorer_index.html", locals(), ctx)
