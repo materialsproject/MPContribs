a306c2d1220cf72f796bf31b05428d8bd8fc6f5f
Patrick Huck <phuck@lbl.gov>
2016-01-19 17:23:00 -0800

    uw_si2: working on interactive graphs

--- uw_si2/explorer/views.py
+++ uw_si2/explorer/views.py
@@ -15,6 +15,17 @@ def index(request):
         ENDPOINT = request.build_absolute_uri(get_endpoint())
         with UWSI2Rester(API_KEY, endpoint=ENDPOINT) as mpr:
             contribs = jsanitize(mpr.get_uwsi2_contributions())
+            ranges = None
+            for contrib in contribs:
+                table = contrib['tables']['data_supporting']
+                if ranges is None:
+                    col_names = [ c['name'] for c in table['columns'][1:] ] # skip solute string
+                    ranges = dict((k, [1e3, -1e3]) for k in col_names)
+                for row in table['rows']:
+                    for k,v in ranges.iteritems():
+                        val = float(row[k])
+                        if val < v[0]: v[0] = val
+                        if val > v[1]: v[1] = val
     else:
         ctx.update({'alert': 'Please log in!'})
     return render_to_response("uwsi2_explorer_index.html", locals(), ctx)
