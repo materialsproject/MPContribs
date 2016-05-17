2378a8f1c4235ce333232d9f357be462a9ce47c4
Patrick Huck <phuck@lbl.gov>
2016-05-16 22:56:22 -0700

    io.archieml.mpfile: use MPFile.from_dict at end of MPFile.from_string

--- mpcontribs/io/archieml/mpfile.py
+++ mpcontribs/io/archieml/mpfile.py
@@ -12,41 +12,40 @@ class MPFile(MPFileCore):
     @staticmethod
     def from_string(data):
         # use archieml-python parse to import data
-        mpfile = MPFile.from_dict(RecursiveDict(archieml.loads(data)))
+        recdict = RecursiveDict(archieml.loads(data))
         # post-process internal representation of file contents
-        for key in mpfile.document.keys():
+        for key in recdict.keys():
             is_general, root_key = normalize_root_level(key)
             if is_general:
                 # make part of shared (meta-)data, i.e. nest under `general` at
                 # the beginning of the MPFile
-                if mp_level01_titles[0] not in mpfile.document:
-                    mpfile.document.insert_before(
-                        mpfile.document.keys()[0],
+                if mp_level01_titles[0] not in recdict:
+                    recdict.insert_before(
+                        recdict.keys()[0],
                         (mp_level01_titles[0], RecursiveDict())
                     )
-                mpfile.document.rec_update(nest_dict(
-                    mpfile.document.pop(key),
-                    [ mp_level01_titles[0], root_key ]
+                recdict.rec_update(nest_dict(
+                    recdict.pop(key), [ mp_level01_titles[0], root_key ]
                 ))
             else:
                 # normalize identifier key (pop & insert)
                 # using rec_update since we're looping over all entries
                 # also: support data in bare tables (marked-up only by
                 #       root-level identifier) by nesting under 'data'
-                value = mpfile.document.pop(key)
+                value = recdict.pop(key)
                 keys = [ root_key ]
                 if isinstance(value, list): keys.append('table')
-                mpfile.document.rec_update(nest_dict(value, keys))
+                recdict.rec_update(nest_dict(value, keys))
                 # Note: CSV section is marked with 'data ' prefix during iterate()
-                for k,v in mpfile.document[root_key].iterate():
+                for k,v in recdict[root_key].iterate():
                     if isinstance(k, six.string_types) and \
                        k.startswith(mp_level01_titles[1]):
                         # k = table name (incl. data prefix)
                         # v = csv string from ArchieML free-form arrays
                         table_name = k[len(mp_level01_titles[1]+'_'):]
                         pd_obj = read_csv(v)
-                        mpfile.document[root_key].pop(table_name)
-                        mpfile.document[root_key].rec_update(nest_dict(
+                        recdict[root_key].pop(table_name)
+                        recdict[root_key].rec_update(nest_dict(
                             pandas_to_dict(pd_obj), [k]
                         ))
                         # make default plot (add entry in 'plots') for each
@@ -55,15 +54,15 @@ class MPFile(MPFileCore):
                             {'x': pd_obj.columns[0], 'table': table_name},
                             [mp_level01_titles[2], 'default_{}'.format(k)]
                         )
-                        if mp_level01_titles[2] in mpfile.document[root_key]:
-                            mpfile.document[root_key].rec_update(plots_dict)
+                        if mp_level01_titles[2] in recdict[root_key]:
+                            recdict[root_key].rec_update(plots_dict)
                         else:
                           kv = (
                             mp_level01_titles[2],
                             plots_dict[mp_level01_titles[2]]
                           )
-                          mpfile.document[root_key].insert_before(k, kv)
-        return mpfile
+                          recdict[root_key].insert_before(k, kv)
+        return MPFile.from_dict(recdict)
 
     def get_string(self):
         lines, scope = [], []
