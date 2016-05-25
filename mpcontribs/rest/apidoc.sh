aa53ad33d69210731fb619dbc56952fb867aae30
Patrick Huck <phuck@lbl.gov>
2016-05-25 11:34:59 -0700

    rest.apidoc: use gnu sed

--- mpcontribs/rest/apidoc.sh
+++ mpcontribs/rest/apidoc.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+
+sed_cmd=sed
+$sed_cmd --version > /dev/null 2>&1
+if [ $? == 1 ]; then sed_cmd=gsed; fi
+$sed_cmd --version > /dev/null 2>&1
+if [ $? == 1 ]; then
+  echo "gnu sed not found!"
+  exit 1
+fi
+echo "using $sed_cmd"
+
 apidoc -f "views.py" -f "_apidoc.py" -o static
+
 for name in locales/locale main; do
-  sed -e "s:\'\.\/:'/static/:g" -i '' static/${name}.js
+  $sed_cmd --in-place "s:'./:'/static/:g" static/${name}.js
 done
-sed -e 's:link href=":link href="/static/:g' -i '' static/index.html
-sed -e 's:script src=":script src="/static/:g' -i '' static/index.html
-sed -e \
+
+$sed_cmd --in-place 's:link href=":link href="/static/:g' static/index.html
+$sed_cmd --in-place 's:script src=":script src="/static/:g' static/index.html
+$sed_cmd --in-place \
   's:<script data-main="main.js" src="vendor/require.min.js"></script>:<script src="/static/vendor/require.min.js"></script><script src="/static/main.js"></script>:g' \
-  -i '' static/index.html
-sed -e '1,3d' -i '' static/utils/send_sample_request.js
-sed -e '1 i\
+  static/index.html
+
+$sed_cmd --in-place '1,3d' static/utils/send_sample_request.js
+$sed_cmd --in-place '1 i\
   define(["jquery", "/static/jquery_cookie.js" ], function($, Cookies) {\
-' -i '' static/utils/send_sample_request.js
-sed -e '92 i\
+' static/utils/send_sample_request.js
+$sed_cmd -in-place '92 i\
   var csrftoken = Cookies.get("csrftoken");\
   function csrfSafeMethod(method) {\
     // these HTTP methods do not require CSRF protection\
@@ -25,4 +39,4 @@ sed -e '92 i\
       }\
     }\
   });\
-' -i '' static/utils/send_sample_request.js
+' static/utils/send_sample_request.js
