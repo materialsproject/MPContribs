e8ef8f2d3897a44b17d5d6debc4fad2039da2ec1
Patrick Huck <phuck@lbl.gov>
2016-06-23 17:55:35 -0700

    builder: switch to notebook HTML export

--- mpcontribs/builder/__init__.py
+++ mpcontribs/builder/__init__.py
@@ -8,6 +8,8 @@ from mpcontribs.io.core.mpfile import MPFileCore
 from nbformat import v4 as nbf
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert.preprocessors.execute import CellExecutionError
+from nbconvert import HTMLExporter
+from bs4 import BeautifulSoup
 
 class MPContributionsBuilder():
     """build user contributions from `mpcontribs.contributions`"""
@@ -68,35 +70,49 @@ class MPContributionsBuilder():
                 if 'project' not in contrib else contrib['project']
 
         nb = nbf.new_notebook()
-        nb['cells'].append(nbf.new_code_cell(
-            "from mpcontribs.rest.rester import MPContribsRester"
-        ))
-        # NOTE need to get API_KEY from user when executing NB on server
-        nb['cells'].append(nbf.new_code_cell(
-            "with MPContribsRester() as mpr:\n"
-            "    mpfile = mpr.find_contribution('{}')\n"
-            "    mpid = mpfile.ids[0]"
-            .format(cid)
+        if isinstance(self.db, dict):
+            contrib.pop('_id')
+            contrib['content'].pop('cid')
+            nb['cells'].append(nbf.new_code_cell(
+                "from mpcontribs.io.core.mpfile import MPFileCore\n"
+                "from mpcontribs.io.core.recdict import RecursiveDict\n"
+                "mpfile = MPFileCore.from_contribution({})\n"
+                "mpid = '{}'"
+                .format(contrib, mp_cat_id)
+            ))
+        else:
+            nb['cells'].append(nbf.new_code_cell(
+                "from mpcontribs.rest.rester import MPContribsRester"
+            ))
+            # NOTE need to get API_KEY from user when executing NB on server
+            nb['cells'].append(nbf.new_code_cell(
+                "with MPContribsRester() as mpr:\n"
+                "    mpfile = mpr.find_contribution('{}')\n"
+                "    mpid = mpfile.ids[0]"
+                .format(cid)
+            ))
+        nb['cells'].append(nbf.new_markdown_cell(
+            "# Contribution #{} for {}".format(cid_short, mp_cat_id)
         ))
         nb['cells'].append(nbf.new_markdown_cell(
-            "# Contribution #{} for {}\n"
             "## Hierarchical Data"
-            .format(cid_short, mp_cat_id)
         ))
         nb['cells'].append(nbf.new_code_cell(
             "hdata = mpfile.hdata[mpid]\n"
             "hdata"
         ))
-        nb['cells'].append(nbf.new_markdown_cell("## Tabular Data"))
+        if mpfile.tdata[mp_cat_id]:
+            nb['cells'].append(nbf.new_markdown_cell("## Tabular Data"))
         for table_name, table in mpfile.tdata[mp_cat_id].iteritems():
             nb['cells'].append(nbf.new_markdown_cell(
                 "### {}".format(table_name)
             ))
             nb['cells'].append(nbf.new_code_cell(
-                "{} = mpfile.tdata[mpid]['{}'] # pandas.DataFrame\n"
+                "{} = mpfile.tdata[mpid]['{}']\n"
                 "{}".format(table_name, table_name, table_name)
             ))
-        nb['cells'].append(nbf.new_markdown_cell("## Graphical Data"))
+        if mpfile.gdata[mp_cat_id]:
+            nb['cells'].append(nbf.new_markdown_cell("## Graphical Data"))
         for plot_name, plot in mpfile.gdata[mp_cat_id].iteritems():
             nb['cells'].append(nbf.new_markdown_cell(
                 "### {}".format(plot_name)
@@ -111,24 +127,34 @@ class MPContributionsBuilder():
         try:
             out = ep.preprocess(nb, {'metadata': {'path': nbdir}})
         except CellExecutionError:
-            print 'Execution error in above cell!' # TODO logging
+            return 'Execution Error in Jupyter Cell!'
         finally:
-            nbpath = os.path.join(nbdir, 'test.ipynb')
-            with open(nbpath, mode='wt') as f:
-                nbformat.write(nb, f)
-
-        # update collection with tree and table data
-        #if isinstance(self.db, dict):
-        #    unflatten_dict(all_data)
-        #    self.curr_coll.rec_update(nest_dict(all_data, [mp_cat_id]))
-        #else:
-        #    self.curr_coll.update({'_id': mp_cat_id}, {'$set': all_data}, upsert=True)
-        #if isinstance(self.db, dict):
-        #    return [
-        #      mp_cat_id, project, cid_str,
-        #      self.curr_coll[mp_cat_id][project][cid_str]
-        #    ]
-        #else:
-        #    return '{}/{}/{}/{}'.format( # return URL for contribution page
-        #        ('materials' if is_mp_id else 'compositions'),
-        #        mp_cat_id, project, cid_str)
+            if isinstance(self.db, dict):
+                html_exporter = HTMLExporter()
+                html_exporter.template_file = 'basic'
+                (body, resources) = html_exporter.from_notebook_node(nb)
+                soup = BeautifulSoup(body, 'html.parser')
+                soup.div.extract() # remove first code cell (loads mpfile)
+                [t.extract() for t in soup.find_all('a', 'anchor-link')] # rm anchors
+                # mark cells with special name for toggling, and
+                # make element id's unique by appending cid
+                # NOTE every cell has only one tag with id
+                for idx, div in enumerate(soup.find_all('div', 'cell')[1:]):
+                    tag = div.find('h2', id=True)
+                    if tag is not None:
+                        tag['id'] = '-'.join([tag['id'], str(cid)])
+                        div_name = tag['id'].split('-')[0]
+                    div['name'] = div_name
+                # name divs for toggling code_cells
+                for div in soup.find_all('div', 'input'):
+                    div['name'] = 'Input'
+                return [mp_cat_id, project, cid_short, soup.prettify()]
+            else:
+                build_doc = RecursiveDict()
+                build_doc['mp_cat_id'] = mp_cat_id
+                build_doc['project'] = project
+                build_doc['nb'] = nb
+                self.curr_coll.update({'_id': cid}, {'$set': build_doc}, upsert=True)
+                return '{}/{}/{}/{}'.format( # return URL for contribution page
+                    ('materials' if is_mp_id else 'compositions'),
+                    mp_cat_id, project, cid_str)
