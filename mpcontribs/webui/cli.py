b0cd0486d3860c3be982d1e747e25d1dfdbc55c7
Patrick Huck <phuck@lbl.gov>
2016-07-27 15:54:07 -0700

    webui.cli: implement CLI and add sbx option

--- mpcontribs/webui/cli.py
+++ mpcontribs/webui/cli.py
@@ -1,5 +1,5 @@
 # http://flask.pocoo.org/docs/0.10/patterns/appdispatch/
-import os
+import os, argparse
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware, SharedDataMiddleware
 from mpcontribs.webui.webui import app as flask_app
@@ -8,18 +8,29 @@ from test_site.settings import STATIC_ROOT
 from django.core.management import call_command
 from subprocess import call
 
-cwd = os.getcwd()
-os.chdir(os.path.join('mpcontribs', 'rest'))
-call(['./apidoc.sh'])
-os.chdir(cwd)
+def cli():
+    parser = argparse.ArgumentParser(
+        description='Command Line Interface for MPContribs WebUI',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--sbx', help='ArchieML Sandbox Content')
+    args = parser.parse_args()
 
-flask_app.debug = True
-call_command('collectstatic', '--clear', '--noinput', '-v 0')
-print 'static files collected.'
+    cwd = os.getcwd()
+    os.chdir(os.path.join('mpcontribs', 'rest'))
+    call(['./apidoc.sh'])
+    os.chdir(cwd)
 
-application = DispatcherMiddleware(flask_app, { '/test_site': django_app })
-application = SharedDataMiddleware(application, { '/static': STATIC_ROOT })
+    flask_app.debug = True
+    flask_app.config['SANDBOX_CONTENT'] = args.sbx
+    call_command('collectstatic', '--clear', '--noinput', '-v 0')
+    print 'static files collected.'
 
-if __name__ == '__main__':
+    application = DispatcherMiddleware(flask_app, { '/test_site': django_app })
+    application = SharedDataMiddleware(application, { '/static': STATIC_ROOT })
+
     run_simple('localhost', 5000, application, use_reloader=True,
                use_debugger=True, use_evalex=True, threaded=True)
+
+if __name__ == '__main__':
+    cli()
