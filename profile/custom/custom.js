11a0d753188fa58daa928e9f31264b5a63fcfd1c
Patrick Huck <phuck@lbl.gov>
2016-08-10 22:39:05 -0700

    profile: fix custom.js and css/js symlinks

--- profile/custom/custom.js
+++ profile/custom/custom.js
@@ -1,1 +1,12 @@
-//require(['custom/js/json.human']);
+console.log('executing custom.js ...');
+requirejs.config({
+  baseUrl: '/custom/js/lib',
+  paths: {
+    backbone: "backbone-min",
+    plotly: "plotly.min",
+  }
+});
+require(['json.human']);
+require(['backbone']);
+require(['plotly']);
+console.log('DONE');
