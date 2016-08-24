bc01ac9d54734d723347cecce10d4a826c859d03
Patrick Huck <phuck@lbl.gov>
2016-08-24 15:46:00 -0700

    setup: add nbformat dependency

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'matplotlib', 'pymongo', 'pyyaml', 'ipython', 'cufflinks',
         'Django==1.8.5', 'archieml', 'django-browserid', 'sphinx', 'notebook',
         'ipywidgets', 'celery', 'tqdm', 'beautifulsoup4', 'whichcraft',
-        'unidecode', 'psutil'
+        'unidecode', 'psutil', 'nbformat'
     ],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive'],
