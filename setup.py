07f0ac593fbe187c93e6cc803ab0b5f3ca236ee7
Patrick Huck <phuck@lbl.gov>
2016-10-03 22:46:26 +0000

    update requirements

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'matplotlib', 'pymongo', 'pyyaml', 'ipython', 'cufflinks',
         'Django>=1.8.5,<1.9', 'archieml', 'django-browserid', 'sphinx', 'notebook',
         'ipywidgets', 'celery', 'tqdm', 'beautifulsoup4', 'whichcraft',
-        'unidecode', 'psutil', 'nbformat'
+        'unidecode', 'psutil', 'nbformat', 'xlrd'
     ],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive'],
