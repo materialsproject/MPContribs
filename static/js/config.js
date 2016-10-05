101eb975d8afd02699e3211d08914bc89306a4ba
Patrick Huck <phuck@lbl.gov>
2016-10-05 23:52:01 +0000

    use require for js

---
+++ static/js/config.js
@@ -0,0 +1,34 @@
+requirejs.config({
+    baseUrl: 'static/js',
+    paths: {
+        jquery: 'components/jquery/dist/jquery',
+        backbone: 'components/backbone/backbone',
+        backgrid: 'components/backgrid/lib/backgrid',
+        bootstrap: 'components/bootstrap/dist/js/bootstrap',
+        filestyle: 'components/bootstrap-filestyle/src/bootstrap-filestyle',
+        chosen: 'components/chosen/chosen.jquery',
+        toggle: 'components/bootstrap-toggle/js/bootstrap-toggle',
+        underscore: 'components/underscore/underscore',
+        lunr: 'components/lunr.js/lunr',
+        plotly: 'components/plotlyjs/plotly',
+        'backgrid-select-all': 'components/backgrid-select-all/backgrid-select-all',
+        'backgrid-filter': 'components/backgrid-filter/backgrid-filter',
+        'json-human': 'components/json-human/src/json.human'
+        //waitfor: 'jquery.waitFor',
+        //thebe: 'main-built',
+    },
+    shim: {
+        jquery: {exports: '$'},
+        backbone: {deps: ['underscore', 'jquery'], exports: 'Backbone'},
+        backgrid: {deps: ['backbone'], exports: 'Backgrid'},
+        bootstrap: {deps: ['jquery']},
+        filestyle: {deps: ['bootstrap']},
+        chosen: { deps: ['jquery', 'bootstrap']},
+        toggle: {deps: ['jquery', 'bootstrap']},
+        'backgrid-select-all': {deps: ['backgrid']},
+        'backgrid-filter': {deps: ['backgrid']}
+        //waitfor: {deps: ['jquery']},
+        //sandbox: {deps: ['archieml']},
+    }
+});
+
