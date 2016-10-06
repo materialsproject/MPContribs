53079c0c2798336690ac8a26a8a4c75d721427a0
Patrick Huck <phuck@lbl.gov>
2016-10-06 19:42:47 +0000

    mv dashboard js to config

--- static/js/config.js
+++ static/js/config.js
@@ -34,3 +34,33 @@ requirejs.config({
     }
 });
 
+requirejs(['jquery', 'underscore'], function() {
+    $(document).ready(function () {
+        require(['js-cookie'], function(Cookies) {
+            function csrfSafeMethod(method) {
+                // these HTTP methods do not require CSRF protection
+                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+            }
+            $.ajaxSetup({
+                beforeSend: function(xhr, settings) {
+                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                        var csrftoken = Cookies.get('csrftoken');
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    }
+                }
+            });
+            $("#keygen").click(function(){
+                chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+                key = _.sample(chars, 16).join("");
+                var saveData = $.ajax({
+                    type: 'POST',
+                    url: $('#dashboard_url').val(),
+                    data: {'apikey': key},
+                    dataType: "text",
+                    success: function(data, textStatus, jqXHR) { $("#key").text(key); },
+                    error: function(jqXHR, textStatus, errorThrown) { $("#key").text('ERROR'); }
+                });
+            });
+        });
+    });
+});
