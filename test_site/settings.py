4130a30af9eb9248a23296b35fc997bdff3d678d
Patrick Huck <phuck@lbl.gov>
2016-10-18 01:54:36 +0000

    settings: add SECURE_PROXY_SSL_HEADER

--- test_site/settings.py
+++ test_site/settings.py
@@ -165,3 +165,5 @@ DEFAULT_FROM_EMAIL = 'Test Site <test_site@matgen8.lbl.gov>'
 
 REQUIRE_JS = 'components/requirejs/require.js'
 REQUIRE_DEBUG = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
