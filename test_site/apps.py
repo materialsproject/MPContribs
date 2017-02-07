48d757cea7fc312c8a11c067a9b35d7bb745de1e
Patrick Huck <tschaume@users.noreply.github.com>
2017-02-07 21:50:28 +0000

    add MnO2PhaseSelectionExplorer Django app

--- test_site/apps.py
+++ test_site/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class UWSI2ExplorerConfig(AppConfig):
     name = 'mpcontribs.users.uw_si2.explorer'
     label = 'uwsi2_explorer'
+
+class MnO2PhaseSelectionExplorerConfig(AppConfig):
+    name = 'mpcontribs.users.MnO2_phase_selection.explorer'
+    label = 'MnO2_phase_selection_explorer'
