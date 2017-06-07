5412b77d9f197bd72a099e6607180819f442e92b
Patrick Huck <tschaume@users.noreply.github.com>
2017-06-07 07:54:21 +0000

    uw_si2: fix rester

--- uw_si2/rest/rester.py
+++ uw_si2/rest/rester.py
@@ -4,6 +4,7 @@ from bson.json_util import dumps, loads
 from mpcontribs.rest.rester import MPContribsRester
 from mpcontribs.io.core.utils import get_short_object_id
 from mpcontribs.io.archieml.mpfile import MPFile
+from mpcontribs.config import mp_level01_titles
 from pandas import Series
 
 class UWSI2Rester(MPContribsRester):
@@ -15,14 +16,12 @@ class UWSI2Rester(MPContribsRester):
     def get_uwsi2_contributions(self):
         data = []
         for doc in self.query_contributions(
-            criteria={
-                'project': {'$in': ['LBNL', 'UW-Madison']},
-                'content.figshare_id': {'$exists': 1}
-            }, projection={'_id': 1, 'mp_cat_id': 1, 'content': 1}
+            criteria={'content.figshare_id': '1546772'},
+            projection={'_id': 1, 'mp_cat_id': 1, 'content': 1}
         ):
             mpfile = MPFile.from_contribution(doc)
             mp_id = mpfile.ids[0]
-            table = mpfile.tdata[mp_id]['data_D0_Q']
+            table = mpfile.tdata[mp_id][mp_level01_titles[1]+'_D0_Q']
             anums = [self.z[el] for el in table['element']]
             table.insert(0, 'Z', Series(anums, index=table.index))
             table.sort_values('Z', inplace=True)
