625beb7391318c070f09953680e034a023e8c8e3
Patrick Huck <tschaume@users.noreply.github.com>
2017-06-09 01:03:58 +0000

    mno2: add provenance to app

--- MnO2_phase_selection/explorer/views.py
+++ MnO2_phase_selection/explorer/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mpcontribs.rest.views import get_endpoint
 from mpcontribs.io.core.components import render_dataframe
+from mpcontribs.io.core.recdict import render_dict
 from ..rest.rester import MnO2PhaseSelectionRester
 
 def index(request):
@@ -14,7 +15,7 @@ def index(request):
         API_KEY = request.user.api_key
         ENDPOINT = request.build_absolute_uri(get_endpoint())
         with MnO2PhaseSelectionRester(API_KEY, endpoint=ENDPOINT) as mpr:
-            #provenance = mpr.get_provenance()
+            provenance = render_dict(mpr.get_provenance(), webapp=True)
             tables = {}
             for phase in mpr.get_phases():
                 df = mpr.get_contributions(phase=phase)
