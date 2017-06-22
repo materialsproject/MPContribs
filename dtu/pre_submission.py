79782205be399019d54a6b338e9e456bf706f008
Mackensie Smith <smithmackensie96@users.noreply.github.com>
2017-06-22 16:32:16 +0000

    data added for 10 mpids from dtu

--- dtu/pre_submission.py
+++ dtu/pre_submission.py
@@ -1,26 +1,29 @@
 import os
 import ase.db
-from collections import OrderedDict
+import collections as coll
 
 def run(mpfile):
     url = mpfile.hdata['_hdata']['url']
     # TODO
     # - extract db name from url -> dbfile = 'mp_gllbsc.db'
     # - check if mp_gllbsc.db exists
-    # - if not, download using python code instead of wget
-    dbfile = 'mp_gllbsc.db'
-    if os.path.exists(dbfile):
-        print 'file exists'
-    else:
-        print 'need to download'
-
+    dbfile = url.rsplit('/')[-1]
     con = ase.db.connect(dbfile)
+    count = 0
     for row in con.select('mpid'):
-        d = OrderedDict()
+        d = coll.OrderedDict([])
+        d['kohn-sham_bandgap'] = coll.OrderedDict([])
+        d['derivative_discontinuity'] = coll.OrderedDict([]) 
+        d['quasi-particle_bandgap'] = coll.OrderedDict([])
+        count = count + 1
         mpid = 'mp-' + str(row.mpid)
-        d['band_gap'] = 0.5
-        mpfile.add_hierarchical_data(mpid, d)
-        print 'added hierarchical data for', mpid
-        break
+        d['kohn-sham_bandgap']['indirect'] = row.gllbsc_ind_gap - row.gllbsc_disc
+        d['kohn-sham_bandgap']['direct'] = row.gllbsc_dir_gap - row.gllbsc_disc
+        d['derivative_discontinuity'] = row.gllbsc_disc
+        d['quasi-particle_bandgap']['indirect'] = row.gllbsc_ind_gap
+        d['quasi-particle_bandgap']['direct'] = row.gllbsc_dir_gap
+        mpfile.add_hierarchical_data(mpid,d) 
+        if count == 10:
+            break
     print mpfile
 
