03ea482eab54c6822d37e83d831fe35854468b86
Patrick Huck <tschaume@users.noreply.github.com>
2017-06-22 23:37:42 +0000

    dtu: start explorer

---
+++ dtu/explorer/views.py
@@ -0,0 +1,22 @@
+"""This module provides the views for the DTU explorer interface."""
+
+import json
+from bson import ObjectId
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from mpcontribs.rest.views import get_endpoint
+from mpcontribs.io.core.components import render_dataframe
+from mpcontribs.io.core.recdict import render_dict
+from ..rest.rester import DtuRester
+
+def index(request):
+    ctx = RequestContext(request)
+    if request.user.is_authenticated():
+        API_KEY = request.user.api_key
+        ENDPOINT = request.build_absolute_uri(get_endpoint())
+        with DtuRester(API_KEY, endpoint=ENDPOINT) as mpr:
+            provenance = render_dict(mpr.get_provenance(), webapp=True)
+            table = render_dataframe(mpr.get_contributions(), webapp=True)
+    else:
+        ctx.update({'alert': 'Please log in!'})
+    return render_to_response("dtu_explorer_index.html", locals(), ctx)
