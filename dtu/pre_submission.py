4096a4f9574b10378d72997d22d92d5be0846b52
Patrick Huck <tschaume@users.noreply.github.com>
2017-06-22 18:47:59 +0000

    dtu.pre_submission: minor improvs

--- dtu/pre_submission.py
+++ dtu/pre_submission.py
@@ -6,28 +6,24 @@ import urllib
 def run(mpfile):
     url = mpfile.hdata['_hdata']['url']
     dbfile = url.rsplit('/')[-1]
-    
-    if os.path.exists(dbfile):
-        pass
-    else:
+
+    if not os.path.exists(dbfile):
         data = urllib.URLopener()
-        data.retrieve(url,dbfile)
-        
+        data.retrieve(url, dbfile)
+
     con = ase.db.connect(dbfile)
-    count = 0
-    for row in con.select('mpid'):
+
+    for idx, row in enumerate(con.select('mpid')):
         d = coll.OrderedDict([])
         d['kohn-sham_bandgap'] = coll.OrderedDict([])
-        d['derivative_discontinuity'] = coll.OrderedDict([]) 
+        d['derivative_discontinuity'] = coll.OrderedDict([])
         d['quasi-particle_bandgap'] = coll.OrderedDict([])
-        count = count + 1
         mpid = 'mp-' + str(row.mpid)
         d['kohn-sham_bandgap']['indirect'] = row.gllbsc_ind_gap - row.gllbsc_disc
         d['kohn-sham_bandgap']['direct'] = row.gllbsc_dir_gap - row.gllbsc_disc
         d['derivative_discontinuity'] = row.gllbsc_disc
         d['quasi-particle_bandgap']['indirect'] = row.gllbsc_ind_gap
         d['quasi-particle_bandgap']['direct'] = row.gllbsc_dir_gap
-        mpfile.add_hierarchical_data(mpid,d) 
-        if count == 10:
+        mpfile.add_hierarchical_data(mpid,d)
+        if idx == 10:
             break
-   
