5b369d4e80e39f8fe94807cfc397b741dbfb40fa
Patrick Huck <tschaume@users.noreply.github.com>
2017-06-27 21:43:35 +0000

    settings: automate user modules for INSTALLED_APPS

--- test_site/apps.py
+++ test_site/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-class UWSI2ExplorerConfig(AppConfig):
+class UwSi2ExplorerConfig(AppConfig):
     name = 'mpcontribs.users.uw_si2.explorer'
     label = 'uwsi2_explorer'
 
-class MnO2PhaseSelectionExplorerConfig(AppConfig):
+class Mno2PhaseSelectionExplorerConfig(AppConfig):
     name = 'mpcontribs.users.MnO2_phase_selection.explorer'
     label = 'MnO2_phase_selection_explorer'
 
