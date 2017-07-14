1e34568d3c7d60033c67b9391ceee6479e9accb6
Patrick Huck <tschaume@users.noreply.github.com>
2017-07-14 18:43:04 +0000

    dtu: add cat picture as static example

--- dtu/explorer/views.py
+++ dtu/explorer/views.py
@@ -1,12 +1,13 @@
 """This module provides the views for the DTU explorer interface."""
 
-import json
+import json, os
 from bson import ObjectId
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mpcontribs.rest.views import get_endpoint
 from mpcontribs.io.core.components import render_dataframe
 from mpcontribs.io.core.recdict import render_dict
+from test_site.settings import STATIC_URL
 from ..rest.rester import DtuRester
 
 def index(request):
@@ -18,6 +19,8 @@ def index(request):
             try:
                 provenance = render_dict(mpr.get_provenance(), webapp=True)
                 table = render_dataframe(mpr.get_contributions(), webapp=True)
+                mod = os.path.dirname(__file__).split(os.sep)[-2]
+                static_url = '_'.join([STATIC_URL[:-1], mod])
             except Exception as ex:
                 ctx.update({'alert': str(ex)})
     else:
