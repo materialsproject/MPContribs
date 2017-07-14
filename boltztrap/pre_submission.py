7e75d1acebf28640999eb61fda8c52ff280fdefd
Patrick Huck <tschaume@users.noreply.github.com>
2017-07-14 16:46:52 +0000

    boltztrap: add rester

--- boltztrap/pre_submission.py
+++ boltztrap/pre_submission.py
@@ -1,15 +1,15 @@
 import os
-#from mpcontribs.users.dtu.rest.rester import DtuRester
+from mpcontribs.users.boltztrap.rest.rester import BoltztrapRester
 
 def run(mpfile, nmax=None, dup_check_test_site=True):
 
-    #existing_mpids = {}
-    #for b in [False, True]:
-    #    with DtuRester(test_site=b) as mpr:
-    #        for doc in mpr.query_contributions(criteria=mpr.dtu_query):
-    #            existing_mpids[doc['mp_cat_id']] = doc['_id']
-    #    if not dup_check_test_site:
-    #        break
+    existing_mpids = {}
+    for b in [False, True]:
+        with BoltztrapRester(test_site=b) as mpr:
+            for doc in mpr.query_contributions(criteria=mpr.boltztrap_query):
+                existing_mpids[doc['mp_cat_id']] = doc['_id']
+        if not dup_check_test_site:
+            break
 
     input_dir = mpfile.hdata.general['input_dir']
     print input_dir
