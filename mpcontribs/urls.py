636dc9ed7dc8a834f9c81b4f37d3f539455dced5
Patrick Huck <tschaume@users.noreply.github.com>
2017-07-17 21:53:24 +0000

    add swf

--- mpcontribs/urls.py
+++ mpcontribs/urls.py
@@ -4,7 +4,8 @@ from mpcontribs.users_modules import get_user_urlpatterns
 urlpatterns = [
     url(r'', include('mpcontribs.portal.urls')),
     url(r'^rest/', include('mpcontribs.rest.urls')),
-    url(r'^explorer/', include('mpcontribs.explorer.urls'))
+    url(r'^explorer/', include('mpcontribs.explorer.urls')),
+    url(r'^fe-co-v/dataset-01/', include('mpcontribs.users.swf.explorer.urls'))
 ] + [
     url(urlpattern[0], include(urlpattern[1]))
     for urlpattern in get_user_urlpatterns()
