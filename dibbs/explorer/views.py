2922929c784c46b4133b665d2c3eab4f33a93538
Patrick Huck <tschaume@users.noreply.github.com>
2017-07-19 05:58:28 +0000

    dibbs: add explorer

---
+++ dibbs/explorer/views.py
@@ -0,0 +1,26 @@
+"""This module provides the views for the Dibbs explorer interface."""
+
+import json, os
+from bson import ObjectId
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from mpcontribs.rest.views import get_endpoint
+from mpcontribs.io.core.components import render_dataframe, render_plot
+from mpcontribs.io.core.recdict import render_dict
+from test_site.settings import STATIC_URL
+from ..rest.rester import DibbsRester
+
+def index(request):
+    ctx = RequestContext(request)
+    if request.user.is_authenticated():
+        API_KEY = request.user.api_key
+        ENDPOINT = request.build_absolute_uri(get_endpoint())
+        with DibbsRester(API_KEY, endpoint=ENDPOINT) as mpr:
+            try:
+                title = mpr.get_provenance().get('title')
+                table = render_dataframe(mpr.get_contributions(), webapp=True)
+            except Exception as ex:
+                ctx.update({'alert': str(ex)})
+    else:
+        ctx.update({'alert': 'Please log in!'})
+    return render_to_response("dibbs_explorer_index.html", locals(), ctx)
