5ecefd6fd7426d585226734afdc21bac1eb4b322
Patrick Huck <tschaume@users.noreply.github.com>
2017-07-24 22:26:43 +0000

    swf: use google sheet, review

--- swf/pre_submission.py
+++ swf/pre_submission.py
@@ -1,75 +1,80 @@
+from mpcontribs.config import mp_level01_titles
 from mpcontribs.users.swf.rest.rester import SWFRester
 from mpcontribs.io.core.recdict import RecursiveDict
 import pandas as pd
 import pymatgen
 
+def round_to_100_percent(number_set, digit_after_decimal=1):
+    unround_numbers = [
+        x / float(sum(number_set)) * 100 * 10**digit_after_decimal
+        for x in number_set
+    ]
+    decimal_part_with_index = sorted([
+        (index, unround_numbers[index] % 1)
+        for index in range(len(unround_numbers))
+    ], key=lambda y: y[1], reverse=True)
+    remainder = 100 * 10**digit_after_decimal - sum(map(int, unround_numbers))
+    index = 0
+    while remainder > 0:
+        unround_numbers[decimal_part_with_index[index][0]] += 1
+        remainder -= 1
+        index = (index + 1) % len(number_set)
+    return [int(x)/float(10**digit_after_decimal) for x in unround_numbers]
+
 def run(mpfile, nmax=None):
-    df = pd.read_excel('~/work/MPContribs/mpcontribs/users/swf/swf_master_sheet.xlsx')
-    df1 = df.iloc[:,0:4]; df1 = df1.dropna(how='all')
-    df2 = df.iloc[:,5:8]
-    df3 = df.iloc[:,9:14]; df3 = df3.dropna(how='all'); 
-    df4 = df.iloc[:,15:20]; df4 = df4.dropna(how='all');
-    df5 = df.iloc[:,21:27]; df5 = df5.dropna(how='all');
 
-    df1 = df1.rename(index=str,columns={'IP Energy product (kJ/m3)': 'IP_Energy_product',
-                'Fe (at%)': 'Fe', 'V (at%)': 'V', 'Co (at%)': 'Co'})
-    df2 = df2.rename(index=str,columns={'Fe (tot)': 'Fe', 'V (tot)': 'V', 'Co (tot)': 'Co'})
-    df3 = df3.rename(index=str,columns={'Fe (6c)': 'Fe', 'V (6c)': 'V', 'Co (6c)': 'Co', 'thickness (nm) (6c)': 'thickness',
-                                       'MOKE IP Hc (Oe)': 'MOKE_IP_Hc'})
-    df4 = df4.rename(index=str,columns={'Fe (6d)': 'Fe', 'V (6d)': 'V', 'Co (6d)': 'Co', 'thickness (nm) (6d)': 'thickness',
-                                       'VSM IP Hc (Oe)': 'VSM_IP_Hc'})
-    df5 = df5.rename(index=str,columns={'Fe (5a)': 'Fe', 'V (5a)': 'V', 'Co (5a)': 'Co', 'angle': 'angle',
-                                       'Kondorsky Model (T)': 'Kondorsky_Model', 'Experiment (T)' : 'Experiment'})
-    df_all = [df1, df2, df3, df4, df5]
+    # load data from google sheet
+    # TODO should google sheets URL be removed from contribution?
+    google_sheet = mpfile.document[mp_level01_titles[0]].pop('google_sheet')
+    google_sheet += '/export?format=xlsx'
+    df_dct = pd.read_excel(google_sheet, sheetname=None)
 
-    def round_to_100_percent(number_set, digit_after_decimal=1):
-        unround_numbers = [
-            x / float(sum(number_set)) * 100 * 10**digit_after_decimal
-            for x in number_set
-        ]
-        decimal_part_with_index = sorted([
-            (index, unround_numbers[index] % 1)
-            for index in range(len(unround_numbers))
-        ], key=lambda y: y[1], reverse=True)
-        remainder = 100 * 10**digit_after_decimal - sum(map(int, unround_numbers))
-        index = 0
-        while remainder > 0:
-            unround_numbers[decimal_part_with_index[index][0]] += 1
-            remainder -= 1
-            index = (index + 1) % len(number_set)
-        return [int(x)/float(10**digit_after_decimal) for x in unround_numbers]
+    # rename sheet columns
+    elements = ['Fe', 'V', 'Co']
+    df_dct['IP Energy Product'].columns = ['IP_Energy_product'] + elements
+    df_dct['total'].columns = elements
+    df_dct['MOKE'].columns = elements + ['thickness', 'MOKE_IP_Hc']
+    df_dct['VSM'].columns = elements + ['thickness', 'VSM_IP_Hc']
+    df_dct['formula'].columns = elements
+    df_dct['Kondorsky'].columns = ['angle', 'Kondorsky_Model', 'Experiment']
 
-    for i in range(len(df_all)):
-        for index, row in df_all[i].iterrows():
-            if pd.isnull(row['Fe']) == True:
-                break
-            row_replace = round_to_100_percent([row['Fe'], row['Co'], row['V']])
-            row['Fe'] = row_replace[0]
-            row['Co'] = row_replace[1]
-            row['V'] = row_replace[2]
-    
-    comp5 = pymatgen.Composition(10*df5.iloc[0,0:3])
-    formula5 = str(comp5).replace(' ', '')
-    row5 = df5.iloc[0,0:3]; d5 = row5.to_dict(); 
-    mpfile.add_hierarchical_data(d5, identifier=formula5)
-    mpfile.add_data_table(formula5, df5.iloc[:,3:6], name='Angular Dependence of Switching Field')
+    # round all compositions to 100%
+    for sheet, df in df_dct.items():
+        if sheet != 'Kondorsky':
+            for idx, row in df.iterrows():
+                df.loc[idx:idx, elements] = round_to_100_percent(row[elements])
 
-    idx = 0
-    df_allH = [df1, df2, df3, df4]
-    for i in range(len(df_allH)):
-        d = RecursiveDict()
-        for index, row in df_allH[i].iterrows():
-            d['Fe'] = row['Fe']*10
-            d['V'] = row['V']*10
-            d['Co'] = row['Co']*10
-            comp = pymatgen.Composition(d)
-            formula = str(comp).replace(' ', '')
-            p = RecursiveDict()
-            row_round = row.round(decimals=1)
-            p = row_round.to_dict()
-            mpfile.add_hierarchical_data(p, identifier=formula)
-            if nmax is not None and idx >= nmax-1:
+    row5 = df_dct['formula'].iloc[0]
+    formula5 = pymatgen.Composition(10*row5).formula.replace(' ', '')
+    mpfile.add_hierarchical_data(
+        {'data': row5.to_dict()}, identifier=formula5
+    )
+    mpfile.add_data_table(
+        formula5, df_dct['Kondorsky'], name='Angular Dependence of Switching Field'
+    )
+
+    count = 0
+    for sheet, df in df_dct.items():
+        if sheet == 'formula' or sheet == 'Kondorsky':
+            continue
+        for idx, row in df.iterrows():
+            composition = pymatgen.Composition(row[elements]*10)
+            formula = composition.formula.replace(' ', '')
+
+            mpfile.add_hierarchical_data(
+                {'data': row[elements].to_dict()},
+                identifier=formula
+            )
+
+            columns = [x for x in row.index if x not in elements]
+            if columns:
+                data = row[columns].round(decimals=1)
+                mpfile.add_hierarchical_data(
+                    {'data': data.to_dict()}, identifier=formula
+                )
+
+            if nmax is not None and count >= nmax-1:
                     break
-            idx += 1
+            count += 1
 
-    print(mpfile)
+    print len(mpfile.ids), 'mp-ids to submit.'
--- swf/rest/rester.py
+++ swf/rest/rester.py
@@ -9,50 +9,53 @@ class SWFRester(MPContribsRester):
     mpfile = MPFile.from_file(os.path.join(
         os.path.dirname(__file__), '..', 'mpfile_init.txt'
     ))
-    swf_query = tuple({'content.doi': mpfile.hdata.general['doi']}.iteritems())
+    query = {'content.doi': mpfile.hdata.general['doi']}
+
+    def query_contributions(self, **kwargs):
+        if 'criteria' in kwargs:
+            kwargs['criteria'].update(self.query)
+        else:
+            kwargs['criteria'] = self.query
+        return super(SWFRester, self).query_contributions(**kwargs)
+
+    def delete_contributions(self, cids=[]):
+        if not cids:
+            cids = [c['_id'] for c in self.query_contributions()]
+        return super(SWFRester, self).delete_contributions(cids)
 
     def get_contributions(self):
-        data = []
-        columns = [
-            'formula', 'contribution',
-            'Fe', 'Co', 'V',
-            'IP_Energy_product','thickness','MOKE_IP_Hc','VSM_IP_Hc'
-        ]
-
         docs = self.query_contributions(
-            criteria=self.swf_query,
-            projection={
-                '_id': 1, 'mp_cat_id': 1,
-                'content.Fe': 1,
-                'content.Co': 1,
-                'content.V': 1,
-                'content.IP_Energy_product': 1,
-                'content.thickness': 1,
-                'content.MOKE_IP_Hc': 1,
-                'content.VSM_IP_Hc': 1}
+            projection={'_id': 1, 'mp_cat_id': 1, 'content.data': 1}
         )
         if not docs:
             raise Exception('No contributions found for SWF Explorer!')
 
+        data = []
+        columns = ['formula', 'contribution']
+
         for doc in docs:
             mpfile = MPFile.from_contribution(doc)
             formula = mpfile.ids[0]
-            contrib = mpfile.hdata[formula]
-            tables = mpfile.tdata.get(formula)
-            cid_url = '/'.join([
-                self.preamble.rsplit('/', 1)[0], 'explorer', 'compositions', doc['_id']
-            ])
-            row = [
-                formula, cid_url, tables,
-                contrib['Fe'],
-                contrib['Co'],
-                contrib['V'],
-                contrib['IP_Energy_product'],
-                contrib['thickness'],
-                contrib['MOKE_IP_Hc'],
-                contrib['VSM_IP_Hc']
-            ]
-            data.append((formula, row))
+            contrib = mpfile.hdata[formula]['data']
+            cid_url = self.get_cid_url(doc)
+
+            for k in contrib.keys():
+                if k not in columns:
+                    columns.append(k)
+
+            row = [formula, cid_url]
+            for col in columns[2:]:
+                row.append(contrib.get(col, ''))
+
+            data.append([formula, row])
+
+        # enforce equal row lengths
+        ncols = len(columns)
+        for entry in data:
+            n = len(entry[1])
+            if n != ncols:
+                entry[1] += [''] * (ncols - n)
+
         return DataFrame.from_items(data, orient='index', columns=columns)
 
 
@@ -61,13 +64,11 @@ class SWFRester(MPContribsRester):
         projection = {'_id': 1, 'mp_cat_id': 1}
         for key in provenance_keys:
             projection['content.' + key] = 1
-        docs = self.query_contributions(criteria=self.swf_query, projection=projection)
+        docs = self.query_contributions(projection=projection)
         if not docs:
             raise Exception('No contributions found for SWF Explorer!')
 
         mpfile = MPFile.from_contribution(docs[0])
         formula = mpfile.ids[0]
         return mpfile.hdata[formula]
-
-
 
