6e3da68ba3f6df010480d98891eedbc4dbaff5d3
Patrick Huck <tschaume@users.noreply.github.com>
2017-08-08 19:41:32 +0000

    register on first MPContribs click

--- mpcontribs/webui/main.py
+++ mpcontribs/webui/main.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals, print_function, absolute_import
 
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 stat_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
@@ -9,4 +9,14 @@ main_bp = Blueprint('webui_main', __name__, template_folder=tmpl_dir, static_fol
 
 @main_bp.route('/')
 def home():
-    return render_template('main.html')
+    from mpcontribs import MPContribsRester
+    try:
+        with MPContribsRester(test_site=True) as mpr:
+            return render_template('main.html')
+    except ValueError:
+        jpy_user = os.environ.get('JPY_USER')
+        if not jpy_user:
+            raise ValueError('Cannot connect to test_site outside MP JupyterHub!')
+        flaskproxy = 'https://jupyterhub.materialsproject.org/flaskproxy'
+        login_url = '/'.join([flaskproxy, jpy_user, 'test_site/login'])
+        return redirect(login_url)
