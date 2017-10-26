9126fa0afef42a8c7ecd12d1f0a882da97974fa9
Patrick Huck <tschaume@users.noreply.github.com>
2017-10-26 19:18:32 +0000

    revert root-level imports

--- mpcontribs/__init__.py
+++ mpcontribs/__init__.py
@@ -7,7 +7,3 @@ __email__ = 'phuck@lbl.gov'
 __copyright__ = 'Copyright 2014-2015, The Materials Project'
 __maintainer__ = 'Patrick Huck'
 
-from mpcontribs.io.archieml.mpfile import MPFile
-from mpcontribs.rest.rester import MPContribsRester
-from mpcontribs.io.core.recdict import RecursiveDict
-from mpcontribs.config import mp_level01_titles
