a40500c41cf0278ae65f101fbb911b6b6f751f5a
Patrick Huck <tschaume@users.noreply.github.com>
2017-10-27 17:46:25 +0000

    jarvis_dft: add explorer

---
+++ jarvis_dft/explorer/views.py
@@ -0,0 +1,29 @@
+"""This module provides the views for the jarvis_dft explorer interface."""
+
+import os
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from mpcontribs.rest.views import get_endpoint
+from mpcontribs.io.core.components import render_dataframe, render_plot
+from mpcontribs.io.core.recdict import render_dict
+from ..rest.rester import JarvisDftRester
+
+def index(request):
+    ctx = RequestContext(request)
+    if request.user.is_authenticated():
+        API_KEY = request.user.api_key
+        ENDPOINT = request.build_absolute_uri(get_endpoint())
+        with JarvisDftRester(API_KEY, endpoint=ENDPOINT) as mpr:
+            try:
+                prov = mpr.get_provenance()
+                title = prov.get('title')
+                provenance = render_dict(prov, webapp=True)
+                tables = {}
+                for typ in ['2d', '3d']:
+                    df = mpr.get_contributions(typ)
+                    tables[typ] = render_dataframe(df, webapp=True)
+            except Exception as ex:
+                ctx.update({'alert': str(ex)})
+    else:
+        ctx.update({'alert': 'Please log in!'})
+    return render_to_response("jarvis_dft_explorer_index.html", locals(), ctx)
