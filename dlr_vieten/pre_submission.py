2d6e61cf316d3817002aad2d741e94cbf2fce9d9
Patrick Huck <tschaume@users.noreply.github.com>
2018-02-17 01:19:45 +0000

    dlr: add fit params to hdata

--- dlr_vieten/pre_submission.py
+++ dlr_vieten/pre_submission.py
@@ -9,6 +9,39 @@ from mpcontribs.io.core.utils import clean_value, read_csv, nest_dict
 from mpcontribs.io.core.components import Table
 from mpcontribs.users.utils import duplicate_check
 
+def get_fit_pars(sample_number):
+    from .solar_perovskite.modelling.isographs import Experimental
+    from .solar_perovskite.init.import_data import Importdata
+    d = RecursiveDict()
+    exp = Experimental(sample_number)
+    fitparam = exp.get_fit_parameters()
+    # fitparam = compstr, delta_0, tolfac, mol_mass, fit_param_enth,
+    #            fit_type_entr, fit_param_entr, delta_min, delta_max
+    fit_par_ent = [fitparam[6][0], fitparam[6][1], fitparam[1]]
+    d['fit_par_ent'] = RecursiveDict(
+        (k, clean_value(v))
+        for k, v in zip('abc', fit_par_ent)
+    )
+    d['fit_param_enth'] = RecursiveDict(
+        (k, clean_value(v))
+        for k, v in zip('abcd', fitparam[4])
+    )
+    d['fit_type_entr'] = clean_value(fitparam[5])
+    d['delta_0'] = clean_value(fitparam[1])
+    d['delta_min'] = clean_value(fitparam[7])
+    d['delta_max'] = clean_value(fitparam[8])
+    fit_param_fe = pd.np.loadtxt(os.path.abspath(os.path.join(
+        os.path.dirname(__file__), 'solar_perovskite', "datafiles", "entropy_fitparam_SrFeOx"
+    )))
+    d['fit_param_fe'] = RecursiveDict(
+        (k, clean_value(v))
+        for k,v in zip('abcd', fit_param_fe)
+    )
+    imp = Importdata()
+    act_mat = imp.find_active(sample_no=sample_number)
+    d['act_mat'] = {act_mat[0]: clean_value(act_mat[1])}
+    return d
+
 def get_table(results, letter):
     y = 'Δ{}'.format(letter)
     df = Table(RecursiveDict([
@@ -44,6 +77,7 @@ def run(mpfile, **kwargs):
             continue
         print identifier
 
+        print 'add hdata ...'
         d = RecursiveDict()
         d['tolerance_factor'] = row['tolerance_factor']
         d['solid_solution'] = row['type of solid solution']
@@ -53,15 +87,12 @@ def run(mpfile, **kwargs):
         d['reduced_phase'] = RecursiveDict()
         d['reduced_phase']['composition'] = row['composition reduced phase']
         d['reduced_phase']['closest_MP'] = row['closest phase MP (reduced)'].replace('n.a.', '')
-        #d['Reference'] = row['Reference']
-        d['sample_number'] = row['sample_number']
-
-        mpfile.add_hierarchical_data(
-            nest_dict(d, ['data']), identifier=identifier
-        )
+        d = nest_dict(d, ['data'])
+        sample_number = int(row['sample_number'])
+        d['pars'] = get_fit_pars(sample_number)
+        mpfile.add_hierarchical_data(d, identifier=identifier)
 
         print 'add ΔH ...'
-        sample_number = int(row['sample_number'])
         exp_thermo = GetExpThermo(sample_number, plotting=False)
         enthalpy = exp_thermo.exp_dh()
         table = get_table(enthalpy, 'H')
