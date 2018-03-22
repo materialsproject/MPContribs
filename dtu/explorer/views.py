c19d3045e9b26599d59b38978b5f297aee4fdb05
Patrick Huck <tschaume@users.noreply.github.com>
2018-03-22 23:55:31 +0000

    dtu: filter contributions

--- dtu/explorer/views.py
+++ dtu/explorer/views.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import json, nbformat
-from bson import ObjectId
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mpcontribs.rest.views import get_endpoint
 from mpcontribs.io.core.components import render_dataframe
 from mpcontribs.io.core.recdict import render_dict
-import collections as coll
-
-
 
 def index(request):
     ctx = RequestContext(request)
@@ -19,39 +13,28 @@ def index(request):
         ENDPOINT = request.build_absolute_uri(get_endpoint())
         from ..rest.rester import DtuRester
         with DtuRester(API_KEY, endpoint=ENDPOINT) as mpr:
-
-            if request.method == 'GET':
-
-                try:
-                    prov = mpr.get_provenance()
-                    title = prov.pop('title')
-                    provenance = render_dict(prov, webapp=True)
-                    table = render_dataframe(mpr.get_contributions(), webapp=True)
-                except Exception as ex:
-                    ctx.update({'alert': str(ex)})
-
-            elif request.method == 'POST':
-                ids = ['C_range', 'KS_ID_range', 'KS_D_range', 'QP_ID_range', 'QP_D_range']
-
-                d = coll.OrderedDict([])
-                d['C'] = coll.OrderedDict([])
-                d['ΔE-KS'] = coll.OrderedDict([])
-                d['ΔE-QP'] = coll.OrderedDict([])
-                d['C'] = str(request.POST['C_range'])
-                d['ΔE-KS']['indirect'] = str(request.POST['KS_ID_range'])
-                d['ΔE-KS']['direct'] = str(request.POST['KS_D_range'])
-                d['ΔE-QP']['indirect'] = str(request.POST['QP_ID_range'])
-                d['ΔE-QP']['direct'] = str(request.POST['QP_D_range'])
-                
-                try:
-                    prov = mpr.get_provenance()
-                    title = prov.pop('title')
-                    provenance = render_dict(prov, webapp=True)
-                    table = render_dataframe(mpr.get_contributions(bandgap_range = d), webapp=True)
-                except Exception as ex:
-                    ctx.update({'alert': str(ex)})
-
-
+            try:
+                prov = mpr.get_provenance()
+                ctx['title'] = prov.pop('title')
+                ctx['provenance'] = render_dict(prov, webapp=True)
+                ctx['filters'] = {}
+                filters = ['C']
+                keys, subkeys = ['ΔE-KS', 'ΔE-QP'], ['indirect', 'direct']
+                filters += ['_'.join([k, sk]) for k in keys for sk in subkeys]
+                if request.method == 'POST':
+                    ctx['filters'] = dict(
+                        (f, map(float, request.POST['{}_slider'.format(f)].split(',')))
+                        for f in filters
+                    )
+                df = mpr.get_contributions(bandgap_range=ctx['filters'])
+                if request.method == 'GET':
+                    for f in filters:
+                        values = [float(v.split()[0]) for i,v in df[f.replace('_', '##')].iteritems()]
+                        ctx['filters'][f] = [min(values), max(values)]
+                ctx['nresults'] = df.shape[0]
+                ctx['table'] = render_dataframe(df, webapp=True)
+            except Exception as ex:
+                ctx['alert'] = str(ex)
     else:
         ctx.update({'alert': 'Please log in!'})
-    return render_to_response("dtu_explorer_index.html", locals(), ctx)
+    return render_to_response("dtu_explorer_index.html", ctx)
--- dtu/rest/rester.py
+++ dtu/rest/rester.py
@@ -3,8 +3,6 @@ from __future__ import division, unicode_literals
 from mpcontribs.rest.rester import MPContribsRester
 from mpcontribs.io.archieml.mpfile import MPFile
 from mpcontribs.io.core.components import Table
-import decimal
-D = decimal.Decimal
 
 class DtuRester(MPContribsRester):
     """DTU-specific convenience functions to interact with MPContribs REST interface"""
@@ -13,9 +11,12 @@ class DtuRester(MPContribsRester):
     released = True
 
     # TODO implement decorator to reduce this to column definitions and rows
-    def get_contributions(self, bandgap_range= None):
+    def get_contributions(self, bandgap_range=None):
 
-        projection = {'_id': 1, 'mp_cat_id': 1, 'content': 1}
+        projection = {
+            '_id': 1, 'mp_cat_id': 1,
+            'content.formula': 1, 'content.ICSD': 1, 'content.data': 1
+        }
         docs = self.query_contributions(projection=projection)
         if not docs:
             raise Exception('No contributions found for DTU Explorer!')
@@ -33,18 +34,17 @@ class DtuRester(MPContribsRester):
             row = [mp_id, cid_url, contrib['formula'], contrib['ICSD'], contrib['data']['C']]
             row += [contrib['data'][k][sk] for k in keys for sk in subkeys]
             if bandgap_range:
-                for k1, v1 in bandgap_range.iteritems():
-                    if isinstance(v1, dict):
-                        for k2, v2 in v1.iteritems():
-                            dec = D(contrib['data'][k1][k2][:-3])
-                            dec = float(dec)
-                            if dec >= float(v2[0]) and dec <= float(v2[2]):
-                                data.append((mp_id, row))   
-                    else:
-                        dec = D(contrib['data'][k1][:-3])
-                        dec = float(dec)
-                        if dec >= float(v1[0]) and dec <= float(v1[2]):
-                            data.append((mp_id, row))
+                in_filter = True
+                for k, v in bandgap_range.iteritems():
+                    ks = k.split('_')
+                    val = contrib['data'][ks[0]][ks[1]] if len(ks) > 1 else contrib['data'][k]
+                    dec = float(val.split()[0])
+                    if dec < v[0] or dec > v[1]:
+                        in_filter = False
+                        break
+                if in_filter:
+                    data.append((mp_id, row))
             else:
                 data.append((mp_id, row))
+
         return Table.from_items(data, orient='index', columns=columns)
