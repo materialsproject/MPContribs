16434dda04b92e9e2ffd00d39d93fc8fc309b9f4
Patrick Huck <tschaume@users.noreply.github.com>
2018-03-29 00:09:18 +0000

    boltztrap: add graph on row click

--- boltztrap/rest/views.py
+++ boltztrap/rest/views.py
@@ -4,12 +4,20 @@ import json
 from webtzite import mapi_func
 from webtzite.connector import ConnectorBase
 from mpcontribs.rest.views import Connector
+from mpcontribs.io.core.components import Table
 ConnectorBase.register(Connector)
 
 @mapi_func(supported_methods=["POST", "GET"], requires_api_key=False)
 def index(request, cid, db_type=None, mdb=None):
     try:
-        response = 'hello'
+        doc = mdb.contrib_ad.query_contributions(
+            {'_id': cid}, projection={'_id': 0, 'content.S(p)': 1}
+        )[0]
+        table = Table.from_dict(doc['content']['S(p)'])
+        x = [col.split()[0] for col in table.columns[1:]]
+        y = list(table[table.columns[0]])
+        z = table[table.columns[1:]].values.tolist()
+        response = {'x': x, 'y': y, 'z': z, 'type': 'heatmap'}
     except Exception as ex:
         raise ValueError('"REST Error: "{}"'.format(str(ex)))
     return {"valid_response": True, 'response': response}
