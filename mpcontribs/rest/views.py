f46569f8eea432571a9026682d70c9e7db3f55ea
Patrick Huck <tschaume@users.noreply.github.com>
2018-04-02 05:56:57 +0000

    get_card: tooltip w/o bootstrap

--- mpcontribs/rest/views.py
+++ mpcontribs/rest/views.py
@@ -477,10 +477,8 @@ def get_card(request, cid, db_type=None, mdb=None):
 
     provenance = '<h5 style="margin: 5px;">{}'.format(authors[0])
     if len(authors) > 1:
-        provenance += '''<button class="btn btn-sm btn-link"
-        data-toggle="tooltip" data-placement="bottom" data-html="true"
-        data-container="body" title="{}" style="padding: 0px 0px 0px 3px;"
-        >et al.</a>'''.format(authors[1].strip().replace(', ', '<br/>'))
+        authors = authors[1].strip().replace(', ', '<br/>')
+        provenance += ' <a class="mytooltip" href="#">et al.<span class="classic">' + authors + '</span></a>'
     provenance += '</h5>'
     urls = hdata.get('urls', {}).values()
     provenance += ''.join(['''<a href={}
@@ -529,6 +527,44 @@ def get_card(request, cid, db_type=None, mdb=None):
         landing_page = reverse('mpcontribs_users_{}_explorer_index'.format(project))
 
     card = '''
+    <style>
+    .mytooltip {{
+          border-bottom: 1px dotted #000000;
+          color: #000000; outline: none;
+          cursor: help; text-decoration: none;
+          position: relative;
+    }}
+    .mytooltip span {{
+          margin-left: -999em;
+          position: absolute;
+    }}
+    .mytooltip:hover span {{
+      font-family: Calibri, Tahoma, Geneva, sans-serif;
+      position: absolute;
+      left: 1em;
+      top: 2em;
+      z-index: 99;
+      margin-left: 0;
+      width: 100px;
+    }}
+    .mytooltip:hover img {{
+      border: 0;
+      margin: -10px 0 0 -55px;
+      float: left;
+      position: absolute;
+    }}
+    .mytooltip:hover em {{
+      font-family: Candara, Tahoma, Geneva, sans-serif;
+      font-size: 1.2em;
+      font-weight: bold;
+      display: block;
+      padding: 0.2em 0 0.6em 0;
+    }}
+    .classic {{ padding: 0.8em 1em; }}
+    .custom {{ padding: 0.5em 0.8em 0.8em 2em; }}
+    * html a:hover {{ background: transparent; }}
+    .classic {{ background: #000000; color: #FFFFFF; }}
+    </style>
     <div class="panel panel-default">
         <div class="panel-heading">
             <h4 class="panel-title">
