b09342a0c0d6e8604c58e4aeeaa83825af96de5e
Patrick Huck <tschaume@users.noreply.github.com>
2018-04-03 02:30:32 +0000

    add linkify js

--- notebooks/profile/custom/custom.js
+++ notebooks/profile/custom/custom.js
@@ -5,13 +5,16 @@ requirejs.config({
     underscore: 'underscore-min',
     backbone: "backbone-min",
     plotly: "plotly.min",
-    "backgrid-paginator": "backgrid.paginator"
+    "backgrid-paginator": "backgrid.paginator",
+    "linkify": "linkify.amd",
+    "linkify-element": "linkify-element.amd"
   },
   shim: {
     underscore: {exports: '_'},
     "backgrid-paginator": { deps: ['backgrid'] },
     "backgrid-filter": { deps: ['backgrid'] },
     "backgrid-grouped-columns": { deps: ['backgrid', 'backgrid-patch'] },
+    'linkify-element': {deps: ['linkify']},
   }
 });
 require(['json.human']);
@@ -20,4 +23,5 @@ require([
     'backgrid-filter', 'backgrid-grouped-columns'
 ]);
 require(['plotly']);
+require(['linkify-element']);
 console.log('DONE');
