1b42af2187f163aeda318b7a24d608e51ba4de06
Mackensie Smith <smithmackensie96@users.noreply.github.com>
2018-05-01 17:48:11 +0000

    jarvis compare pull request

--- jarvis_dft/explorer/views.py
+++ jarvis_dft/explorer/views.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 """This module provides the views for the jarvis_dft explorer interface."""
 
-import os
+import json, os
+from bson import ObjectId
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from mpcontribs.rest.views import get_endpoint
-from mpcontribs.io.core.components import render_dataframe
+from mpcontribs.io.core.components import render_dataframe, render_plot
 from mpcontribs.io.core.recdict import render_dict
+
 
 def index(request):
     ctx = RequestContext(request)
@@ -16,14 +19,11 @@ def index(request):
         with JarvisDftRester(API_KEY, endpoint=ENDPOINT) as mpr:
             try:
                 prov = mpr.get_provenance()
-                title = prov.get('title')
-                provenance = render_dict(prov, webapp=True)
-                tables = {}
-                for typ in ['2d']:
-                    df = mpr.get_contributions(typ)
-                    tables[typ] = render_dataframe(df, webapp=True)
+                ctx['title'] = prov.pop('title')
+                ctx['provenance'] = render_dict(prov, webapp=True)
+                ctx['table'] = render_dataframe(mpr.get_contributions(), webapp=True)
             except Exception as ex:
                 ctx.update({'alert': str(ex)})
     else:
         ctx.update({'alert': 'Please log in!'})
-    return render_to_response("jarvis_dft_explorer_index.html", locals(), ctx)
+    return render_to_response("jarvis_dft_explorer_index.html", ctx)
