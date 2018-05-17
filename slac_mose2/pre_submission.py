1a651a7cb62207c078822198d1db1a41edbad7e8
Patrick Huck <tschaume@users.noreply.github.com>
2018-05-17 00:03:57 +0000

    slac_mose2: minor mpfile_init, pre-sub and template

--- slac_mose2/pre_submission.py
+++ slac_mose2/pre_submission.py
@@ -7,12 +7,11 @@ from mpcontribs.users.utils import duplicate_check
 
 @duplicate_check
 def run(mpfile, **kwargs):
-    meta_data = mpfile.hdata[mp_level01_titles[0]]
-    csv_path = meta_data['csv_path']
+    meta_data = mpfile.document['_hdata'].pop('input')
+    csv_path = os.path.join(os.environ['HOME'], 'work', meta_data['csv_path'])
+    if not os.path.exists(csv_path):
+        return 'Please upload', csv_path
     table_columns = meta_data['table_columns'].split(' -- ')
-
     df = read_csv(csv_path, names=table_columns)
     mpfile.add_data_table(mpfile.ids[0], df, 'main_table')
-
     print('Added data from {}'.format(csv_path))
-
