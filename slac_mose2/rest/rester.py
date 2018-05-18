17be3d92290ae636fdb460fb70205c41c20857f1
Patrick Huck <tschaume@users.noreply.github.com>
2018-05-18 00:04:07 +0000

    slac_mose2: add 2D intensity data to contribution

--- slac_mose2/rest/rester.py
+++ slac_mose2/rest/rester.py
@@ -1,11 +1,12 @@
-from __future__ import division, unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from mpcontribs.rest.rester import MPContribsRester
 from mpcontribs.io.archieml.mpfile import MPFile
 
 class SlacMose2Rester(MPContribsRester):
     """SLAC MoSe2-specific convenience functions to interact with MPContribs REST interface"""
-    query = {'project': {'$in': ['LBL']}, 'content.formula': 'MoSe2'}
-    provenance_keys = ['authors', 'description', 'reference']
+    query = {'content.title': 'SLAC MoSe₂/2H-MoTe₂'}
+    provenance_keys = ['title', 'authors', 'description', 'urls']
 
     def get_graphs(self):
         docs = self.query_contributions(
@@ -20,7 +21,7 @@ class SlacMose2Rester(MPContribsRester):
         graphs = mpfile.gdata[mp_id]
 
         return graphs
-        
+
     def get_line_profiles(self):
 
         docs = self.query_contributions(
@@ -28,22 +29,22 @@ class SlacMose2Rester(MPContribsRester):
             )
         if not docs:
             raise Exception('No contributions found for SLAC MoSe2 Explorer!')
-            
+
         doc = docs[0] # there should be only one for MoSe2
         mpfile = MPFile.from_contribution(doc)
         mp_id = mpfile.ids[0]
         table = mpfile.tdata[mp_id]['main_table']
-        
+
         global_x_values = []
         y_values = {}
-        
+
         for key, values in table.items():
             values = list(map(float, values))
             if 'delay time' in key:
                 global_x_values = values
             else:
                 y_values[key] = values
-                
+
         return {
             'global_x_values': global_x_values,
             'y_values': y_values,
