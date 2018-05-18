db87f0ffce68a6d72fa0500d34336ce47489f29a
Patrick Huck <tschaume@users.noreply.github.com>
2018-05-18 22:00:45 +0000

    slac: restrict access to user group

--- slac_mose2/explorer/views.py
+++ slac_mose2/explorer/views.py
@@ -12,28 +12,31 @@ from test_site.settings import STATIC_URL
 def index(request):
     ctx = RequestContext(request)
     if request.user.is_authenticated():
-        API_KEY = request.user.api_key
-        ENDPOINT = request.build_absolute_uri(get_endpoint())
-        from ..rest.rester import SlacMose2Rester
-        with SlacMose2Rester(API_KEY, endpoint=ENDPOINT) as mpr:
-            try:
-                prov = mpr.get_provenance()
-                ctx['title'] = prov.pop('title')
-                ctx['provenance'] = render_dict(prov, webapp=True)
+        if request.user.groups.filter(name='slac_mose2').exists():
+            API_KEY = request.user.api_key
+            ENDPOINT = request.build_absolute_uri(get_endpoint())
+            from ..rest.rester import SlacMose2Rester
+            with SlacMose2Rester(API_KEY, endpoint=ENDPOINT) as mpr:
+                try:
+                    prov = mpr.get_provenance()
+                    ctx['title'] = prov.pop('title')
+                    ctx['provenance'] = render_dict(prov, webapp=True)
 
-                contribs = mpr.get_contributions()
-                ctx['graphs'], ctx['uuids'] = [], []
-                for plot in contribs['graphs']:
-                    rplot = render_plot(plot, webapp=True)
-                    ctx['graphs'].append(rplot[0])
-                    ctx['uuids'].append(str(rplot[1]))
+                    contribs = mpr.get_contributions()
+                    ctx['graphs'], ctx['uuids'] = [], []
+                    for plot in contribs['graphs']:
+                        rplot = render_plot(plot, webapp=True)
+                        ctx['graphs'].append(rplot[0])
+                        ctx['uuids'].append(str(rplot[1]))
 
-                ctx['traces'] = dumps(contribs['traces'])
-                ctx['trace_names'] = [trace['name'] for trace in contribs['traces']]
-                mod = os.path.dirname(__file__).split(os.sep)[-2]
-                ctx['static_url'] = '_'.join([STATIC_URL[:-1], mod])
-            except Exception as ex:
-                ctx.update({'alert': str(ex)})
+                    ctx['traces'] = dumps(contribs['traces'])
+                    ctx['trace_names'] = [trace['name'] for trace in contribs['traces']]
+                    mod = os.path.dirname(__file__).split(os.sep)[-2]
+                    ctx['static_url'] = '_'.join([STATIC_URL[:-1], mod])
+                except Exception as ex:
+                    ctx.update({'alert': str(ex)})
+        else:
+            ctx.update({'alert': 'access restricted to "slac_mose2" user group!'})
     else:
         ctx.update({'alert': 'Please log in!'})
     return render_to_response("slac_mose2_explorer_index.html", ctx)
