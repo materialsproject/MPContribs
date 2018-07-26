552f3e4a266080412199fac6fdfa92d5484dd964
Patrick Huck <phuck@lbl.gov>
2018-07-26 15:05:58 -0700

    try secret-key from django-extensions

--- test_site/settings.py
+++ test_site/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from django.core.management import utils
-SECRET_KEY = utils.get_random_secret_key()
+from django_extensions.management.commands.generate_secret_key import get_random_secret_key
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'django_cas_ng',
     'require',
     'webtzite',
