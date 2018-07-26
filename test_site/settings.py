9dedc431e4c2ccfb80d2aab2abfa9e4dff415338
Patrick Huck <phuck@lbl.gov>
2018-07-26 14:56:32 -0700

    random secret key

--- test_site/settings.py
+++ test_site/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+from django.core.management import utils
+SECRET_KEY = utils.get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
