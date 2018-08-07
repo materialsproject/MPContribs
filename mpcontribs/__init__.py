f67465e31602a91684fa810add8fbfcc8f805b27
Patrick Huck <phuck@lbl.gov>
2018-08-07 21:48:24 +0000

    ignore some warnings

--- mpcontribs/__init__.py
+++ mpcontribs/__init__.py
@@ -7,3 +7,8 @@ __email__ = 'phuck@lbl.gov'
 __copyright__ = 'Copyright 2014-2015, The Materials Project'
 __maintainer__ = 'Patrick Huck'
 
+
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ndarray size changed")
+warnings.filterwarnings("ignore", message="\nPymatgen will drop Py2k support")
