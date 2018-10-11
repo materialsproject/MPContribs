bd163b2be02bf5b78f74961d8880bf6e5f6d77b3
Patrick Huck <phuck@lbl.gov>
2018-10-11 23:10:24 +0000

    fix swf redirect

--- mpcontribs/urls.py
+++ mpcontribs/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from mpcontribs.users_modules import get_user_urlpatterns
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'', include('mpcontribs.portal.urls')),
-    url(r'^rest/', include('mpcontribs.rest.urls')),
-    url(r'^explorer/', include('mpcontribs.explorer.urls')),
-    url(r'^fe-co-v/dataset-01/', include('mpcontribs.users.swf.explorer.urls'))
+    url(r'^(?i)rest/', include('mpcontribs.rest.urls')),
+    url(r'^(?i)explorer/', include('mpcontribs.explorer.urls')),
+    url(r'^(?i)fe-co-v/dataset-01/', lambda request: redirect('mpcontribs_users_swf_explorer_index', permanent=False))
 ] + [
     url(urlpattern[0], include(urlpattern[1]))
     for urlpattern in get_user_urlpatterns()
