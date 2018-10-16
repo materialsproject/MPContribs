046c9544a0133ded7a5342733fb442acd5486256
Patrick Huck <phuck@lbl.gov>
2018-10-16 23:31:45 +0000

    preserve old boltztrap url

--- mpcontribs/urls.py
+++ mpcontribs/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'', include('mpcontribs.portal.urls')),
     url(r'^(?i)rest/', include('mpcontribs.rest.urls')),
     url(r'^(?i)explorer/', include('mpcontribs.explorer.urls')),
-    url(r'^(?i)fe-co-v/dataset-01/', lambda request: redirect('mpcontribs_users_swf_explorer_index', permanent=False))
+    url(r'^(?i)fe-co-v/dataset-01/', lambda request: redirect('mpcontribs_users_swf_explorer_index', permanent=True)),
+    url(r'^(?i)boltztrap/', lambda request: redirect('mpcontribs_users_carrier_transport_explorer_index', permanent=True))
 ] + [
     url(urlpattern[0], include(urlpattern[1]))
     for urlpattern in get_user_urlpatterns()
