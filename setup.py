0603542beed94d3476f87f54991588aa6cec969d
Patrick Huck <phuck@lbl.gov>
2018-10-29 12:47:47 -0700

    add adhoc ssl_context for nginx

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'ipywidgets', 'celery', 'tqdm', 'beautifulsoup4', 'whichcraft', 'pymatgen',
         'unidecode', 'psutil', 'nbformat', 'xlrd', 'django-nopassword', 'mod_wsgi',
         'django-require', 'pympler', 'django_cas_ng', 'django-extensions', 'selenium',
-        'dash', 'dash-html-components', 'dash-core-components'
+        'dash', 'dash-html-components', 'dash-core-components', 'pyopenssl'
     ],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive', 'jupyter'],
