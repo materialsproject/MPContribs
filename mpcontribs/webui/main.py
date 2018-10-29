150548fab30f24658e907adcf3ae9ce0a91fc684
Patrick Huck <phuck@lbl.gov>
2018-10-29 13:36:04 -0700

    open main index

--- mpcontribs/webui/main.py
+++ mpcontribs/webui/main.py
@@ -9,14 +9,4 @@ main_bp = Blueprint('webui_main', __name__, template_folder=tmpl_dir, static_fol
 
 @main_bp.route('/')
 def home():
-    from mpcontribs.rest.rester import MPContribsRester
-    try:
-        with MPContribsRester(test_site=True) as mpr:
-            return render_template('main.html')
-    except ValueError:
-        jpy_user = os.environ.get('JPY_USER')
-        if not jpy_user:
-            raise ValueError('Cannot connect to test_site outside MP JupyterHub!')
-        flaskproxy = url_for('.home')
-        login_url = '/'.join([flaskproxy, jpy_user, 'test_site/login'])
-        return redirect(login_url)
+    return render_template('main.html')
