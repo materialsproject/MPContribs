5cb79aaee5b579c7f8a642329108818e4636cd67
Patrick Huck <phuck@lbl.gov>
2018-11-12 18:46:42 -0800

    update webtzite, and typo fix

--- mpcontribs/portal/assets/index.js
+++ mpcontribs/portal/assets/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 
 function importAll(r) { return r.keys().map(r); }
-importAll(require.context('../images/', false, /\.(png|jpe?g|svg)$/));
+importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
 
 $(document).ready(function() {
   $('.btn-link').tooltip();
