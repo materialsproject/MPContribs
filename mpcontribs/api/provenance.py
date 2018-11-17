2855e633eb7c222304d43b6830107a02e558276f
Patrick Huck <phuck@lbl.gov>
2018-11-17 14:06:13 -0800

    api: better org scheme for handlers

--- mpcontribs/api/provenance.py
+++ mpcontribs/api/provenance.py
@@ -13,20 +13,23 @@
 #    required=True, description='list of URLs for references'
 #)
 
+from flask import g
+import marshmallow as ma
 from marshmallow_mongoengine import ModelSchema
-from flask_rebar import HandlerRegistry
-from mongoengine import fields, DynamicEmbeddedDocument, EmbeddedDocumentField
 from flask_mongoengine import Document, DynamicDocument
+from mongoengine import fields, DynamicEmbeddedDocument, EmbeddedDocumentField
 
-class Url(DynamicEmbeddedDocument):
+class Urls(DynamicEmbeddedDocument):
     main = fields.URLField(required=True)
 
-class Provenance(DynamicDocument): # TODO Document
+# TODO Document
+# TODO make sure all fields in urls show up in response
+class Provenance(DynamicDocument):
     _id = fields.ObjectIdField(required=True)
     title = fields.StringField(max_length=30, required=True, unique=True)
     authors = fields.StringField(required=True)
     description = fields.StringField(required=True)
-    urls = EmbeddedDocumentField(Url, required=True)
+    urls = EmbeddedDocumentField(Urls, required=True)
     project = fields.StringField(max_length=30, required=True, unique=True)
     meta = {
         'collection': 'provenances',
@@ -37,7 +40,18 @@ class ProvenanceSchema(ModelSchema):
     class Meta:
         model = Provenance
 
-def get_provenance(project):
-    """retrieve provenance for a project"""
-    #project = rebar.validated_args.get('project')
-    return Provenance.objects.get_or_404(project=project)
+class GetProvenancesQuerySchema(ma.Schema):
+    project = ma.fields.String(max_length=30)
+    title = ma.fields.String(max_length=30)
+
+def get_provenances():
+    """retrieve provenance information for projects"""
+    valid_args = g.validated_args
+    return Provenance.objects.get_or_404(**valid_args)
+
+def add_all_handlers(registry):
+    registry.add_handler(
+        get_provenances, rule='/provenances', method='GET',
+        query_string_schema=GetProvenancesQuerySchema(),
+        marshal_schema=ProvenanceSchema(),
+    )
--- mpcontribs/api/registry.py
+++ mpcontribs/api/registry.py
@@ -1,18 +1,20 @@
 from flask_rebar import HandlerRegistry
 from marshmallow_mongoengine import fields
+from flask_rebar.swagger_generation import SwaggerV2Generator
 from flask_rebar.swagger_generation.marshmallow_to_swagger import \
         response_converter_registry, StringConverter
 
 from mpcontribs.api.config import VERSION
-from mpcontribs.api.provenance import get_provenance, ProvenanceSchema
+from mpcontribs.api.provenance import add_all_handlers
 
 class ObjectIdConverter(StringConverter):
     MARSHMALLOW_TYPE = fields.ObjectId
 
-registry = HandlerRegistry(prefix=VERSION)
-response_converter_registry.register_type(ObjectIdConverter())
-
-registry.add_handler(
-    get_provenance, rule='/provenances/<project>', method='GET',
-    marshal_schema=ProvenanceSchema(),
+swaggen = SwaggerV2Generator(
+    title='MPContribs API', version=VERSION,
+    description='Operations to retrieve materials data contributed to MP'
 )
+
+registry = HandlerRegistry(prefix=VERSION, swagger_generator=swaggen)
+response_converter_registry.register_type(ObjectIdConverter())
+add_all_handlers(registry)
