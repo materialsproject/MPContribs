1190259a216b4a08716e51a1510b176631d951fd
Patrick Huck <phuck@lbl.gov>
2019-02-01 12:32:40 -0800

    minor setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,32 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import io, re, glob, os
-from setuptools import setup
+from setuptools import setup, find_namespace_packages, find_packages
 
-package_name = 'mpcontribs'
-init_py = io.open('{}/__init__.py'.format(package_name)).read()
-metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
-metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
-
-with open(os.path.join(SETUP_PTH, 'mpcontribs/api/requirements.txt')) as f:
+with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
 setup(
-    name = package_name,
-    version = metadata['version'],
-    description = metadata['doc'],
-    author = metadata['author'],
-    author_email = metadata['email'],
-    url = metadata['url'],
-    packages = [
-        package_name, '{}.io'.format(package_name), '{}.webui'.format(package_name),
-        '{}.explorer'.format(package_name), '{}.portal'.format(package_name),
-        '{}.rest'.format(package_name), '{}.builder'.format(package_name),
-        '{}.api'.format(package_name),
-    ],
+    name = 'mpcontribs',
+    version = '2019.01.31',
+    description = "The Materials Project's Community Contribution Framework",
+    author = 'Patrick Huck',
+    author_email = 'phuck@lbl.gov',
+    url = 'https://mpcontribs.org',
+    #packages = find_namespace_packages(include=['mpcontribs-*']),
+    packages = ['mpcontribs-api.mpcontribs.api',
+                'mpcontribs-users.mpcontribs.users'],
     install_requires = required,
-    dependency_links = ['git+https://github.com/rochacbruno/flasgger.git#egg=flasgger-0.9.3.dev0'],
+    #dependency_links = ['git+https://github.com/rochacbruno/flasgger.git#egg=flasgger-0.9.3.dev0'],
     license = 'MIT',
     keywords = ['materials', 'contribution', 'framework', 'data', 'interactive', 'jupyter'],
     scripts = glob.glob(os.path.join(SETUP_PTH, "scripts", "*")),
