4c5e9e7c8dde78f95981921f4e7ca438b7bbec7e
Patrick Huck <phuck@lbl.gov>
2019-02-01 17:57:19 -0800

    webtzite: reqs and fix path to assets

--- mpcontribs-webtzite/webtzite/assets/index.js
+++ mpcontribs-webtzite/webtzite/assets/index.js
@@ -1,15 +1,15 @@
 import img from './logo.png'
 
-import("../../node_modules/bootstrap/dist/css/bootstrap.min.css");
-import("../../node_modules/components-font-awesome/css/font-awesome.min.css");
-import("../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css");
-import("../../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css");
-import("../../node_modules/backgrid/lib/backgrid.min.css");
-import("../../node_modules/backgrid-paginator/backgrid-paginator.min.css");
-import("../../node_modules/backgrid-filter/backgrid-filter.min.css");
-import("../../node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
-import("../../node_modules/json-human/css/json.human.css");
-import("../../node_modules/chosen-js/chosen.min.css");
+import("../../../node_modules/bootstrap/dist/css/bootstrap.min.css");
+import("../../../node_modules/components-font-awesome/css/font-awesome.min.css");
+import("../../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css");
+import("../../../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css");
+import("../../../node_modules/backgrid/lib/backgrid.min.css");
+import("../../../node_modules/backgrid-paginator/backgrid-paginator.min.css");
+import("../../../node_modules/backgrid-filter/backgrid-filter.min.css");
+import("../../../node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
+import("../../../node_modules/json-human/css/json.human.css");
+import("../../../node_modules/chosen-js/chosen.min.css");
 import("./extra.css");
 
 window.tables = [];
