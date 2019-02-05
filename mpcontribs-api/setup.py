0545c8b700c70d17f7f2bd79f8d306f580398bb7
Patrick Huck <phuck@lbl.gov>
2019-02-04 19:53:46 -0800

    api: use git commit id as version

--- mpcontribs-api/setup.py
+++ mpcontribs-api/setup.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-import io, re, glob, os
+import io, re, glob, os, git
 from setuptools import setup
 
+VERSION = git.Repo().head.object.hexsha[:7]
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
 setup(
     name = 'mpcontribs-api',
-    version = '2018.12.12',
+    version = VERSION,
     description = 'API for community-contributed Materials Project data',
     author = 'Patrick Huck',
     author_email = 'phuck@lbl.gov',
