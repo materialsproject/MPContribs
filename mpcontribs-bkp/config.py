3830653815ee45570a8d2f48154b4da9ce40eb80
Patrick Huck <phuck@lbl.gov>
2019-02-12 13:04:53 -0800

    cleanup, test_files, scripts->mpcontribs-cli

--- mpcontribs-bkp/config.py
+++ mpcontribs-bkp/config.py
@@ -1,26 +1,8 @@
-from __future__ import unicode_literals, print_function
 import os, re
 from tempfile import gettempdir
 
-indent_symbol = '>'
-min_separator_length = 3 # minimum separator length to avoid collision w/ '>>'
-mp_categories = {
-    'mp_id': [
-        'numerify', 'mp-####'
-    ], # mp-id, e.g. mp-1234
-    'composition': [
-        'numerify', 'A#B#'
-    ], # composition, e.g. A2B3
-    'chemical_system': [
-        'lexify', '??'
-    ], # chem. system, e.g. AB
-}
 mp_level01_titles = [ '_hdata', '_tdata', '_gdata', '_structures' ]
 csv_comment_char = '#'
-csv_database = os.path.join(
-  os.path.dirname(os.path.realpath(__file__)),
-  '../test_files/lahman-csv_2014-02-14'
-)
 mp_id_pattern = re.compile('^(mp|por|mvc)-\d+(?:--\d+)?$', re.IGNORECASE)
 object_id_pattern = re.compile('^[a-f\d]{24}$')
 default_mpfile_path = os.path.join(gettempdir(), 'mpfile.txt')
--- mpcontribs-io/mpcontribs/io/archieml/test_mpfile.py
+++ mpcontribs-io/mpcontribs/io/archieml/test_mpfile.py
@@ -1,6 +1,8 @@
+import os
 from mpcontribs.io.archieml.mpfile import MPFile
 
 def test_get_string():
-    mpfile = MPFile.from_file('test_files/test_archieml.txt')
+    test_file = os.path.join(os.path.dirname(__file__), 'test_archieml.txt')
+    mpfile = MPFile.from_file(test_file)
     mpfile_test = MPFile.from_string(mpfile.get_string())
     assert mpfile.document == mpfile_test.document
