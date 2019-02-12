858915a20fcb55b5daa5cb18cb4887bbb7cd3530
Patrick Huck <phuck@lbl.gov>
2019-02-12 12:15:30 -0800

    docker dir is obsolete

--- docker/mpcontribs/README.md
+++
@@ -1,6 +0,0 @@
-```
-sudo ifconfig lo0 alias 127.0.0.2 up
-cd app && docker image build --tag mpcontribs-app .
-cd web && docker image build --tag mpcontribs-nginx .
-cd mpdev && docker image build --tag mpdev-app --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/tschaume\@mac14)" .
-```
--- docker/my_jupyterhub/jupyterhub_config.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-c.Authenticator.admin_users = ['admin']
-c.Spawner.notebook_dir = '~/work'
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-from jupyter_client.localinterfaces import public_ips
-c.JupyterHub.hub_ip = public_ips()[0]
-print('hup_ip: {}'.format(c.JupyterHub.hub_ip))
-c.DockerSpawner.container_image = 'materialsproject/jupyterhub-singleuser'
