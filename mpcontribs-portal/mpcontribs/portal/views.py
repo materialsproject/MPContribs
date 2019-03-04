b73e0581049e4441adcafac4a763755dc0498156
Patrick Huck <phuck@lbl.gov>
2019-03-04 11:25:45 -0800

    portal: container networking test

--- mpcontribs-portal/mpcontribs/portal/views.py
+++ mpcontribs-portal/mpcontribs/portal/views.py
@@ -21,17 +21,12 @@ def index(request):
     ctx['email'] = request.META.get('HTTP_X_CONSUMER_USERNAME')
     api_key = request.META.get('HTTP_X_CONSUMER_CUSTOM_ID')
     if api_key and ctx['email']:
-        return render(request, "mpcontribs_portal_index.html", ctx.flatten())
         import requests
-        #host = 'elasticloadbalancing.us-east-1.amazonaws.com'
-        #url = f'https://{host}/apispec.json'
-        host = '10.0.11.34:8000'
-        url = f'http://{host}/apispec.json'
-        headers = {'Host': 'api.mpcontribs.org', 'X-API-Key': b64decode(api_key)}
+        url = 'http://127.0.0.1:5000/provenances/'
+        headers = {'X-API-Key': b64decode(api_key)}
         r = requests.get(url, headers=headers)
         from django.http import JsonResponse, HttpResponse
-        #return JsonResponse(r.json())
-        return r.raw #HttpResponse(r.raw)
+        return JsonResponse(r.json())
 
         host = os.environ['MPCONTRIBS_API_HOST']
         http_client.set_api_key(
