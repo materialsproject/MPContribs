3dd5ca08287b710a4fcffaad83921e58a1eb6a76
Patrick Huck <phuck@lbl.gov>
2019-04-08 11:54:54 -0700

    explorer: card display with select2 working

--- mpcontribs-explorer/mpcontribs/explorer/assets/index.js
+++ mpcontribs-explorer/mpcontribs/explorer/assets/index.js
@@ -1,29 +1,79 @@
 import 'bootstrap';
-import 'chosen';
-
-$('select').chosen({
-    search_contains: true, allow_single_deselect: true, width: "100%"
-});
+import 'select2';
 
 var api_url = 'http://localhost:5000/contributions/'
 
+$('#projects_list').select2({
+    multiple: true, width: 'style', maximumSelectionLength: 3
+});
+$('#projects_list').on('change', function() {
+    $('#identifiers_list').val(null).trigger('change');
+});
+
+$('#identifiers_list').select2({
+    ajax: {
+        url: api_url,
+        delay: 400,
+        minimumInputLength: 3,
+        maximumSelectionLength: 3,
+        multiple: true,
+        width: 'style',
+        data: function (params) {
+            var projects_select = $("#projects_list").select2("data");
+            var projects = $.map(projects_select, function(project) {
+                return project['text'];
+            });
+            var query = {
+                contains: params.term,
+                projects: projects.join(",")
+            };
+            return query;
+        },
+        processResults: function (data) {
+            var results = [];
+            $.each(data, function(index, element) {
+                var entry = {id: index, text: element["identifier"]};
+                results.push(entry);
+            });
+            return {results: results};
+        }
+    },
+});
+
 $('#btnFind').on('click', function(event) {
     event.preventDefault(); // To prevent following the link (optional)
-    // TODO loop projects, identifiers, and combinations thereof
-    var identifiers = $("#identifiers_list").val() || [];
-    var projects = $("#projects_list").val() || [];
-    var cids = $.map(projects, function(project) {
-        var query = {'project': project, 'mask': 'id'};
+    var queries = [];
+    $.each(["identifiers", "projects"], function(index, name) {
+        var select_data = $('#'+name+'_list').select2('data');
+        var selection = $.map(select_data, function(entry) { return entry['text']; });
+        if (selection.length > 0) {
+            if (index === 0) { // identifiers selected
+                queries.push({'mask': 'id', 'identifiers': selection.join(",")});
+            } else if (index === 1) { // projects selected
+                if (queries.length > 0) { // projects & identifiers selected
+                    var query_tpl = queries.pop();
+                    $.each(selection, function(idx, project) {
+                        var query = $.extend(true, {}, query_tpl); // deep copy
+                        query['projects'] = project;
+                        queries.push(query);
+                    })
+                } else { // only projects selected
+                    queries.push({'mask': 'id', 'projects': selection.join(",")});
+                }
+            }
+        }
+    });
+    var cids = $.map(queries, function(query) {
         return $.get({url: api_url, data: query});
     });
     $.when.apply($, cids).done(function() {
-        var cards = [];
         var args = arguments;
+        if (args.length === 0) { return; }
+        var cards = [];
         if (args[0].length != 3) { args = [arguments]; } // only one project selected
         $.map(args, function(response) {
             $.each(response[0], function (index, contrib) {
-                if (index && cards.length > 0) { return; }
-                console.log('retrieve ' + contrib['id'] + ' ...');
+                if (index && cards.length > 3) { return; }
                 var ajax = $.get({url: api_url + contrib['id'] + '/card'});
                 cards.push(ajax);
             });
--- mpcontribs-webtzite/webtzite/assets/index.js
+++ mpcontribs-webtzite/webtzite/assets/index.js
@@ -10,6 +10,7 @@ import("../../../node_modules/backgrid-filter/backgrid-filter.min.css");
 import("../../../node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
 import("../../../node_modules/json-human/css/json.human.css");
 import("../../../node_modules/chosen-js/chosen.min.css");
+import("../../../node_modules/select2/dist/css/select2.min.css");
 import("./extra.css");
 
 function importAll(r) { return r.keys().map(r); }
