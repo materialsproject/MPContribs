4ccc18b859c04d6842125fcfcf9f5e6f04df1c28
Patrick Huck <phuck@lbl.gov>
2019-04-24 15:39:58 -0700

    mno2: overview graph working w/ api

--- mpcontribs-users/mpcontribs/users/MnO2_phase_selection/explorer/assets/index.js
+++ mpcontribs-users/mpcontribs/users/MnO2_phase_selection/explorer/assets/index.js
@@ -1,22 +1,16 @@
 import Plotly from 'plotly';
 
-var xvals = []; var yvals = [];
-for (var t = 0; t < window.tables.length; t++) {
-    var table = window.tables[t];
-    for (var i = 0; i < table['rows'].length; i++) {
-        var row = table['rows'][i];
-        var mpid_split = row['mp-id'].split('/');
-        var mpid = mpid_split[mpid_split.length-1];
-        xvals.push(mpid);
-        var value = parseFloat(row['ΔH [eV/mol]'].split(' ')[0]);
-        yvals.push(value);
-    }
-}
+var api_url = window.api['host'] + 'contributions/MnO2_phase_selection/graph';
 var graph = document.getElementById('graph');
 var layout = {
     margin: {l: 40, t: 0, b: 25, r: 0},
     xaxis: {type: 'category', showticklabels: false, ticks: ''},
     yaxis: {title: 'Formation Enthalpy [eV/mol]'}
 };
-var data = [{x: xvals, y: yvals, type: 'bar'}];
-Plotly.plot(graph, data, layout);
+
+$.get({
+    url: api_url, data: {'columns': 'ΔH'}, headers: window.api['headers']
+}).done(function() {
+    arguments[0][0]['type'] = 'bar';
+    Plotly.plot(graph, arguments[0], layout);
+});
