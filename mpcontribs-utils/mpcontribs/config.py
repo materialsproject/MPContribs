9cf037dd82997bd33f74aaa3ece12422e6d36df1
Patrick Huck <phuck@lbl.gov>
2019-05-06 14:18:11 -0700

    new HierarchicalData constructor

--- mpcontribs-utils/mpcontribs/config.py
+++ mpcontribs-utils/mpcontribs/config.py
@@ -1,7 +1,7 @@
 import os, re
 from tempfile import gettempdir
 
-mp_level01_titles = [ '_hdata', '_tdata', 'graphs', 'structures' ]
+mp_level01_titles = [ '_hdata', 'tables', 'graphs', 'structures' ]
 csv_comment_char = '#'
 mp_id_pattern = re.compile('^(mp|por|mvc)-\d+(?:--\d+)?$', re.IGNORECASE)
 object_id_pattern = re.compile('^[a-f\d]{24}$')
