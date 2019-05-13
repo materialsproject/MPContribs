abb7a45fe8247dda93f69d558e16365741595481
Patrick Huck <phuck@lbl.gov>
2019-05-13 11:36:01 -0700

    use require to import css and force order

--- mpcontribs-webtzite/webtzite/assets/index.js
+++ mpcontribs-webtzite/webtzite/assets/index.js
@@ -1,19 +1,18 @@
 import img from './logo.png'
 
-import("../../../node_modules/bootstrap/dist/css/bootstrap.min.css");
-import("../../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css");
-import("../../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css");
-import("../../../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css");
-import("../../../node_modules/json-human/css/json.human.css");
-import("../../../node_modules/chosen-js/chosen.min.css");
-import("../../../node_modules/select2/dist/css/select2.min.css");
-import("../../../node_modules/spin.js/spin.css");
-import("../../../node_modules/backgrid/lib/backgrid.min.css");
-import("../../../node_modules/backgrid-paginator/backgrid-paginator.min.css");
-import("../../../node_modules/backgrid-filter/backgrid-filter.min.css");
-import("../../../node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
-
-import("./extra.css");
+require("../../../node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("../../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css");
+require("../../../node_modules/bootstrap-slider/dist/css/bootstrap-slider.min.css");
+require("../../../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css");
+require("../../../node_modules/json-human/css/json.human.css");
+require("../../../node_modules/chosen-js/chosen.min.css");
+require("../../../node_modules/select2/dist/css/select2.min.css");
+require("../../../node_modules/spin.js/spin.css");
+require("../../../node_modules/backgrid/lib/backgrid.min.css");
+require("../../../node_modules/backgrid-paginator/backgrid-paginator.min.css");
+require("../../../node_modules/backgrid-filter/backgrid-filter.min.css");
+require("../../../node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
+require("./extra.css");
 
 function importAll(r) { return r.keys().map(r); }
 importAll(require.context('../../../node_modules/chosen-js', true, /\.(png|jpe?g|svg)$/));
