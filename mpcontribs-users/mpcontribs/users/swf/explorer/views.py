33764cb240e2e27ac9d4f02916ab18fbbbbd754c
Patrick Huck <phuck@lbl.gov>
2019-05-20 11:52:31 -0700

    swf: fix table columns

--- mpcontribs-users/mpcontribs/users/swf/explorer/views.py
+++ mpcontribs-users/mpcontribs/users/swf/explorer/views.py
@@ -8,7 +8,8 @@ project = os.path.dirname(__file__).split(os.sep)[-2]
 def index(request):
     ctx = RequestContext(request)
     try:
-        ctx.update(get_context(project))
+        columns = ['Co', 'Fe', 'V', 'thickness', 'BH|max', 'Hc|MOKE', 'Hc|VSM']
+        ctx.update(get_context(project, columns=columns))
     except Exception as ex:
         ctx['alert'] = str(ex)
     return render(request, "explorer_index.html", ctx.flatten())
