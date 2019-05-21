46c712229a888eeacc4cae34e0c39d489ef6e578
Patrick Huck <phuck@lbl.gov>
2019-05-21 12:11:06 -0700

    remove esters rest

--- mpcontribs-users/mpcontribs/users/esters/rest/rester.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division, unicode_literals
-from mpcontribs.rest.rester import MPContribsRester
-
-class EstersRester(MPContribsRester):
-    query = {'author': 'Marco Esters'}
-    #provenance_keys = ['author', 'description']
-    released = True
