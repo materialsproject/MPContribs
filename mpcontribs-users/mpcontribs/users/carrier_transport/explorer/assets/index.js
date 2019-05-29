e54ef8655a2df0fc35fd9924c4b02a1e8e9fc6a0
Patrick Huck <phuck@lbl.gov>
2019-05-28 17:05:06 -0700

    carrier_transport cleanup

--- mpcontribs-users/mpcontribs/users/carrier_transport/explorer/assets/index.js
+++ mpcontribs-users/mpcontribs/users/carrier_transport/explorer/assets/index.js
@@ -1,10 +1,3 @@
-//<div class="bg-success" style="padding: 5px;">
-//    Click a green cell to update the temperature and
-//    doping level dependence above, or click anywhere else in
-//    a row to display the according eigenvalues.
-//</div>
-//
-//
 //$.get({
 //    url: 'rest',
 //    success: function(data, textStatus, jqXHR) {
@@ -22,50 +15,13 @@
 //                yaxis: {title: 'S [μV/K]'},
 //                margin: {l: 60, b: 50, t: 20}
 //            });
-//        }
-//        $(".backgrid > tbody > tr:first > td:nth-child(6)").click()
-//        $('#spinner').spin(false);
-//
-//});
-
-//    Backbone.on('cellclicked', function(e) {
-//        var row = $(e.currentTarget).parent();
-//        var url = row.find("td:nth-child(2) > a").attr('href');
-//        var cid = url.split('/').pop();
-//        var col = $(e.currentTarget).index();
-//        if ( col > 4 && col < 9 ) {
-//            var classes = $(e.currentTarget).attr("class").split(' ');
-//            var payload = JSON.stringify({name: classes[3] + ' ' + classes[4]});
-//            $.ajax({
-//                type: 'POST',
-//                url: 'rest/' + cid,
-//                data: payload,
-//                dataType: "json",
-//                contentType: "application/json; charset=utf-8",
-//                success: function(data, textStatus, jqXHR) {
-//                    if ( 'null' != data['response'] ) {
-//                        var div = document.getElementById('graph2');
 //                        Plotly.newPlot(div, [data['response']], {
 //                            showLegend: false, margin: {l: 50, r: 0, b: 50, t: 20},
 //                            xaxis: {type: 'log', title: 'doping level [cm⁻³]', exponentformat: "power"},
 //                            yaxis: {title: 'temperature [K]'}
 //                        });
-//                        window.onresize = function() {
-//                            Plotly.Plots.resize(div);
-//                        };
-//                        document.getElementById("graphs").scrollIntoView();
-//                    }
-//                },
-//                error: function(jqXHR, textStatus, errorThrown) {
-//                    console.log(errorThrown);
-//                }
-//            });
-//        } else {
-//            $.getJSON('rest/eigenvalues/'+cid, function(data){
-//                var modal = $('#modal').modal();
-//                var node = JsonHuman.format(data);
-//                modal.find('.modal-body').html(node);
-//                modal.show();
-//            });
 //        }
-//    });
+//        $(".backgrid > tbody > tr:first > td:nth-child(6)").click()
+//        $('#spinner').spin(false);
+//
+//});
--- mpcontribs-users/mpcontribs/users/carrier_transport/explorer/rest_views.py
+++ mpcontribs-users/mpcontribs/users/carrier_transport/explorer/rest_views.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-import json, math
-from webtzite import mapi_func
-from webtzite.connector import ConnectorBase
-from mpcontribs.rest.views import Connector
-from mpcontribs.io.core.components import Table
-from mpcontribs.io.archieml.mpfile import MPFile
-ConnectorBase.register(Connector)
-
-@mapi_func(supported_methods=["POST", "GET"], requires_api_key=True)
 def index(request, cid=None, db_type=None, mdb=None):
     try:
         response = None
@@ -59,61 +48,6 @@ def index(request, cid=None, db_type=None, mdb=None):
         raise ValueError('"REST Error: "{}"'.format(str(ex)))
     return {"valid_response": True, 'response': response}
 
-
-@mapi_func(supported_methods=["GET"], requires_api_key=False)
-def table(request, db_type=None, mdb=None):
-    try:
-        page = int(request.GET.get('page', '1'))
-        crit = {'project': 'carrier_transport'}
-        search = request.GET.get('q')
-        if search is not None:
-            crit.update({'content.extra_data.pretty_formula': {'$regex': search}})
-        proj = {'content.data': 1, 'content.extra_data': 1, 'identifier': 1}
-        total_count = mdb.contrib_ad.query_contributions(crit).count()
-        last_id = None
-        if page is not None and page > 1:
-            limit = (page-1) * 20 # TODO page_size from where?
-            ids = [d['_id'] for d in mdb.contrib_ad.query_contributions(crit, limit=limit)]
-            last_id = ids[-1]
-        sort = {'key': request.GET.get('sort'), 'order': request.GET.get('order')}
-        docs, last_id = mdb.contrib_ad.query_paginate(crit, projection=proj, last_id=last_id, sort=sort)
-        if not docs:
-            raise Exception('No contributions found for CarrierTransport Explorer!')
-
-        items = []
-        columns = ['##'.join(['general', sk]) for sk in ['mp-id', 'cid', 'formula']]
-        keys, subkeys = {'<mₑᶜᵒⁿᵈ>': '[mₑ]', '<S>': '[μV/K]', '<σ>': '[(Ωms)⁻¹]', '<S²σ>': '[μW/(cmK²s)]'}, ['n', 'p']
-        columns += ['##'.join([k, ' '.join([sk, keys[k]])]) for k in keys for sk in subkeys]
-
-        for doc in docs:
-            mpfile = MPFile.from_contribution(doc)
-            mp_id = mpfile.ids[0]
-            contrib = mpfile.hdata[mp_id]
-            cid_url = '/'.join([
-                request.build_absolute_uri(request.path).rsplit('/', 3)[0],
-                'explorer', 'materials' , unicode(doc['_id'])
-            ])
-            mp_id_url = 'https://materialsproject.org/materials/{}'.format(mp_id)
-            row = [mp_id_url, cid_url, contrib['extra_data']['pretty_formula']]
-            row += [
-                contrib['data'].get(k[1:-1], {}).get(sk, {}).get('<ε>', '-').split()[0]
-                for k in keys for sk in subkeys
-            ]
-            items.append(dict((k, v) for k, v in zip(columns, row)))
-
-        per_page = len(items)
-        total_pages = total_count/per_page
-        if total_pages%per_page:
-            total_pages += 1
-        response = {
-            'total_count': total_count, 'total_pages': total_pages, 'page': page,
-            'last_page': total_pages, 'per_page': per_page, 'last_id': last_id, 'items': items
-        }
-    except Exception as ex:
-        raise ValueError('"REST Error: "{}"'.format(str(ex)))
-    return response
-
-@mapi_func(supported_methods=["GET"], requires_api_key=False)
 def eigenvalues(request, cid, db_type=None, mdb=None):
     doc = mdb.contrib_ad.query_contributions(
         {'_id': cid}, projection={'_id': 0, 'content.data': 1}
