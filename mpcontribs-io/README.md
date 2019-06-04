23da73ccda70ab09f48f1b3639f6dc0ca2b906bc
Patrick Huck <phuck@lbl.gov>
2019-06-04 16:25:14 -0700

    mpcontribs-io package

---
+++ mpcontribs-io/README.md
@@ -0,0 +1,1 @@
+MPContribs I/O Library. See the [MPContribs Docs](https://mpcontribs.org) for specific usage and examples.
--- mpcontribs-io/setup.py
+++ mpcontribs-io/setup.py
@@ -1,21 +1,26 @@
-# -*- coding: utf-8 -*-
-import io, re, glob, os, datetime
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(SETUP_PTH, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'mpcontribs-io',
-    version = datetime.datetime.today().strftime('%Y.%m.%d'),
-    description = "MPContribs I/O Library",
     author = 'Patrick Huck',
     author_email = 'phuck@lbl.gov',
-    url = 'https://mpcontribs.org',
-    packages = ['mpcontribs.io'],
+    description = "MPContribs I/O Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/materialsproject/MPContribs/tree/master/mpcontribs-io',
+    packages = find_packages(),
     install_requires = required,
     license = 'MIT',
     zip_safe=False,
-    include_package_data=True
+    include_package_data=True,
+    use_scm_version={"root": "..", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
 )
