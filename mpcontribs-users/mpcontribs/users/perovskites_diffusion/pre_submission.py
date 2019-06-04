666f2be1245198ca66e12fe51c9209e990caee21
Patrick Huck <phuck@lbl.gov>
2019-06-04 12:31:49 -0700

    start perovskites_diffusion

--- mpcontribs-users/mpcontribs/users/perovskites_diffusion/pre_submission.py
+++ mpcontribs-users/mpcontribs/users/perovskites_diffusion/pre_submission.py
@@ -1,27 +1,19 @@
-import tarfile
+import tarfile, os
 from pandas import read_excel
-from six import string_types
 from mpcontribs.io.archieml.mpfile import MPFile
 from mpcontribs.io.core.recdict import RecursiveDict
-from mpcontribs.config import mp_level01_titles
-from mpcontribs.users.perovskites_diffusion.rest.rester import PerovskitesDiffusionRester
 
-def run(mpfile, nmax=None, dup_check_test_site=True):
+project = 'perovskites_diffusion'
 
-    existing_mpids = {}
-    for b in [False, True]:
-        with PerovskitesDiffusionRester(test_site=b) as mpr:
-            for doc in mpr.query_contributions(
-                    projection={'content.data.directory': 1, 'mp_cat_id': 1}
-                ):
-                key = '_'.join([doc['mp_cat_id'], doc['content']['data']['directory']])
-                existing_mpids[key] = doc['_id']
-        if not dup_check_test_site:
-            break
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://'+os.environ['MPCONTRIBS_MONGO_HOST'])
+db = client['mpcontribs']
+print(db.contributions.count_documents({'project': project}))
 
-    general = mpfile.document[mp_level01_titles[0]]
-    google_sheet = general.pop('google_sheet') + '/export?format=xlsx'
-    contcars_filepath = general.pop('contcars_filepath')
+def run(mpfile):
+
+    google_sheet = 'https://docs.google.com/spreadsheets/d/1Wep4LZjehrxu3Cl5KJFvAAhKhP92o4K5aC-kZYjGz2o/export?format=xlsx'
+    contcars_filepath = 'bulk_CONTCARs.tar.gz'
     contcars = tarfile.open(contcars_filepath)
 
     df = read_excel(google_sheet)
@@ -38,7 +30,7 @@ def run(mpfile, nmax=None, dup_check_test_site=True):
             if col == 'level_0' or col == 'index':
                 continue
             key = keys[col]
-            if isinstance(key, string_types):
+            if isinstance(key, str):
                 key = key.strip()
                 if not key in abbreviations:
                     abbreviations[key] = col
@@ -58,9 +50,9 @@ def run(mpfile, nmax=None, dup_check_test_site=True):
                     contcar.read(), fmt='poscar',
                     name=name, identifier=mpid
                 )
-                if not mp_id_pattern.match(mpid_match):
-                    print 'skipping', name
-                    continue
+                #if not mp_id_pattern.match(mpid_match):
+                #    print('skipping', name)
+                #    continue
                 mpid = mpid_match
             else:
                 data[key] = value
@@ -68,29 +60,13 @@ def run(mpfile, nmax=None, dup_check_test_site=True):
         if mpid is None:
             continue
 
-        mpid_mod = '_'.join([mpid, data['directory']])
-        if nmax is not None and mpid_mod in existing_mpids:
-            print 'skipping', mpid_mod
-            skipped += 1
-            continue # skip duplicates
-
         mpfile_single.add_hierarchical_data({'data': data}, identifier=mpid)
-
-        if mpid_mod in existing_mpids:
-            cid = existing_mpids[mpid_mod]
-            mpfile_single.insert_id(mpid, cid)
-            update += 1
 
         mpfile.concat(mpfile_single)
 
-        if nmax is not None and count >= nmax-1:
-            break
-        count += 1
-
     mpfile.add_hierarchical_data({'abbreviations': abbreviations})
 
-    print len(mpfile.ids), 'mp-ids to submit.'
-    if nmax is None and update > 0:
-        print update, 'mp-ids to update.'
-    if nmax is not None and skipped > 0:
-        print skipped, 'duplicates to skip.'
+mpfile = MPFile()
+mpfile.max_contribs = 1
+run(mpfile)
+print(mpfile)
