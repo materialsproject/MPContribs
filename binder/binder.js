a2bd33c7705e485f949f1046cb7d7a0963d3595c
Patrick Huck <phuck@lbl.gov>
2019-06-05 16:15:16 -0700

    trying to get binder working

---
+++ binder/binder.js
@@ -0,0 +1,13 @@
+require("./node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("./node_modules/bootstrap/dist/css/bootstrap-theme.min.css");
+require("./node_modules/json-human/css/json.human.css");
+require("./node_modules/backgrid/lib/backgrid.min.css");
+require("./node_modules/backgrid-paginator/backgrid-paginator.min.css");
+require("./node_modules/backgrid-filter/backgrid-filter.min.css");
+require("./node_modules/backgrid-grouped-columns/backgrid-grouped-columns.css");
+require("./mpcontribs-webtzite/webtzite/assets/extra.css");
+require("./mpcontribs-webtzite/webtzite/assets/render_json");
+require("./mpcontribs-webtzite/webtzite/assets/render_table");
+require("./mpcontribs-webtzite/webtzite/assets/render_plot");
+
+window.api = {'host': 'https://api.mpcontribs.org/'};
---
+++ binder/custom.js
@@ -0,0 +1,1 @@
+var mpcontribs = require(['/custom/js/mpcontribs.var.js']);
