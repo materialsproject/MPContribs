ddc484ebf2417a9ab160ce11e4389e92cd5934bc
Patrick Huck <phuck@lbl.gov>
2019-06-14 12:19:50 -0700

    dilute_solute_diffusion pre_submission

--- mpcontribs-users/mpcontribs/users/dilute_solute_diffusion/pre_submission.py
+++ mpcontribs-users/mpcontribs/users/dilute_solute_diffusion/pre_submission.py
@@ -1,29 +1,31 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 import os, json, requests
 from pandas import read_excel, isnull, ExcelWriter
 from mpcontribs.io.core.recdict import RecursiveDict
 from mpcontribs.io.core.utils import clean_value, nest_dict
-from mpcontribs.users.utils import duplicate_check
+from mpcontribs.io.archieml.mpfile import MPFile
+from pymatgen import MPRester
 
-@duplicate_check
-def run(mpfile, hosts=None, download=False, **kwargs):
-    #mpfile.unique_mp_cat_ids = False
-    from pymatgen import MPRester
+project = 'dilute_solute_diffusion'
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://'+os.environ['MPCONTRIBS_MONGO_HOST'])
+db = client['mpcontribs']
+print(db.contributions.count_documents({'project': project}))
+
+def run(mpfile, hosts=None, download=False):
     mpr = MPRester()
-
-    fpath = os.path.join(os.environ['HOME'], 'work', 'dilute_solute_diffusion.xlsx')
+    fpath = f'{project}.xlsx'
 
     if download or not os.path.exists(fpath):
 
-        figshare_id = mpfile.hdata.general['info']['figshare_id']
+        figshare_id = 1546772
         url = 'https://api.figshare.com/v2/articles/{}'.format(figshare_id)
-        print 'get figshare article {}'.format(figshare_id)
+        print('get figshare article {}'.format(figshare_id))
         r = requests.get(url)
         figshare = json.loads(r.content)
-        mpfile.document['_hdata']['version'] = figshare['version']
+        print('version =', figshare['version']) # TODO set manually in "other"?
 
-        print 'read excel from figshare into DataFrame'
+        print('read excel from figshare into DataFrame')
         df_dct = None
         for d in figshare['files']:
             if 'xlsx' in d['name']:
@@ -31,10 +33,10 @@ def run(mpfile, hosts=None, download=False, **kwargs):
                 df_dct = read_excel(d['download_url'], sheet_name=None)
                 break
         if df_dct is None:
-            print 'no excel sheet found on figshare'
+            print('no excel sheet found on figshare')
             return
 
-        print 'save excel to disk'
+        print('save excel to disk')
         writer = ExcelWriter(fpath)
         for sheet, df in df_dct.items():
             df.to_excel(writer, sheet)
@@ -43,9 +45,9 @@ def run(mpfile, hosts=None, download=False, **kwargs):
     else:
         df_dct = read_excel(fpath, sheet_name=None)
 
-    print len(df_dct), 'sheets loaded.'
+    print(len(df_dct), 'sheets loaded.')
 
-    print 'looping hosts ...'
+    print('looping hosts ...')
     host_info = df_dct['Host Information']
     host_info.set_index(host_info.columns[0], inplace=True)
     host_info.dropna(inplace=True)
@@ -57,22 +59,22 @@ def run(mpfile, hosts=None, download=False, **kwargs):
             elif isinstance(hosts, list) and not host in hosts:
                 continue
 
-        print 'get mp-id for {}'.format(host)
+        print('get mp-id for {}'.format(host))
         mpid = None
         for doc in mpr.query(
             criteria={'pretty_formula': host},
             properties={'task_id': 1}
         ):
-            if doc['sbxd'][0]['decomposes_to'] is None:
+            if 'decomposes_to' not in doc['sbxd'][0]:
                 mpid = doc['task_id']
                 break
         if mpid is None:
-            print 'mp-id for {} not found'.format(host)
+            print('mp-id for {} not found'.format(host))
             continue
 
-        print 'add host info for {}'.format(mpid)
+        print('add host info for {}'.format(mpid))
         hdata = host_info[host].to_dict(into=RecursiveDict)
-        for k in hdata.keys():
+        for k in list(hdata.keys()):
             v = hdata.pop(k)
             ks = k.split()
             if ks[0] not in hdata:
@@ -80,25 +82,25 @@ def run(mpfile, hosts=None, download=False, **kwargs):
             unit = ks[-1][1:-1] if ks[-1].startswith('[') else ''
             subkey = '_'.join(ks[1:-1] if unit else ks[1:]).split(',')[0]
             if subkey == "lattice_constant":
-                unit = u'Å'
+                unit = 'Å'
             try:
-                hdata[ks[0]][subkey] = clean_value(v, unit.replace('angstrom', u'Å'))
+                hdata[ks[0]][subkey] = clean_value(v, unit.replace('angstrom', 'Å'))
             except ValueError:
                 hdata[ks[0]][subkey] = v
         hdata['formula'] = host
         df = df_dct['{}-X'.format(host)]
         rows = list(isnull(df).any(1).nonzero()[0])
         if rows:
-            cells = df.ix[rows].dropna(how='all').dropna(axis=1)[df.columns[0]]
+            cells = df.iloc[rows].dropna(how='all').dropna(axis=1)[df.columns[0]]
             note = cells.iloc[0].replace('following', cells.iloc[1])[:-1]
             hdata['note'] = note
             df.drop(rows, inplace=True)
         mpfile.add_hierarchical_data(nest_dict(hdata, ['data']), identifier=mpid)
 
-        print 'add table for D₀/Q data for {}'.format(mpid)
+        print('add table for D₀/Q data for {}'.format(mpid))
         df.set_index(df['Solute element number'], inplace=True)
         df.drop('Solute element number', axis=1, inplace=True)
-        df.columns = df.ix[0]
+        df.columns = df.iloc[0]
         df.index.name = 'index'
         df.drop('Solute element name', inplace=True)
         df = df.T.reset_index()
@@ -111,12 +113,12 @@ def run(mpfile, hosts=None, download=False, **kwargs):
             df_D0_Q = df[['Solute element name', 'Solute D0 basal [cm^2/s]', 'Solute Q basal [eV]']]
         else:
             df_D0_Q = df[['Solute element name', 'Solute D0 [cm^2/s]', 'Solute Q [eV]']]
-        df_D0_Q.columns = ['El.', 'D₀ [cm²/s]', 'Q [eV]']
+        df_D0_Q.columns = ['Solute', 'D₀ [cm²/s]', 'Q [eV]']
         mpfile.add_data_table(mpid, df_D0_Q, 'D₀_Q')
 
         if hdata['Host']['crystal_structure'] == 'BCC':
 
-            print 'add table for hop activation barriers for {} (BCC)'.format(mpid)
+            print('add table for hop activation barriers for {} (BCC)'.format(mpid))
             columns_E = [
                 'Hop activation barrier, E_{} [eV]'.format(i) for i in range(2,5)
             ] + [
@@ -127,7 +129,7 @@ def run(mpfile, hosts=None, download=False, **kwargs):
                 'Hop activation barrier, E_{} [eV]'.format(i) for i in range(5,7)
             ]
             df_E = df[['Solute element name'] + columns_E]
-            df_E.columns = ['El.'] + [
+            df_E.columns = ['Solute'] + [
                 'E{} [eV]'.format(i) for i in ['₂', '₃', '₄']
             ] + [
                 'E`{} [eV]'.format(i) for i in ['₃', '₄']
@@ -138,7 +140,7 @@ def run(mpfile, hosts=None, download=False, **kwargs):
             ]
             mpfile.add_data_table(mpid, df_E, 'hop_activation_barriers')
 
-            print 'add table for hop attempt frequencies for {} (BCC)'.format(mpid)
+            print('add table for hop attempt frequencies for {} (BCC)'.format(mpid))
             columns_v = [
                 'Hop attempt frequency, v_{} [THz]'.format(i) for i in range(2,5)
             ] + [
@@ -149,10 +151,10 @@ def run(mpfile, hosts=None, download=False, **kwargs):
                 'Hop attempt frequency, v_{} [THz]'.format(i) for i in range(5,7)
             ]
             df_v = df[['Solute element name'] + columns_v]
-            df_v.columns = ['El.'] + [
+            df_v.columns = ['Solute'] + [
                 'v{} [THz]'.format(i) for i in ['₂', '₃', '₄']
             ] + [
-                'v``{} [THz]'.format(i) for i in ['₃', '₄']
+                'v`{} [THz]'.format(i) for i in ['₃', '₄']
             ] + [
                 'v``{} [THz]'.format(i) for i in ['₃', '₄']
             ] + [
@@ -162,21 +164,21 @@ def run(mpfile, hosts=None, download=False, **kwargs):
 
         elif hdata['Host']['crystal_structure'] == 'FCC':
 
-            print 'add table for hop activation barriers for {} (FCC)'.format(mpid)
+            print('add table for hop activation barriers for {} (FCC)'.format(mpid))
             columns_E = ['Hop activation barrier, E_{} [eV]'.format(i) for i in range(5)]
             df_E = df[['Solute element name'] + columns_E]
-            df_E.columns = ['El.'] + ['E{} [eV]'.format(i) for i in ['₀', '₁', '₂', '₃', '₄']]
+            df_E.columns = ['Solute'] + ['E{} [eV]'.format(i) for i in ['₀', '₁', '₂', '₃', '₄']]
             mpfile.add_data_table(mpid, df_E, 'hop_activation_barriers')
 
-            print 'add table for hop attempt frequencies for {} (FCC)'.format(mpid)
+            print('add table for hop attempt frequencies for {} (FCC)'.format(mpid))
             columns_v = ['Hop attempt frequency, v_{} [THz]'.format(i) for i in range(5)]
             df_v = df[['Solute element name'] + columns_v]
-            df_v.columns = ['El.'] + ['v{} [THz]'.format(i) for i in ['₀', '₁', '₂', '₃', '₄']]
+            df_v.columns = ['Solute'] + ['v{} [THz]'.format(i) for i in ['₀', '₁', '₂', '₃', '₄']]
             mpfile.add_data_table(mpid, df_v, 'hop_attempt_frequencies')
 
         elif hdata['Host']['crystal_structure'] == 'HCP':
 
-            print 'add table for hop activation barriers for {} (HCP)'.format(mpid)
+            print('add table for hop activation barriers for {} (HCP)'.format(mpid))
             columns_E = [
                 "Hop activation barrier, E_X [eV]", "Hop activation barrier, E'_X [eV]",
                 "Hop activation barrier, E_a [eV]", "Hop activation barrier, E'_a [eV]",
@@ -184,17 +186,52 @@ def run(mpfile, hosts=None, download=False, **kwargs):
                 "Hop activation barrier, E_c [eV]", "Hop activation barrier, E'_c [eV]"
             ]
             df_E = df[['Solute element name'] + columns_E]
-            df_E.columns = ['El.'] + [
+            df_E.columns = ['Solute'] + [
                 'Eₓ [eV]', 'E`ₓ [eV]', 'Eₐ [eV]', 'E`ₐ [eV]',
                 'E_b [eV]', 'E`_b [eV]', 'Eꪱ [eV]', 'E`ꪱ [eV]'
             ]
             mpfile.add_data_table(mpid, df_E, 'hop_activation_barriers')
 
-            print 'add table for hop attempt frequencies for {} (HCP)'.format(mpid)
+            print('add table for hop attempt frequencies for {} (HCP)'.format(mpid))
             columns_v = ['Hop attempt frequency, v_a [THz]'] + ['Hop attempt frequency, v_X [THz]']
             df_v = df[['Solute element name'] + columns_v]
-            df_v.columns = ['El.'] + ['vₐ [THz]'] + ['vₓ [THz]']
+            df_v.columns = ['Solute'] + ['vₐ [THz]'] + ['vₓ [THz]']
             mpfile.add_data_table(mpid, df_v, 'hop_attempt_frequencies')
 
-            print mpfile
-    print 'DONE'
+    print('DONE')
+
+mpfile = MPFile()
+mpfile.max_contribs = 15
+run(mpfile)
+print(mpfile)
+
+filename = f'{project}.txt'
+mpfile.write_file(filename=filename)
+mpfile = MPFile.from_file(filename)
+print(len(mpfile.ids))
+
+table_names = ['D₀_Q', 'hop_activation_barriers', 'hop_attempt_frequencies']
+
+for idx, (identifier, content) in enumerate(mpfile.document.items()):
+    doc = {'identifier': identifier, 'project': project, 'content': {}}
+    doc['content']['data'] = content['data']
+    doc['collaborators'] = [{'name': 'Patrick Huck', 'email': 'phuck@lbl.gov'}]
+    r = db.contributions.insert_one(doc)
+    cid = r.inserted_id
+    print(idx, ':', cid)
+
+    tids = []
+    for name in table_names:
+        table = mpfile.document[identifier][name]
+        table.pop('@module')
+        table.pop('@class')
+        table['identifier'] = identifier
+        table['project'] = project
+        table['name'] = name
+        table['cid'] = cid
+        r = db.tables.insert_one(table)
+        tids.append(r.inserted_id)
+
+    print(tids)
+    query = {'identifier': identifier, 'project': project}
+    r = db.contributions.update_one(query, {'$set': {'content.tables': tids}})
