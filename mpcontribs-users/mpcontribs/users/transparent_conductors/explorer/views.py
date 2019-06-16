d6b0f1e4fa94770f2202aa3dfb5ff5315e23467a
Patrick Huck <phuck@lbl.gov>
2019-06-15 21:58:13 -0700

    transparent_conductors: set initial columns

--- mpcontribs-users/mpcontribs/users/transparent_conductors/explorer/views.py
+++ mpcontribs-users/mpcontribs/users/transparent_conductors/explorer/views.py
@@ -8,7 +8,13 @@ project = os.path.dirname(__file__).split(os.sep)[-2]
 def index(request):
     ctx = RequestContext(request)
     try:
-        ctx.update(get_context(project))
+        columns = [
+            "doping", "branch_point_energy.bpe_ratio",
+            "computed_gap.hse06_band_gap", "computed_gap.hse06_direct_gap",
+            "experimental_gap.max_experimental_gap", "computed_m*.m*_avg",
+            "max_experimental_conductivity.max_conductivity"
+        ]
+        ctx.update(get_context(project, columns=columns))
     except Exception as ex:
         ctx['alert'] = str(ex)
     return render(request, "explorer_index.html", ctx.flatten())
