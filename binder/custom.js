0bc6165e5f0e0e1169ab7f221d1e9e06ee6c9cf7
Patrick Huck <phuck@lbl.gov>
2019-07-23 11:54:29 -0700

    custom.js: try app_initialized event

--- binder/custom.js
+++ binder/custom.js
@@ -1,6 +1,13 @@
-var prefix = '';
-if ( location.pathname.startsWith('/user') ) {
-    var user = location.pathname.split('/')[2];
-    prefix = '/user/' + user;
-}
-var mpcontribs = require([prefix + '/custom/js/mpcontribs.var.js']);
+define([
+    'base/js/namespace',
+    'base/js/events'
+], function(IPython, events) {
+    events.on('app_initialized.NotebookApp', function() {
+        var prefix = '';
+        if ( location.pathname.startsWith('/user') ) {
+            var user = location.pathname.split('/')[2];
+            prefix = '/user/' + user;
+        }
+        var mpcontribs = require([prefix + '/custom/js/mpcontribs.var.js']);
+    });
+});
