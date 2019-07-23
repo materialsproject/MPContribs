f763364aa2113a4a554997ce6a61db266fa3a6b8
Patrick Huck <phuck@lbl.gov>
2019-07-23 12:19:53 -0700

    custom.js: check pathname for MPContribs

--- binder/custom.js
+++ binder/custom.js
@@ -3,11 +3,14 @@ define([
     'base/js/events'
 ], function(IPython, events) {
     events.on('app_initialized.NotebookApp', function() {
-        var prefix = '';
-        if ( location.pathname.startsWith('/user') ) {
-            var user = location.pathname.split('/')[2];
-            prefix = '/user/' + user;
+        if ( location.pathname.indexOf('MPContribs') !== -1 ) {
+            var prefix = '';
+            if ( location.pathname.startsWith('/user') ) {
+                var user = location.pathname.split('/')[2];
+                prefix = '/user/' + user;
+            }
+            var mpcontribs = require([prefix + '/custom/js/mpcontribs.var.js']);
+            console.log('MPContribs loaded.')
         }
-        var mpcontribs = require([prefix + '/custom/js/mpcontribs.var.js']);
     });
 });
