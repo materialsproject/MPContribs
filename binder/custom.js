099b49b3159723e7a68c8ce72e149205b417fc7f
Patrick Huck <phuck@lbl.gov>
2019-09-05 16:43:25 -0700

    try fix binder

--- binder/custom.js
+++ binder/custom.js
@@ -4,7 +4,7 @@ define([
     'base/js/promises'
 ], function(Jupyter, promises) {
     promises.app_initialized.then(function(appname) {
-        if (appname === 'NotebookApp' && location.pathname.indexOf('MPContribs') !== -1) {
+        if (appname === 'NotebookApp') {
             var prefix = '';
             if ( location.pathname.startsWith('/user') ) {
                 var user = location.pathname.split('/')[2];
