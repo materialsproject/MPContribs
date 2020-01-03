0c6f44e868c59f8e1e793fe35007ba319e71d0ca
Patrick Huck <phuck@lbl.gov>
2020-01-03 13:45:01 -0800

    portal: try externalPort for websocket

--- run_server.py
+++ run_server.py
@@ -1,5 +1,6 @@
 import sys
 from test_site.wsgi import application as portal_app
+from django.conf import settings
 from hendrix.deploy.base import HendrixDeploy
 from twisted.logger import globalLogPublisher, eventAsText, FileLogObserver
 from hendrix.logger import hendrixObserver
@@ -7,6 +8,7 @@ from hendrix.experience import hey_joe
 
 globalLogPublisher.addObserver(FileLogObserver(sys.stdout, lambda e: eventAsText(e) + "\n"))
 deployer = HendrixDeploy(options={'wsgi': portal_app, 'http_port': 8080, 'global-cache': True})
-ws_service = hey_joe.WebSocketService('localhost', 9000)
+kwargs = {} if settings.DEBUG else {externalPort: 80}
+ws_service = hey_joe.WebSocketService('localhost', 9000, **kwargs)
 deployer.add_non_tls_websocket_service(ws_service)
 deployer.run()
