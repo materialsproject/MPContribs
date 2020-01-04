75a24c6515252013072964fbd1ddbc5bb255640c
Patrick Huck <phuck@lbl.gov>
2020-01-03 16:48:18 -0800

    portal: log stderr, back to externalPort 80

--- run_server.py
+++ run_server.py
@@ -7,8 +7,9 @@ from hendrix.logger import hendrixObserver
 from hendrix.experience import hey_joe
 
 globalLogPublisher.addObserver(FileLogObserver(sys.stdout, lambda e: eventAsText(e) + "\n"))
-deployer = HendrixDeploy(options={'wsgi': portal_app, 'http_port': 8080, 'global-cache': True})
-kwargs = {} if settings.DEBUG else {'externalPort': 443}
+globalLogPublisher.addObserver(FileLogObserver(sys.stderr, lambda e: eventAsText(e) + "\n"))
+deployer = HendrixDeploy(options={'wsgi': portal_app, 'http_port': 8080, 'global-cache': True, 'loud': settings.DEBUG})
+kwargs = {} if settings.DEBUG else {'externalPort': 80}
 ws_service = hey_joe.WebSocketService('localhost', 9000, **kwargs)
 deployer.add_non_tls_websocket_service(ws_service)
 deployer.run()
