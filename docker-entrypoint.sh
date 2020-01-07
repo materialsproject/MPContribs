c58d5c9a810b693bff6b18106ed7d2b170f62c8b
Patrick Huck <phuck@lbl.gov>
2020-01-07 00:47:38 -0800

    nb imports, mapi key

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ set -e
 # TODO replace load_client cell for api_key
 out=mpcontribs-portal/mpcontribs/portal/templates
 /venv/bin/jupyter nbconvert --to notebook --execute --inplace notebooks/contribute/get_started.ipynb
+unset MAPI_KEY
 for i in `ls -1 notebooks/*/*.ipynb`; do
    /venv/bin/jupyter nbconvert --to html --template basic --output-dir=$out/`dirname $i` $i
 done
