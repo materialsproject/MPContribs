64c692ff1fb6611e583b35fcf63348ab79f0f502
Patrick Huck <phuck@lbl.gov>
2020-01-07 02:13:41 -0800

    portal: skip get_started exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # TODO replace load_client cell for api_key
 out=mpcontribs-portal/mpcontribs/portal/templates
-/venv/bin/jupyter nbconvert --to notebook --execute --inplace notebooks/contribute/get_started.ipynb
+#/venv/bin/jupyter nbconvert --to notebook --execute --inplace notebooks/contribute/get_started.ipynb
 for i in `ls -1 notebooks/*/*.ipynb`; do
    /venv/bin/jupyter nbconvert --to html --template basic --output-dir=$out/`dirname $i` $i
 done
