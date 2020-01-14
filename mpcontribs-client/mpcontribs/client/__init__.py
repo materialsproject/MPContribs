683ef5e6f2bf1acc5474a5f46d21f9050004feeb
Patrick Huck <phuck@lbl.gov>
2020-01-13 22:26:52 -0800

    client: use fido only

--- mpcontribs-client/mpcontribs/client/__init__.py
+++ mpcontribs-client/mpcontribs/client/__init__.py
@@ -49,10 +49,7 @@ def load_client(apikey=None, headers=None):
 
     if client is not None:
         http_client = client.swagger_spec.http_client
-        force = bool(
-            (apikey and http_client.authenticator.api_key != apikey) or \
-            (headers is not None and http_client.headers != headers)
-        )
+        force = bool(headers is not None and http_client.headers != headers)
 
     if force or client is None:
         # docker containers networking within docker-compose or Fargate task
@@ -60,16 +57,10 @@ def load_client(apikey=None, headers=None):
         if apikey is None:
             host = 'api:5000' if DEBUG else 'localhost:5000'
 
-        if apikey:
-            # TODO Kong forwards consumer headers when api-key used for auth
-            http_client = RequestsClient()
-            http_client.set_api_key(
-                host, apikey, param_in='header', param_name='x-api-key'
-            )
-        else:
-            # forward consumer headers when connecting through localhost
-            http_client = FidoClientGlobalHeaders(headers=headers)
-
+        # - Kong forwards consumer headers when api-key used for auth
+        # - forward consumer headers when connecting through localhost
+        headers = {'x-api-key': apikey} if apikey else headers
+        http_client = FidoClientGlobalHeaders(headers=headers)
         loader = Loader(http_client)
         protocol = 'https' if apikey else 'http'
         spec_url = f'{protocol}://{host}/apispec.json'
