1d3c887a1978d04df169a2dbed10a0e287502d26
Patrick Huck <phuck@lbl.gov>
2020-01-17 14:59:27 -0800

    fix generate_formulae

--- mpcontribs-api/mpcontribs/api/contributions/generate_formulae.py
+++ mpcontribs-api/mpcontribs/api/contributions/generate_formulae.py
@@ -5,8 +5,9 @@ from pymatgen import MPRester
 data = {}
 
 with MPRester() as mpr:
-    for i, d in enumerate(mpr.query(criteria={}, properties=["task_id", "pretty_formula"])):
-        data[d['task_id']] = d['pretty_formula']
+    for i, d in enumerate(mpr.query(criteria={}, properties=["task_ids", "pretty_formula"])):
+        for task_id in d['task_ids']:
+            data[task_id] = d['pretty_formula']
 
 out = os.path.join(os.path.dirname(__file__), 'formulae.json')
 with open(out, 'w') as f:
