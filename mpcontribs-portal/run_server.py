19deedeb0a0d48cbd78b65a622f6b26327657f02
Patrick Huck <phuck@lbl.gov>
2020-01-16 18:12:04 -0800

    portal: stay with run_server

--- mpcontribs-portal/run_server.py
+++ mpcontribs-portal/run_server.py
@@ -6,9 +6,6 @@ from twisted.logger import globalLogPublisher, eventAsText, FileLogObserver
 
 globalLogPublisher.addObserver(FileLogObserver(sys.stdout, lambda e: eventAsText(e) + "\n"))
 globalLogPublisher.addObserver(FileLogObserver(sys.stderr, lambda e: eventAsText(e) + "\n"))
-deployer = HendrixDeploy(options={
-    'wsgi': portal_app, 'http_port': 8765, 'cache_port': 8080,
-    'global_cache': True, 'loud': settings.DEBUG, 'cache': True
-})
+deployer = HendrixDeploy(options={'wsgi': portal_app, 'http_port': 8080, 'loud': True})
 kwargs = {} if settings.DEBUG else {'externalPort': 80}
 deployer.run()
