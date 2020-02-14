686ea66acae4e9cf8000fc4530e8ce9c7093203a
Patrick Huck <phuck@lbl.gov>
2020-02-13 23:10:52 -0800

    portal: mv get_consumer, get_context

--- mpcontribs-portal/mpcontribs/portal/views.py
+++ mpcontribs-portal/mpcontribs/portal/views.py
@@ -15,14 +15,56 @@ from django.template.loaders.app_directories import get_app_template_dirs
 from django.template.loader import select_template
 
 from mpcontribs.client import load_client
-from mpcontribs.portal import get_consumer, get_context
+from mpcontribs.io.core.components.hdata import HierarchicalData
+# TODO should not be needed if render_json.js took care of display/unit/value
+
+def get_consumer(request):
+    names = ['X-Consumer-Groups', 'X-Consumer-Username']
+    headers = {}
+    for name in names:
+        key = f'HTTP_{name.upper().replace("-", "_")}'
+        value = request.META.get(key)
+        if value is not None:
+            headers[name] = value
+    return headers
 
 def landingpage(request):
     ctx = RequestContext(request)
-    project = request.path.replace('/', '')
-
     try:
-        ctx.update(get_context(request, project))
+        project = request.path.replace('/', '')
+        client = load_client(headers=get_consumer(request))
+        prov = client.projects.get_entry(pk=project, _fields=['_all']).result()
+        ctx['project'] = project
+        long_title = prov.get('long_title')
+        ctx['title'] = long_title if long_title else prov['title']
+        ctx['descriptions'] = prov['description'].strip().split('.', 1)
+        authors = prov['authors'].strip().split(',', 1)
+        ctx['authors'] = {'main': authors[0].strip()}
+        if len(authors) > 1:
+            ctx['authors']['etal'] = authors[1].strip()
+        ctx['urls'] = prov['urls']
+        other = prov.get('other', '')
+        if other:
+            ctx['other'] = json.dumps(HierarchicalData(other))
+        if prov['columns']:
+            ctx['columns'] = ['identifier', 'id', 'formula'] + list(prov['columns'].keys())
+            ctx['search_columns'] = ['identifier', 'formula'] + [
+                col for col in prov['columns'].keys()
+                if not col.endswith(']') and not col.startswith('structures')
+            ]
+            ctx['ranges'] = json.dumps(prov['columns'])
+
+        # TODO contribs key is only used in dilute_diffusion and should go through the table
+        #from mpcontribs.io.core.utils import get_short_object_id
+        #ctx['contribs'] = []
+        #for contrib in client.contributions.get_entries(
+        #    project=project, _fields=['id', 'identifier', 'data.formula']
+        #).result()['data']:
+        #    formula = contrib.get('data', {}).get('formula')
+        #    if formula:
+        #        contrib['formula'] = formula
+        #        contrib['short_cid'] = get_short_object_id(contrib['id'])
+        #        ctx['contribs'].append(contrib)
     except Exception as ex:
         ctx['alert'] = str(ex)
 
