edf083a56ea2d166b3c87f999406e8dada933859
Patrick Huck <phuck@lbl.gov>
2020-02-24 15:29:43 -0800

    api: bohr magneton alias

--- mpcontribs-api/mpcontribs/api/__init__.py
+++ mpcontribs-api/mpcontribs/api/__init__.py
@@ -23,6 +23,7 @@ from decimal import Decimal
 ureg = UnitRegistry(auto_reduce_dimensions=True)
 ureg.default_format = 'P~'
 ureg.define('@alias electron_mass = mₑ')
+ureg.define('@alias bohr_magneton = μᵇ')
 Q_ = ureg.Quantity
 delimiter, max_depth = '.', 2
 max_dgts = 6
