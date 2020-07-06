f2ab82f0962b2db1182e58d87892c4ef01457587
Patrick Huck <phuck@lbl.gov>
2020-07-06 11:53:23 -0700

    start cards cleanup

--- mpcontribs-api/mpcontribs/api/cards/views.py
+++ mpcontribs-api/mpcontribs/api/cards/views.py
@@ -4,17 +4,13 @@ import flask_mongorest
 from flask_mongorest.resources import Resource
 from flask_mongorest import operators as ops
 from flask_mongorest.methods import Fetch
-from flask import Blueprint, current_app, render_template, g, request
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from bs4 import BeautifulSoup
+from flask import Blueprint, render_template, request
 from css_html_js_minify import html_minify
-from lxml import html
-from toronado import inline
 from mongoengine.queryset import DoesNotExist
-from fdict import fdict
+from json2html import Json2Html
+from boltons.iterutils import remap
 
-from mpcontribs.api import get_resource_as_string, quantity_keys, delimiter
+from mpcontribs.api import quantity_keys, delimiter
 from mpcontribs.api.core import SwaggerView
 from mpcontribs.api.cards.document import Cards
 from mpcontribs.api.projects.document import Projects
@@ -22,23 +18,13 @@ from mpcontribs.api.contributions.document import Contributions
 
 templates = os.path.join(os.path.dirname(flask_mongorest.__file__), "templates")
 cards = Blueprint("cards", __name__, template_folder=templates)
+j2h = Json2Html()
 
 
-def get_browser():
-    if "browser" not in g:
-        options = webdriver.ChromeOptions()
-        options.add_argument("no-sandbox")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=800,600")
-        options.add_argument("--disable-dev-shm-usage")
-        options.set_headless()
-        host = "chrome" if current_app.config["DEBUG"] else "127.0.0.1"
-        g.browser = webdriver.Remote(
-            command_executor=f"http://{host}:4444/wd/hub",
-            desired_capabilities=DesiredCapabilities.CHROME,
-            options=options,
-        )
-    return g.browser
+def visit(path, key, value):
+    if isinstance(value, dict) and "display" in value:
+        return key, value["display"]
+    return key not in ["value", "unit"]
 
 
 class CardsResource(Resource):
@@ -62,40 +48,23 @@ class CardsView(SwaggerView):
             if not card.html:
                 contrib = Contributions.objects.only("project", "data").get(pk=cid)
                 info = Projects.objects.get(pk=contrib.project.id)
-                ctx = {"cid": cid}
-                ctx["title"] = info.title
+                ctx = info.to_mongo()
+                ctx["cid"] = cid
                 ctx["descriptions"] = info.description.strip().split(".", 1)
                 authors = [a.strip() for a in info.authors.split(",") if a]
                 ctx["authors"] = {"main": authors[0], "etal": authors[1:]}
                 ctx["landing_page"] = f"/{contrib.project.id}/"
                 ctx["more"] = f"/{cid}"
-                ctx["urls"] = info.urls.values()
-                card_script = get_resource_as_string("templates/linkify.min.js")
-                card_script += get_resource_as_string(
-                    "templates/linkify-element.min.js"
+                data = contrib.to_mongo().get("data", {})
+                ctx["data"] = j2h.convert(
+                    json=remap(data, visit=visit), table_attributes='class="table"'
                 )
-                card_script += get_resource_as_string("templates/card.min.js")
-
-                fd = fdict(contrib.data, delimiter=delimiter)
-                ends = [f"{delimiter}{qk}" for qk in quantity_keys]
-                for key in list(fd.keys()):
-                    if any(key.endswith(e) for e in ends):
-                        value = fd.pop(key)
-                        if key.endswith(ends[0]):
-                            new_key = key.rsplit(delimiter, 1)[0]  # drop .display
-                            fd[new_key] = value
-                data = fd.to_dict_nested()
-
-                browser = get_browser()
-                browser.execute_script(card_script, data)
-                bs = BeautifulSoup(browser.page_source, "html.parser")
-                ctx["data"] = bs.body.table
-                # browser.close()
-                rendered = html_minify(render_template("card.html", **ctx))
-                tree = html.fromstring(rendered)
-                inline(tree)
-                card = Cards.objects.get(pk=cid)
-                card.html = html.tostring(tree.body[0]).decode("utf-8")
+                # TODO make html a DictField with bootstrap/bulma/... keys
+                # NOTE holding off until switch to new MP website for MP detail pages
+                card.html = html_minify(
+                    render_template("card.html", **ctx)
+                )  # bootstrap
+                # TODO card.html_bulma
                 card.save()
             return self._resource.serialize(card, params=request.args)
 
