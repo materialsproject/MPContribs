2bdf6f5cfce1cb5b70efed405302d12d9679ada4
Patrick Huck <phuck@lbl.gov>
2020-07-30 02:36:36 -0700

    api: looks like concurrent/async is working

--- mpcontribs-api/mpcontribs/api/notebooks/__init__.py
+++ mpcontribs-api/mpcontribs/api/notebooks/__init__.py
@@ -1,43 +1,24 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import requests
 from uuid import uuid4
-from notebook.utils import url_path_join
-from tornado.httpclient import HTTPRequest
-from tornado.websocket import websocket_connect
-from notebook.gateway.managers import GatewayKernelManager, GatewayClient
 from tornado.escape import json_encode, json_decode, url_escape
+from websocket import create_connection
+from notebook.utils import url_path_join
+from notebook.gateway.managers import GatewayClient
 
 
-async def execute_cells(cid, cells, loop=None):
-    manager = GatewayKernelManager()
-    manager.loop = loop
-    ws = None
-
-    while ws is None:
-        await manager.list_kernels()
-        for kernel_id, kernel in manager._kernels.items():
-            if kernel["execution_state"] == "idle":
-                print(f"kernel {kernel_id} is available.")
-                client = GatewayClient.instance()
-                url = url_path_join(
-                    client.ws_url,
-                    client.kernels_endpoint,
-                    url_escape(kernel_id),
-                    "channels",
-                )
-                ws_req = HTTPRequest(url=url)
-                ws = await websocket_connect(ws_req)
-
-        print("waiting for kernel to become available...")
-        await asyncio.sleep(5, loop=loop)
-
+def run_cells(kernel_id, cid, cells):
+    gw_client = GatewayClient.instance()
+    url = url_path_join(
+        gw_client.ws_url, gw_client.kernels_endpoint, url_escape(kernel_id), "channels",
+    )
     outputs = {}
+    ws = create_connection(url)
+
     for idx, cell in enumerate(cells):
         if cell["cell_type"] == "code":
-            print(cid, idx)
-            outputs[idx] = await asyncio.sleep(1, loop=loop)
-
-            await ws.write_message(
+            ws.send(
                 json_encode(
                     {
                         "header": {
@@ -66,18 +47,38 @@ async def execute_cells(cid, cells, loop=None):
             outputs[idx] = []
             status = None
             while status is None or status == "busy":
-                msg = await ws.read_message()
+                msg = ws.recv()
                 msg = json_decode(msg)
                 msg_type = msg["msg_type"]
                 if msg_type == "status":
                     status = msg["content"]["execution_state"]
                 elif msg_type in ["stream", "display_data", "execute_result"]:
-                    # display_data/execute_result required fields: "output_type", "data", "metadata"
+                    # display_data/execute_result required fields:
+                    #   "output_type", "data", "metadata"
                     # stream required fields: "output_type", "name", "text"
                     output = msg["content"]
                     output.pop("transient", None)
                     output["output_type"] = msg_type
                     outputs[idx].append(output)
 
-    ws.close()  #  TODO await?
+    ws.close()
     return outputs
+
+
+async def execute_cells(cid, cells, loop=None):
+    gw_client = GatewayClient.instance()
+    base_endpoint = url_path_join(gw_client.url, gw_client.kernels_endpoint)
+    response = await loop.run_in_executor(None, requests.get, base_endpoint)
+    kernels = response.json()
+
+    while 1:
+        for kernel in kernels:
+            if kernel["execution_state"] == "idle":
+                kernel_id = kernel["id"]
+                print(f"{kernel_id} AVAILABLE")
+                return await loop.run_in_executor(
+                    None, run_cells, kernel_id, cid, cells
+                )
+        else:
+            print("WAITING for kernel")
+            await asyncio.sleep(5, loop=loop)
