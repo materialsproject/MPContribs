a3c535197a2de51c34ae9c84061c322593141015
Patrick Huck <phuck@lbl.gov>
2020-09-16 16:49:47 -0700

    api: add --error-logfile

--- mpcontribs-api/start.sh
+++ mpcontribs-api/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:$API_PORT -k gevent -w $NWORKERS --access-logfile - --log-level debug $RELOAD "mpcontribs.api:create_app()"
+gunicorn -b 0.0.0.0:$API_PORT -k gevent -w $NWORKERS --access-logfile - --error-logfile - --log-level debug $RELOAD "mpcontribs.api:create_app()"
