34a627242d3934ec8044013193f5f1beda7d8adc
Patrick Huck <phuck@lbl.gov>
2021-08-07 02:28:58 -0700

    portal: remove images

--- mpcontribs-portal/mpcontribs/portal/assets/js/browse.js
+++ mpcontribs-portal/mpcontribs/portal/assets/js/browse.js
@@ -26,33 +26,6 @@ $(document).ready(function () {
     li.siblings().removeClass('is-active');
     li.addClass('is-active');
 
-    var imageNames = require.context('../images/', false, /\.(png)$/).keys();
-
-    $.each(imageNames, function(idx, name) {
-        var imageName = name.replace('./', '');
-        import('../images/' + imageName).then(function(src) {
-            var suffix = imageName.replace('.png', '_img');
-            $('[name=thumbnail_' + suffix + ']').each(function(idx) {
-                var img = $("<img/>", {src: src.default, width: "100%"});
-                $(this).append(img);
-            });
-        }).catch(function(err) { console.log(err); });
-    });
-
-    $('[name=thumbnail]').on('click', function(e) {
-        e.preventDefault();
-        var selector = '#' + $(this).attr("id") + "_modal";
-        window.scrollTo(0, 0);
-        $(selector).addClass("is-active");
-        $("html").addClass("is-clipped");
-    });
-
-    $('[name=thumbnail_close]').on('click', function(e) {
-        e.preventDefault();
-        $(this).parent().removeClass("is-active");
-        $('html').removeClass("is-clipped");
-    });
-
     $('a[name="download"]').click(function(e) {
         const project = $(this).data('project');
         const fmt = $(this).data('format');
