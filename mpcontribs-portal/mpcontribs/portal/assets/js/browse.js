6d8d413699b6f26c3cf8d7fe13c9098b8104c4bc
Patrick Huck <phuck@lbl.gov>
2021-08-08 02:10:48 -0700

    portal: row hover

--- mpcontribs-portal/mpcontribs/portal/assets/js/browse.js
+++ mpcontribs-portal/mpcontribs/portal/assets/js/browse.js
@@ -121,4 +121,14 @@ $.get({
         className: "htCenter htMiddle",
         //columnSorting: {initialConfig: {column: 4, sortOrder: 'desc'}}
     });
+    hot.addHook('afterOnCellMouseOver', function(e, coords, TD) {
+        var row = coords["row"];
+        if (row > 0) { $(TD).parent().addClass("htHover"); }
+    });
+
+    hot.addHook('afterOnCellMouseOut', function(e, coords, TD) {
+        var row = coords["row"];
+        if (row > 0) { $(TD).parent().removeClass("htHover"); }
+    });
+
 });
