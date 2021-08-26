9db0c67cf20ad4749e85ebfbbf0e0d78e8c28039
Patrick Huck <phuck@lbl.gov>
2021-08-26 13:10:09 -0700

    try pinging ECS metadata endpoint

--- mpcontribs-api/main.sh
+++ mpcontribs-api/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 supervisorctl start api
+# TODO only start worker/scheduler on first AWS task/container?
+
+if [ ! -z ${ECS_CONTAINER_METADATA_URI_V4} ]; then
+    http ${ECS_CONTAINER_METADATA_URI_V4}
+fi
+
 supervisorctl start worker
 supervisorctl start scheduler
