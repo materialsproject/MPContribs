8bd817bbca69c7cddea7fcf2c571ac0c6f87ac01
Patrick Huck <phuck@lbl.gov>
2021-08-26 13:54:13 -0700

    api: fix metadata uri check

--- mpcontribs-api/main.sh
+++ mpcontribs-api/main.sh
@@ -4,12 +4,13 @@ supervisorctl start api
 
 echo "METADATA URI: ${METADATA_URI}"
 
-if [ -z ${METADATA_URI} ]; then
+if [ -z "${METADATA_URI}" ]; then
     # in docker-compose stack with one task
     supervisorctl start worker
     supervisorctl start scheduler
 else
     # in AWS Fargate with potentially multiple tasks
+    echo "PINGING ${METADATA_URI}"
     http ${METADATA_URI}
     supervisorctl start worker
     supervisorctl start scheduler
