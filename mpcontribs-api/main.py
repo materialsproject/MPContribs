a1453497db27564a55337396da31b56217611580
Patrick Huck <phuck@lbl.gov>
2021-08-27 15:19:41 -0700

    api: use metadata endpoint and ecs client to count tasks

--- mpcontribs-api/main.py
+++ mpcontribs-api/main.py
@@ -1,18 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env python
+import os
+import requests
+import boto3
 
-supervisorctl start api
+from supervisor.options import ClientOptions
+from supervisor.supervisorctl import Controller
 
-echo "METADATA URI: ${METADATA_URI}"
+client = boto3.client('ecs')
 
-if [ -z "${METADATA_URI}" ]; then
-    # in docker-compose stack with one task
-    supervisorctl start worker
-    supervisorctl start scheduler
-else
+def start(program):
+    args = ["start", program]
+    options = ClientOptions()
+    options.realize(args)
+    c = Controller(options)
+    c.onecmd(" ".join(options.args))
+
+start("api")
+
+metadata_uri = os.environ.get("METADATA_URI", "")
+start_rq = True
+
+if metadata_uri:
     # in AWS Fargate with potentially multiple tasks
-    echo "PINGING ${METADATA_URI}"
-    curl ${METADATA_URI} | python -m json.tool
-    supervisorctl start worker
-    supervisorctl start scheduler
-fi
+    print("METADATA URI", metadata_uri)
+    r = requests.get(metadata_uri)
+    labels = r.json()["Labels"]
+    prefix = "com.amazonaws.ecs"
+    cluster = labels[f"{prefix}.cluster"].split("/", 1)[1]
+    family = labels[f"{prefix}.task-definition-family"]
+    version = labels[f"{prefix}.task-definition-version"]
+    tasks = client.list_tasks(cluster=cluster, family=family).get("taskArns", [])
+    print("TASKS", tasks)
+    # TODO check version of each task
+    start_rq = not tasks
 
+if start_rq:
+    for program in ["worker", "scheduler"]:
+        start(program)
