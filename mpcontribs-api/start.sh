a16693a57020a728a34ef3c50f5bb7619a71caaf
Patrick Huck <phuck@lbl.gov>
2022-01-12 18:18:25 -0800

    gunicorn.conf.py for ddtrace

--- mpcontribs-api/start.sh
+++ mpcontribs-api/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ddtrace-run gunicorn -b 0.0.0.0:$API_PORT -k gevent -w $NWORKERS \
+DD_GEVENT_PATCH_ALL=true gunicorn -c gunicorn.conf.py \
+    -b 0.0.0.0:$API_PORT -k gevent -w $NWORKERS \
     --access-logfile - --error-logfile - --log-level debug $RELOAD \
     --max-requests $MAX_REQUESTS --max-requests-jitter $MAX_REQUESTS_JITTER \
     "mpcontribs.api:create_app()"
