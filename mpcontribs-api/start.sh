c9a4ba7216c54ff433afb9816a4b0ed219a87b5a
Patrick Huck <phuck@lbl.gov>
2022-01-18 13:29:28 -0800

    api: rm DD_GEVENT_PATCH_ALL

--- mpcontribs-api/start.sh
+++ mpcontribs-api/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DD_GEVENT_PATCH_ALL=true gunicorn -c gunicorn.conf.py \
+gunicorn -c gunicorn.conf.py \
     -b 0.0.0.0:$API_PORT -k gevent -w $NWORKERS \
     --access-logfile - --error-logfile - --log-level debug $RELOAD \
     --max-requests $MAX_REQUESTS --max-requests-jitter $MAX_REQUESTS_JITTER \
