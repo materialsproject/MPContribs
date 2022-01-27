7981454173b277658b1cf17203cfa7a01136b871
Patrick Huck <phuck@lbl.gov>
2022-01-27 12:34:41 -0800

    api: disable rq, exec

--- mpcontribs-api/main.py
+++ mpcontribs-api/main.py
@@ -17,28 +17,28 @@ def start(program):
 
 start("apis:*")
 
-metadata_uri = os.environ.get("METADATA_URI", "")
-start_rq = True
-
-if metadata_uri:
-    # in AWS Fargate with potentially multiple tasks
-    print("METADATA URI", metadata_uri)
-    r = requests.get(metadata_uri)
-    labels = r.json()["Labels"]
-    prefix = "com.amazonaws.ecs"
-    cluster = labels[f"{prefix}.cluster"].split("/", 1)[1]
-    family = labels[f"{prefix}.task-definition-family"]
-    version = int(labels[f"{prefix}.task-definition-version"])
-    task_arns = client.list_tasks(cluster=cluster, family=family).get("taskArns", [])
-    tasks = client.describe_tasks(cluster=cluster, tasks=task_arns).get("tasks", [])
-    ntasks = 0
-
-    for task in tasks:
-        v = int(task["taskDefinitionArn"].rsplit(":", 1)[1])
-        ntasks += int(v == version)
-
-    start_rq = ntasks == 1  # this task included in metadata response
-    print(f"TASKS {ntasks}/{len(tasks)} -> START RQ {start_rq}")
-
-if start_rq:
-    start("rq:*")
+#metadata_uri = os.environ.get("METADATA_URI", "")
+#start_rq = True
+#
+#if metadata_uri:
+#    # in AWS Fargate with potentially multiple tasks
+#    print("METADATA URI", metadata_uri)
+#    r = requests.get(metadata_uri)
+#    labels = r.json()["Labels"]
+#    prefix = "com.amazonaws.ecs"
+#    cluster = labels[f"{prefix}.cluster"].split("/", 1)[1]
+#    family = labels[f"{prefix}.task-definition-family"]
+#    version = int(labels[f"{prefix}.task-definition-version"])
+#    task_arns = client.list_tasks(cluster=cluster, family=family).get("taskArns", [])
+#    tasks = client.describe_tasks(cluster=cluster, tasks=task_arns).get("tasks", [])
+#    ntasks = 0
+#
+#    for task in tasks:
+#        v = int(task["taskDefinitionArn"].rsplit(":", 1)[1])
+#        ntasks += int(v == version)
+#
+#    start_rq = ntasks == 1  # this task included in metadata response
+#    print(f"TASKS {ntasks}/{len(tasks)} -> START RQ {start_rq}")
+#
+#if start_rq:
+#    start("rq:*")
--- mpcontribs-api/scripts/start_rq.sh
+++ mpcontribs-api/scripts/start_rq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wait-for-it.sh $JUPYTER_GATEWAY_HOST -s -t 50 -- wait-for-it.sh $MPCONTRIBS_API_HOST -s -t 15 -- ddtrace-run flask rq $1
+exec wait-for-it.sh $JUPYTER_GATEWAY_HOST -s -t 50 -- wait-for-it.sh $MPCONTRIBS_API_HOST -s -t 15 -- ddtrace-run flask rq $1
