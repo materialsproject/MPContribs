a1637d8d618a71715f76bfa68412c400fe4dc7eb
Patrick Huck <phuck@lbl.gov>
2022-01-27 18:20:32 -0800

    portal: fix autostart and exec

--- mpcontribs-portal/start.sh
+++ mpcontribs-portal/start.sh
@@ -4,7 +4,7 @@ zzz=$(($DEPLOYMENT*60))
 echo "$SUPERVISOR_PROCESS_NAME: waiting for $zzz seconds before start..."
 sleep $zzz
 
-exec python manage.py migrate --noinput
+python manage.py migrate --noinput
 exec wait-for-it.sh $MPCONTRIBS_API_HOST -s -t 60 -- gunicorn -c gunicorn.conf.py \
     -b 0.0.0.0:$PORTAL_PORT -k gevent -w $NWORKERS \
     --access-logfile - --error-logfile - --log-level $DD_LOG_LEVEL $RELOAD \
