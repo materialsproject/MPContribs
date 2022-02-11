a025fdba18146624851c342a8b399704e06d519b
Patrick Huck <phuck@lbl.gov>
2022-02-10 16:30:37 -0800

    dummy pmgrc, minor access log fmt change

--- mpcontribs-api/scripts/start.sh
+++ mpcontribs-api/scripts/start.sh
@@ -4,4 +4,7 @@ zzz=$(($DEPLOYMENT*60))
 echo "$SUPERVISOR_PROCESS_NAME: waiting for $zzz seconds before start..."
 sleep $zzz
 
+pmgrc=$HOME/.pmgrc.yaml
+[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" > $pmgrc
+
 exec wait-for-it.sh $JUPYTER_GATEWAY_HOST -q -t 50 -- ddtrace-run gunicorn "mpcontribs.api:create_app()"
--- mpcontribs-portal/start.sh
+++ mpcontribs-portal/start.sh
@@ -4,5 +4,7 @@ zzz=$(($DEPLOYMENT*60))
 echo "$SUPERVISOR_PROCESS_NAME: waiting for $zzz seconds before start..."
 sleep $zzz
 
+pmgrc=$HOME/.pmgrc.yaml
+[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" > $pmgrc
 python manage.py migrate --noinput
 exec wait-for-it.sh $MPCONTRIBS_API_HOST -q -s -t 60 -- ddtrace-run gunicorn wsgi
