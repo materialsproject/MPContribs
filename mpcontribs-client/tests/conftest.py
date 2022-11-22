bcf95c56540ea431043bd8aae49a909233392d3b
Patrick Huck <phuck@lbl.gov>
2022-11-22 14:21:35 -0800

    client: fix tests

--- mpcontribs-client/tests/conftest.py
+++ mpcontribs-client/tests/conftest.py
@@ -1,21 +1,15 @@
 import os
 import pytest
 from mpcontribs.api import create_app
-from mpcontribs.client import Client
 
 os.environ['MPCONTRIBS_DB_NAME'] = 'mpcontribs-ls'
+
 
 @pytest.fixture()
 def app():
     app = create_app()
-    #app.config.update({
-    #    "TESTING": True,
-    #})
-
     # other setup can go here
-
     yield app
-
     # clean up / reset resources here
 
 
