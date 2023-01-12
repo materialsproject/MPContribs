74008402576dffba24ab218991f2e214c111fe11
Patrick Huck <phuck@lbl.gov>
2023-01-11 16:36:59 -0800

    client: return list also for single project

--- mpcontribs-client/mpcontribs/client/__init__.py
+++ mpcontribs-client/mpcontribs/client/__init__.py
@@ -776,7 +776,7 @@ class Client(SwaggerClient):
         query = query or {}
 
         if self.project or "name" in query:
-            return self.get_project(name=query.get("name"), fields=fields)
+            return [self.get_project(name=query.get("name"), fields=fields)]
 
         if term:
             def search_future(search_term):
