7113870aaa07200eb488cc6d895ac2b1d63fe770
Patrick Huck <phuck@lbl.gov>
2024-04-05 12:12:08 -0700

    optional datadog

--- mpcontribs-api/scripts/start_rq.sh
+++ mpcontribs-api/scripts/start_rq.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-zzz=$(($DEPLOYMENT*60))
+set -e
+zzz=$(($DEPLOYMENT * 60))
 echo "$SUPERVISOR_PROCESS_NAME: waiting for $zzz seconds before start..."
 sleep $zzz
 
-exec wait-for-it.sh $JUPYTER_GATEWAY_HOST -s -t 50 -- wait-for-it.sh $MPCONTRIBS_API_HOST -s -t 15 -- ddtrace-run flask rq $1
+CMD="flask rq $1"
+
+if [[ -n "$DD_TRACE_HOST" ]]; then
+	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+fi
+
+exec wait-for-it.sh $JUPYTER_GATEWAY_HOST -q -s -t 50 -- \
+	wait-for-it.sh $MPCONTRIBS_API_HOST -q -s -t 15 -- $CMD
--- mpcontribs-kernel-gateway/start.sh
+++ mpcontribs-kernel-gateway/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
 pmgrc=$HOME/.pmgrc.yaml
-[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" > $pmgrc
+[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" >$pmgrc
 
-exec wait-for-it.sh $DD_TRACE_HOST -s -q -t 60 -- ddtrace-run jupyter kernelgateway \
-    --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
+CMD="jupyter kernelgateway"
 
+if [[ -n "$DD_TRACE_HOST" ]]; then
+	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+fi
 
+exec $CMD --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
