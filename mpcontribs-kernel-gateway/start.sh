d2670961e49d129692e69b48ba187488a525c727
Patrick Huck <phuck@lbl.gov>
2024-04-17 12:51:27 -0700

    kernel gateway: TINI_SUBREAPER, echo CMD

--- mpcontribs-kernel-gateway/start.sh
+++ mpcontribs-kernel-gateway/start.sh
@@ -10,4 +10,5 @@ if [[ -n "$DD_TRACE_HOST" ]]; then
 	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
 fi
 
+echo $CMD
 exec $CMD --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
