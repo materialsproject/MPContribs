ceffcbc72303addf1570f437949978f806823c08
Patrick Huck <phuck@lbl.gov>
2024-04-17 14:00:05 -0700

    api: STATSD_HOST

--- mpcontribs-api/scripts/start.sh
+++ mpcontribs-api/scripts/start.sh
@@ -8,10 +8,16 @@ sleep $zzz
 pmgrc=$HOME/.pmgrc.yaml
 [[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" >$pmgrc
 
-CMD='gunicorn mpcontribs.api:create_app()'
+STATS_ARG=""
+SERVER_APP="mpcontribs.api:create_app()"
+WAIT_FOR="wait-for-it.sh $JUPYTER_GATEWAY_HOST -q -t 50"
 
 if [[ -n "$DD_TRACE_HOST" ]]; then
-	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && STATS_ARG="--statsd-host $DD_AGENT_HOST:8125"
 fi
 
-exec wait-for-it.sh $JUPYTER_GATEWAY_HOST -q -t 50 -- $CMD
+if [[ -n "$STATS_ARG" ]]; then
+	exec $WAIT_FOR -- ddtrace-run gunicorn $STATS_ARG $SERVER_APP
+else
+	exec $WAIT_FOR -- gunicorn $SERVER_APP
+fi
--- mpcontribs-kernel-gateway/start.sh
+++ mpcontribs-kernel-gateway/start.sh
@@ -10,5 +10,4 @@ if [[ -n "$DD_TRACE_HOST" ]]; then
 	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
 fi
 
-echo $CMD
 exec $CMD --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
