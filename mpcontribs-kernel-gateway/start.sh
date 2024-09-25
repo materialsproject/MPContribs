9cae15aa8e65cd308ba9dacbb46837d4a1845946
Patrick Huck <phuck@lbl.gov>
2024-09-25 15:59:07 -0700

    more linting

--- mpcontribs-kernel-gateway/start.sh
+++ mpcontribs-kernel-gateway/start.sh
@@ -7,7 +7,7 @@ pmgrc=$HOME/.pmgrc.yaml
 CMD="jupyter kernelgateway"
 
 if [[ -n "$DD_TRACE_HOST" ]]; then
-	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+  wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
 fi
 
 exec $CMD --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
