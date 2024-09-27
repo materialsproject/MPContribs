9472038ef448b8f204c92f209725249fade098e6
Patrick Huck <phuck@lbl.gov>
2024-09-27 16:17:26 -0700

    kernel-gateway: lint env vars, disable ddtrace-run for now

--- mpcontribs-kernel-gateway/start.sh
+++ mpcontribs-kernel-gateway/start.sh
@@ -6,8 +6,9 @@ pmgrc=$HOME/.pmgrc.yaml
 
 CMD="jupyter kernelgateway"
 
-if [[ -n "$DD_TRACE_HOST" ]]; then
-  wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
-fi
+# NOTE ddtrace-run causes 100% CPU usage
+#if [[ -n "$DD_TRACE_HOST" ]]; then
+#  wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+#fi
 
 exec $CMD --KernelGatewayApp.log_format='%(asctime)s,%(msecs)03d %(levelname)s [%(name)s] [%(module)s:%(lineno)d] - %(message)s'
