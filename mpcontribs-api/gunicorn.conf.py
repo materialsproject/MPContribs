d14dda850a6fe7d850e702469ed2e58a48259858
evidencebp <idanamit@gmail.com>
2024-11-15 13:30:47 +0200

    mpcontribs-api\gunicorn.conf.py line-too-long
    Was a very long format line with 135 characters which wasn't readable.
    I reduced it to 107, higher than the recommended 100, while keeping the main part in the same line.

--- mpcontribs-api/gunicorn.conf.py
+++ mpcontribs-api/gunicorn.conf.py
@@ -6,9 +6,11 @@ workers = os.getenv("NWORKERS")
 statsd_host = "{}:8125".format(os.getenv("DD_AGENT_HOST"))
 accesslog = "-"
 errorlog = "-"
-access_log_format = '{}/{}: %(h)s %(t)s %(m)s %(U)s?%(q)s %(H)s %(s)s %(b)s "%(f)s" "%(a)s" %(D)s %(p)s %({{x-consumer-id}}i)s'.format(
-    os.getenv("SUPERVISOR_GROUP_NAME"), os.getenv("SUPERVISOR_PROCESS_NAME")
-)
+access_log_format = (
+'{}/{}: %(h)s %(t)s %(m)s %(U)s?%(q)s %(H)s %(s)s %(b)s "%(f)s" "%(a)s" %(D)s %(p)s %({{x-consumer-id}}i)s'
+        .format(os.getenv("SUPERVISOR_GROUP_NAME"), 
+                os.getenv("SUPERVISOR_PROCESS_NAME"))
+                    )
 max_requests = os.getenv("MAX_REQUESTS")
 max_requests_jitter = os.getenv("MAX_REQUESTS_JITTER")
 proc_name = os.getenv("SUPERVISOR_PROCESS_NAME")
