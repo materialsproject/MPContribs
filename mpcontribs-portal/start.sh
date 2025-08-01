ff819987828a78f4aabcea937b38d43a1a1d5c57
Patrick Huck <phuck@lbl.gov>
2025-08-01 11:37:55 -0700

    start.sh linting

--- mpcontribs-portal/start.sh
+++ mpcontribs-portal/start.sh
@@ -5,21 +5,21 @@ zzz=$(($DEPLOYMENT * 60))
 echo "$SUPERVISOR_PROCESS_NAME: waiting for $zzz seconds before start..."
 sleep $zzz
 
-if [ ! -z "$METADATA_URI" ]; then
-  task_ip=$(curl ${METADATA_URI}/task | jq -r '.Containers[0].Networks[0].IPv4Addresses[0]')
+if [ -n "$METADATA_URI" ]; then
+  task_ip=$(curl "${METADATA_URI}/task" | jq -r '.Containers[0].Networks[0].IPv4Addresses[0]')
   export MPCONTRIBS_CLIENT_HOST=$task_ip:$MPCONTRIBS_API_PORT
 else
   export MPCONTRIBS_CLIENT_HOST=$MPCONTRIBS_API_HOST
 fi
 
-pmgrc=$HOME/.pmgrc.yaml
-[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" >$pmgrc
+PMGRC=$HOME/.pmgrc.yaml
+[[ ! -e "$PMGRC" ]] && echo "PMG_DUMMY_VAR: dummy" >$PMGRC
 python manage.py migrate --noinput
 
 CMD="gunicorn wsgi"
 
 #if [[ -n "$DD_TRACE_HOST" ]]; then
-#	wait-for-it.sh $DD_TRACE_HOST -q -s -t 10 && CMD="ddtrace-run $CMD"
+#  wait-for-it.sh "$DD_TRACE_HOST" -q -s -t 10 && CMD="ddtrace-run $CMD"
 #fi
 
 exec wait-for-it.sh $MPCONTRIBS_API_HOST -q -s -t 60 -- $CMD
