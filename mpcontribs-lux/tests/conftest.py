65f2dc6519546427a483a69d689644255b8d9d53
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-02 17:47:32 -0800

    port tests over from emmet

---
+++ mpcontribs-lux/tests/conftest.py
@@ -0,0 +1,23 @@
+"""Define common testing fixtures."""
+
+import os
+from pathlib import Path
+import shutil
+import tempfile
+
+import pytest
+
+@pytest.fixture(autouse=True)
+def test_dir():
+    """Same as clean_dir but is fresh for every test"""
+
+    old_cwd = os.getcwd()
+    new_path = tempfile.mkdtemp()
+    os.chdir(new_path)
+    yield
+    os.chdir(old_cwd)
+    shutil.rmtree(new_path)
+
+@pytest.fixture(scope="session")
+def test_data_dir() -> Path:
+    return (Path(__file__) / ".." / ".." / "test_data").resolve()
