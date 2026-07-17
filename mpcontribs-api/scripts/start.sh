54229cdb1b62356f07df8af8d963d2a8b9899cad
Brendan Foley <brendanfoley1214@proton.me>
2026-07-17 10:54:48 -0700

    Synced start.sh with dev

--- mpcontribs-api/scripts/start.sh
+++ mpcontribs-api/scripts/start.sh
@@ -10,19 +10,4 @@ PMGRC=$HOME/.pmgrc.yaml
 
 set -x
 
-<<<<<< HEAD
 exec uvicorn mpcontribs_api.app:app --host 0.0.0.0 --port "$API_PORT" --workers "${NWORKERS:-2}"
-||||||| parent of ba089932 (Infra changes for rewrite)
-if [[ -n "$DD_TRACE_HOST" ]]; then
-  wait-for-it.sh "$DD_TRACE_HOST" -q -s -t 10 && STATS_ARG="--statsd-host $DD_AGENT_HOST:8125" || echo "WARNING: datadog agent unreachable"
-fi
-
-[[ -n "$STATS_ARG" ]] && CMD="ddtrace-run gunicorn $STATS_ARG" || CMD="gunicorn"
-exec $WAIT_FOR -- $CMD $SERVER_APP
-=======
-if [[ -n "$DD_TRACE_HOST" ]]; then
-  wait-for-it.sh "$DD_TRACE_HOST" -q -s -t 10 || echo "WARNING: datadog agent unreachable"
-fi
-
-exec uvicorn mpcontribs_api.app:app --host 0.0.0.0 --port "$API_PORT"
->>>>>>> ba089932 (Infra changes for rewrite)
