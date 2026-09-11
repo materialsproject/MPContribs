cf4c929fbfb1cf03f7b091ac50c5d48c5b6f1a64
Brendan Foley <brendanfoley1214@proton.me>
2026-09-11 12:15:59 -0700

    fix(ContributionService): prevent explosion of search params when specifying long formulas
    Limited formulas used in search to 5 elements. Also added notes for future improvements

--- mpcontribs-api/src/mpcontribs_api/domains/contributions/service.py
+++ mpcontribs-api/src/mpcontribs_api/domains/contributions/service.py
@@ -1081,8 +1081,14 @@ class ContributionService:
         except (CompositionError, ValueError) as err:
             raise ValidationError(message="invalid formula provided", formula=query) from err
 
+        # Guard against explosion of permutations
+        # TODO: Remove once proper SearchIndex Analyzer is used
+        if len(comp.elements) > 5:
+            raise ValidationError(message="too many elements in search formula", query=query)
         # Build the element-count permutations to match against the formula index.
         # This block could probably be replaced by a MongoDB Analyzer.
+        # NOTE: the we should include "matchCriteria: all" so when tokenizing a formula, all tokens are required
+        # This prevents query=Fe2O3 from returning documents like AlO3 (O3 token match)
         ind_str = []
         if len(comp) == 1:
             d = comp.get_integer_formula_and_factor()
