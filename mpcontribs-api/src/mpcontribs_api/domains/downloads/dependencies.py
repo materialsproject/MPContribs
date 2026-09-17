0b6c137e240d80ddffd1708bf1aff76a57b2b134
Brendan Foley <brendanfoley1214@proton.me>
2026-09-17 13:47:30 -0700

    refactor(get_download_service): wired in S3Dep

--- mpcontribs-api/src/mpcontribs_api/domains/downloads/dependencies.py
+++ mpcontribs-api/src/mpcontribs_api/domains/downloads/dependencies.py
@@ -2,13 +2,13 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from mpcontribs_api.dependencies import SQSDep, UserDep
+from mpcontribs_api.dependencies import S3Dep, SQSDep, UserDep
 from mpcontribs_api.domains.downloads.repository import MongoDbDownloadRepository
 from mpcontribs_api.domains.downloads.service import DownloadService
 
 
-def get_download_service(user: UserDep, sqs: SQSDep) -> DownloadService:
-    return DownloadService(downloads=MongoDbDownloadRepository(user), sqs=sqs)
+def get_download_service(user: UserDep, sqs: SQSDep, s3: S3Dep) -> DownloadService:
+    return DownloadService(downloads=MongoDbDownloadRepository(user), sqs=sqs, s3=s3)
 
 
 DownloadServiceDep = Annotated[DownloadService, Depends(get_download_service)]
--- mpcontribs-api/src/mpcontribs_api/domains/downloads/repository.py
+++ mpcontribs-api/src/mpcontribs_api/domains/downloads/repository.py
@@ -13,7 +13,8 @@ class MongoDbDownloadRepository(MongoDbRepository[Download, DownloadIn, Download
     """Repository for download-job documents.
 
     A download job is scoped to the `requester`. However, the physical file can be reused by multiple requesters when
-    the download record hashes to the same s3_key - this provides deduplication, while still maintaining user privacy.
+    the download record hashes to the same s3_key - this provides deduplication of S3 objects, while still maintaining
+    user privacy.
     """
 
     document_model = Download
